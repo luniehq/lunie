9cf7d29888518897b429bee262fd85a0520967ee
Matt Bell <mappum@gmail.com>
2017-01-25 15:08:46 -0800

    Build tendermint binaries in build task

--- tasks/release.js
+++ tasks/release.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const exec = require('child_process').exec
+const path = require('path')
 const packager = require('electron-packager')
+const mkdirp = require('mkdirp').sync
 
 if (process.env.PLATFORM_TARGET === 'clean') {
   require('del').sync(['builds/*', '!.gitkeep'])
@@ -26,6 +28,8 @@ function pack () {
 function build () {
   let options = require('../config').building
 
+  options.afterCopy = [ buildTendermint ]
+
   console.log('\x1b[34mBuilding electron app(s)...\n\x1b[0m')
   packager(options, (err, appPaths) => {
     if (err) {
@@ -38,4 +42,30 @@ function build () {
       console.log('\n\x1b[34mDONE\n\x1b[0m')
     }
   })
+}
+
+const GOARCH = {
+  'x64': 'amd64',
+  'ia32': '386'
+}
+
+/**
+ * Build Tendermint binary for current arch/platform
+ */
+function buildTendermint (buildPath, electronVersion, platform, arch, cb) {
+  console.log('\x1b[34mBuilding Tendermint...\n\x1b[0m')
+
+  if (GOARCH[arch]) arch = GOARCH[arch]
+
+  let binPath = path.join(buildPath, 'bin')
+  mkdirp(binPath)
+  let tmPath = path.join(binPath, 'tendermint')
+  let tmPkg = 'github.com/tendermint/tendermint/cmd/tendermint'
+  let cmd = `GOOS=${platform} GOARCH=${arch} go build -o ${tmPath} ${tmPkg}`
+  console.log(`> ${cmd}\n`)
+  let goBuild = exec(cmd)
+
+  goBuild.stdout.pipe(process.stdout)
+  goBuild.stderr.pipe(process.stderr)
+  goBuild.once('exit', () => cb())
 }
