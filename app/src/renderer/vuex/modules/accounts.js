be677cae1c302984292faf1df2ed56f32a01c018
Peng Zhong <peng@nylira.com>
2017-02-15 13:06:02 +0800

    start adding wallet db data to ui

--- app/src/renderer/vuex/modules/accounts.js
+++ app/src/renderer/vuex/modules/accounts.js
@@ -1,14 +1,36 @@
-import data from '../json/accounts.json'
+let { join } = require('path')
+let Basecoin = require('basecoin')
+let root = require('../../../root.js')
 
-const state = data
+// Connect to our local tendermint RPC server
+let client = Basecoin('ws://localhost:46657')
+
+// Create or load a wallet at a certain file path.
+// A wallet is a database which stores a collection of accounts/keys,
+// as well as a log of transactions.
+let walletPath = join(root, 'wallet2.db')
+
+let balances
+
+client.wallet(walletPath).then((wallet) => {
+  wallet.on('tx', (tx) => {
+    // some coins were sent or received from an account in this wallet
+
+    balances = wallet.getBalances()
+    // balances is in this format:
+    // [
+    //   { denom: 'btc', amount: 1234 },
+    //   { denom: 'atom', amount: 567890 }
+    // ]
+    console.log('balances loaded!', balances)
+  })
+})
+
+const state = balances
 
 const mutations = {
   setWalletExpanded (state, data) {
-    let key = data.key
-    let value = data.value
-    console.log(`state[${key}]`, state[key])
-    console.log('setWalletExpanded', key, value)
-    state[key].expanded = value
+    state[data.key].expanded = data.value
   }
 }
 
