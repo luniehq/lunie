d92f5793ae4db20f21062720e0ea8b019ea5ffb3
Peng Zhong <peng@nylira.com>
2017-05-12 18:40:34 +0200

    add basic styles to pos/neg transactions

--- app/src/renderer/components/CardTransaction.vue
+++ app/src/renderer/components/CardTransaction.vue
@@ -1,18 +1,16 @@
 <template>
   <ul class="card-transaction">
     <li class="date">{{ date }}</li>
-    <li>
-      <span class="key">gas</span>
-      <span class="value">{{ tran.gas }}</span>
-    </li>
-    <li>
-      <span class="key">fee </span>
-      <span class="value">{{ tran.fee.amount }}</span>
-    </li>
-    <li v-for="coin in tran.inputs[0].coins" class="input">
-      <span class="key">{{ coin.denom }}</span>
-      <span class="value">{{ coin.amount }}</span>
-    </li>
+    <template v-for="coin in tran.inputs[0].coins">
+      <li class="input">
+        <span class="key">Denom</span>
+        <span class="value">{{ coin.denom }}</span>
+      </li>
+      <li class="input">
+        <span class="key">Balance</span>
+        <span :class="valueCss(coin.amount)">{{ coin.amount }}</span>
+      </li>
+    </template>
   </ul>
 </template>
 
@@ -24,6 +22,17 @@ export default {
     },
     date () {
       return new Date(this.transactionValue.time).toLocaleString()
+    }
+  },
+  methods: {
+    valueCss (num) {
+      let v = 'value'
+      if (num > 0) {
+        v += ' positive'
+      } else if (num < 0) {
+        v += ' negative'
+      }
+      return v
     }
   },
   props: ['transaction-value']
@@ -50,6 +59,21 @@ ul.card-transaction
       padding-right 0.5rem
     &.date, &.input
       flex 2
+
+    .value
+      display inline-block
+
+      &.positive
+        color hsl(120,100%,35%)
+        &:before
+          content '+'
+          display inline-block
+
+      &.negative
+        color hsl(0,100%,35%)
+        &:before
+          content '-'
+          display inline-block
 
 ul.card-transaction:first-of-type
   border-top 1px solid bc
