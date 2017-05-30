5789bcb68343789f36d73c25cc849d565877aff6
Peng Zhong <peng@nylira.com>
2017-05-30 12:29:13 +0800

    remove unused components

--- app/src/renderer/vuex/getters.js
+++ app/src/renderer/vuex/getters.js
@@ -1,9 +1,4 @@
-export const mainCounter = state => state.counters.main
-export const allWallets = state => state.wallets.wallets
-export const allTransactions = state => state.transactions
-export const allAddresses = state => state.addresses
 export const notifications = state => state.notifications
-export const logOutput = state => state.log.output
 
 export const syncHeight = state => state.node.syncHeight
 export const syncTime = state => state.node.syncTime
