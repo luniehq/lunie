13e36604dab06719dba0a579452ad90d90d4d8a0
Matt Bell <mappum@gmail.com>
2017-08-16 08:51:41 -0700

    Added OS menu to support copy/paste/etc

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
 import fs from 'fs-extra'
 import { join } from 'path'
 import { spawn } from 'child_process'
@@ -53,6 +53,30 @@ function createWindow () {
   }
   webContents.on('will-navigate', handleRedirect)
   webContents.on('new-window', handleRedirect)
+
+  // setup menu to handle copy/paste, etc
+  var template = [
+    {
+      label: "Cosmos UI",
+      submenu: [
+        { label: "About Cosmos UI", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit() }}
+    ]},
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+    ]
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 }
 
 function startProcess (name, ...args) {
