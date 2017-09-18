bea07a3eda7813fb8aecff39879b962155935ce1
Matt Bell <mappum@gmail.com>
2017-09-18 15:46:01 -0700

    Create temp home directory when launching app in tests

--- test/launch.js
+++ test/launch.js
@@ -4,14 +4,21 @@ let { Application } = require('spectron')
 let test = require('tape')
 let electron = require('electron')
 let { join } = require('path')
+let { tmpdir } = require('os')
 
 async function launch () {
+  let home = join(tmpdir(), Math.random().toString(36).slice(2))
+  console.error(`COSMOS_HOME: ${home}`)
+
   let app = new Application({
     path: electron,
     args: [ join(__dirname, '../app/dist/main.js') ],
     startTimeout: 10000,
     waitTimeout: 10000,
-    env: { COSMOS_TEST: 'true' }
+    env: {
+      COSMOS_TEST: 'true',
+      COSMOS_HOME: home
+    }
   })
   await app.start()
   return app
@@ -20,6 +27,5 @@ async function launch () {
 test('launch app', async (t) => {
   let app = await launch()
   t.ok(app.isRunning(), 'app launched')
-  app.stop()
   t.end()
 })
