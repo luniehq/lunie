67bf9bc7c0041c61d6ab255cbfe8f0a4076f1749
Matt Bell <mappum@gmail.com>
2017-09-18 15:45:39 -0700

    Added COSMOS_HOME envvar

--- app/src/root.js
+++ app/src/root.js
@@ -1,5 +1,10 @@
-const { join } = require('path')
-const home = require('user-home')
-const pkg = require('../package.json')
-const DEV = process.env.NODE_ENV === 'development'
-module.exports = join(home, `.${pkg.name}${DEV ? '-dev' : ''}`)
+const { COSMOS_HOME, NODE_ENV } = process.env
+if (COSMOS_HOME) {
+  module.exports = COSMOS_HOME
+} else {
+  const home = require('user-home')
+  const { join } = require('path')
+  const pkg = require('../package.json')
+  const DEV = NODE_ENV === 'development'
+  module.exports = join(home, `.${pkg.name}${DEV ? '-dev' : ''}`)
+}
