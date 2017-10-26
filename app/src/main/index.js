9ea141c9f671458ed6feb69c82d374e45322413f
Fabian <frznhope@gmail.com>
2017-10-26 13:17:24 +0200

    Redirecting output of processes to console

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -145,7 +145,7 @@ app.on('activate', () => {
 
 // start basecoin node
 function startBasecoin (root) {
-  let log = fs.createWriteStream(join(root, 'basecoin.log'))
+  let log = TEST ? process.stdout : fs.createWriteStream(join(root, 'basecoin.log'))
   let opts = {
     env: {
       BCHOME: root,
@@ -172,7 +172,7 @@ function startBasecoin (root) {
 
 // start tendermint node
 async function startTendermint (root) {
-  let log = fs.createWriteStream(join(root, 'tendermint.log'))
+  let log = TEST ? process.stdout : fs.createWriteStream(join(root, 'tendermint.log'))
   let opts = {
     env: {
       BCHOME: root,
@@ -228,7 +228,7 @@ async function startBaseserver (home) {
   console.log('startBaseserver', home)
   const logFile = join(home, 'baseserver.log')
   fs.ensureFileSync(logFile)
-  let log = fs.createWriteStream(logFile)
+  let log = TEST ? process.stdout : fs.createWriteStream(logFile)
 
   let child = startProcess(SERVER_BINARY, [
     'serve',
