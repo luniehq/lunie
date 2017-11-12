f607d41fe34a1cc7cd77d4e1e923743fe016e763
Fabian <frznhope@gmail.com>
2017-11-12 11:35:31 -0500

    shrinking stacktrace to just the files in the project
    also not emitting errors twice like vue does

---
+++ app/src/helpers/shrink-stacktrace.js
@@ -0,0 +1,26 @@
+import StackTrace from 'stacktrace-js'
+
+/*
+* ATTENTION: Enable sourcemaps in Chrome!!
+* ATTENTION: Use "devtool: '#eval-source-map'" for webpack
+*/
+export default function shrinkStacktrace (error,
+  blackList = /node_modules/,
+  whiteList = /.*/
+) {
+  // stacktrace.js gives us the sourcemappes filenames
+  StackTrace.fromError(error, {
+    offline: true
+  })
+  .then(frames => {
+    let filteredFrames = frames.filter(frame => {
+        // we check the sourcemapped frames for filenames blacklisted / whitelisted
+      return blackList.test(frame.fileName) ? false
+            : whiteList.test(frame.fileName)
+    })
+    .map(frame => frame.source)
+
+    console.error(error.message + '\n' + filteredFrames.join('\n'))
+  })
+  .catch(console.error)
+}
