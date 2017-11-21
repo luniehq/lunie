ae09fc9111e3337f2dbf18a6dca9746f7280002b
Peng Zhong <peng@nylira.com>
2017-11-21 12:05:07 -0600

    fix #79 fix #80

--- app/src/renderer/routes.js
+++ app/src/renderer/routes.js
@@ -42,11 +42,6 @@ export default [
         component: monitor('ValidatorIndex')
       },
       {
-        path: 'delegators',
-        name: 'validator-delegators',
-        component: monitor('ValidatorDelegators')
-      },
-      {
         path: 'power',
         name: 'validator-power',
         component: monitor('ValidatorPower')
@@ -68,8 +63,6 @@ export default [
       }
     ]
   },
-  { path: '/delegators', name: 'delegators', component: monitor('Delegators') },
-  { path: '/delegators/:delegator', name: 'delegator', component: monitor('Delegator') },
 
   // USER
   { path: '/signin', name: 'signin', component: common('SignIn') },
