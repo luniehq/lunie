0a898ec9617455970c2c45713752a9a7ade48892
Peng Zhong <peng@nylira.com>
2017-11-21 14:10:23 -0600

    fix error messages not appearing

--- app/src/renderer/components/wallet/PageSend.vue
+++ app/src/renderer/components/wallet/PageSend.vue
@@ -1,40 +1,48 @@
 <template lang='pug'>
-page(title='Send Coins')
-  form-struct(:submit='verifySend')
-    form-group(:error='$v.fields.denom.$error'
-      field-id='send-denomination' field-label='Denomination')
-      field-group
-        field#send-denomination(
-          type="select"
-          v-model="fields.denom"
-          :options="denominations"
-          @input="$v.fields.denom.$touch()"
-          placeholder="Denomination"
-          required)
-      form-msg(name='Denomination', type='required', v-if='!$v.fields.denom.required')
+page(title='Send Tokens'): form-struct(:submit='onSubmit')
+  form-group(:error='$v.fields.denom.$error'
+    field-id='send-denomination' field-label='Denomination')
+    field-group
+      field#send-denomination(
+        type="select"
+        v-model="fields.denom"
+        :options="denominations"
+        @input="$v.fields.denom.$touch()"
+        placeholder="Select token...")
+    form-msg(name='Denomination' type='required' v-if='!$v.fields.denom.required')
 
-    form-group(:error='$v.fields.address.$error'
-      field-id='send-address' field-label='Pay To')
-      field-group
-        field#send-address(type='text', v-model='fields.address', @input='$v.fields.address.$touch()', placeholder='Address', required='')
-      form-msg(name='Address', type='required', v-if='!$v.fields.address.required')
-      form-msg(name='Address', type='exactLength', length='40', v-if='!$v.fields.address.minLength || !$v.fields.address.maxLength')
-      form-msg(name='Address', type='alphaNum', v-if='!$v.fields.address.alphaNum')
+  form-group(:error='$v.fields.address.$error'
+    field-id='send-address' field-label='Pay To')
+    field-group
+      field#send-address(
+        type='text'
+        v-model='fields.address'
+        @input='$v.fields.address.$touch()'
+        placeholder='Address')
+    form-msg(name='Address' type='required' v-if='!$v.fields.address.required')
+    form-msg(name='Address' type='exactLength' length='40'
+      v-if='!$v.fields.address.minLength || !$v.fields.address.maxLength')
+    form-msg(name='Address' type='alphaNum' v-if='!$v.fields.address.alphaNum')
 
-    form-group(:error='$v.fields.amount.$error'
-      field-id='send-amount' field-label='Amount')
-      field-group
-        field#send-amount(type='number', v-model='fields.amount', @input='$v.fields.amount.$touch()', placeholder='Amount', required='')
-        field-addon Coins
-        btn(value='Max')
-      form-msg(name='Amount', type='required', v-if='!$v.fields.amount.required')
-      form-msg(name='Amount', type='between', min='1', max='1000000', v-if='!$v.fields.amount.between')
+  form-group(:error='$v.fields.amount.$error'
+    field-id='send-amount' field-label='Amount')
+    field-group
+      field#send-amount(
+        type='number'
+        v-model='fields.amount'
+        @input='$v.fields.amount.$touch()'
+        placeholder='Amount')
+      field-addon Coins
+      btn(value='Max')
+    form-msg(name='Amount' type='required' v-if='!$v.fields.amount.required')
+    form-msg(name='Amount' type='between' min='1' max='1000000'
+      v-if='!$v.fields.amount.between')
 
-    div(slot='footer')
-      // btn(value='Reset', @click.native='resetForm')
-      div
-      btn(v-if='sending' value='Sending...' disabled)
-      btn(v-else type='submit' value='Send Now')
+  div(slot='footer')
+    // btn(value='Reset' @click.native='resetForm')
+    div
+    btn(v-if='sending' value='Sending...' disabled)
+    btn(v-else @click='onSubmit' icon="check" value="Send Tokens")
 </template>
 
 <script>
@@ -64,7 +72,8 @@ export default {
   computed: {
     ...mapGetters(['wallet']),
     denominations () {
-      return this.wallet.balances.map(i => ({ key: i.denom.toUpperCase(), value: i.denom }))
+      return this.wallet.balances.map(i =>
+        ({ key: i.denom.toUpperCase(), value: i.denom }))
     }
   },
   methods: {
@@ -74,10 +83,10 @@ export default {
       this.sending = false
       this.$v.$reset()
     },
-    verifySend () {
-      console.log('sending', this.fields)
+    onSubmit () {
       this.$v.$touch()
       if (this.$v.$error) return
+
       this.sending = true
       let amount = +this.fields.amount
       let address = this.fields.address
@@ -112,7 +121,7 @@ export default {
     },
     sending: false
   }),
-  validations: {
+  validations: () => ({
     fields: {
       address: {
         required,
@@ -128,7 +137,7 @@ export default {
         required
       }
     }
-  }
+  })
 }
 </script>
 
