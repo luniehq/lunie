74636ec9b04c8959a6e954cc1bf8baa04586fde9
Fabian <frznhope@gmail.com>
2017-11-22 15:22:53 +0100

    fixed and improved test system
    cherry pick of several not merged improvements

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,13 +1,20 @@
-jest.mock('@/renderer/node.js', () => () => ({
+jest.mock('renderer/node.js', () => () => ({
   generateKey: () => ({key: '123'}),
   queryAccount: () => null,
   queryNonce: () => '123'
 }))
 
+// needs to be mocked as it imports the native supercop module and this fails in jest
+jest.mock('app/node_modules/tendermint-crypto', () => {})
+
 describe('App', () => {
   it('has all dependencies', async done => {
-    await require('@/renderer/main.js')
-    .catch(done.fail)
+    document.body.innerHTML = '<div id="app"></div>'
+    try {
+      await require('renderer/main.js')
+    } catch (e) {
+      done.fail(e)
+    }
     done()
   })
 })
