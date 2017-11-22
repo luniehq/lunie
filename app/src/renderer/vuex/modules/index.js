02a9e7f28957d5d0fccd9c81d22df210391ed555
Fabian <frznhope@gmail.com>
2017-11-22 16:02:08 +0100

    fixed merge conflict

--- app/src/renderer/vuex/modules/index.js
+++ app/src/renderer/vuex/modules/index.js
@@ -1,7 +1,6 @@
 export default (opts) => ({
   candidates: require('./candidates.js').default(opts),
   config: require('./config.js').default(opts),
-  delegators: require('./delegators.js').default(opts),
   filters: require('./filters.js').default(opts),
   node: require('./node.js').default(opts),
   notifications: require('./notifications.js').default(opts),
