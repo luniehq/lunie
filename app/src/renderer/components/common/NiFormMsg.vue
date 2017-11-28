6bfb6f9e10ea7a7a9bd5a164927cc6b08eb7e75c
Peng Zhong <peng@nylira.com>
2017-11-29 01:00:55 +0900

    improve new account page

--- app/src/renderer/components/common/NiFormMsg.vue
+++ app/src/renderer/components/common/NiFormMsg.vue
@@ -56,10 +56,10 @@ export default {
 </script>
 
 <style lang="stylus">
-@require '~@/styles/variables.styl'
+@require '~variables'
 
 .ni-form-msg
-  line-height 2rem
+  padding 0.25rem 0
   display flex
   margin 0 !important
  
--- app/src/renderer/components/common/NiModalWelcome.vue
+++ app/src/renderer/components/common/NiModalWelcome.vue
@@ -3,41 +3,60 @@
 
   .ni-mw(v-if="status == 'welcome'"): .ni-mw-container
     .ni-mw-header
-      .ni-mw-title Welcome to Cosmos!
+      .ni-mw-title Welcome to Cosmos
     .ni-mw-main
-      list-item(@click.native="setStatus('new-wallet')" title="Create new wallet") 
-      list-item(@click.native="setStatus('existing-wallet')" title="Sign in with existing wallet")
-      list-item(@click.native="setStatus('hardware-wallet')" title="Sign in with hardware wallet")
+      list-item(@click.native="setStatus('sign-up')" title="Create new account") 
+      list-item(@click.native="setStatus('sign-in-seed')" title="Sign in with seed")
+      list-item(@click.native="setStatus('sign-in-hardware')" title="Sign in with hardware")
     .ni-mw-footer
 
-  .ni-mw(v-if="status == 'new-wallet'"): form-struct(:submit='signUp').ni-mw-container
+  .ni-mw(v-if="status == 'sign-up'"): form-struct(:submit='signUp').ni-mw-container
     .ni-mw-header
-      btn(@click.native="setStatus('welcome')" icon="arrow_back")
-      .ni-mw-title New Wallet
-      div
+      a(@click="setStatus('welcome')"): i.material-icons arrow_back
+      .ni-mw-title New Account
+      a: i.material-icons help_outline
     .ni-mw-main
       form-group(:error='$v.fields.signUpSeed.$error'
-        field-id='sign-up-seed' field-label='Mnemonic (12 words)')
+        field-id='sign-up-seed' field-label='Seed (12 words)')
         field#sign-up-seed(
+          type="textarea"
           v-model="fields.signUpSeed"
           @input="$v.fields.signUpSeed.$touch()")
         form-msg(name='Seed' type='required' v-if='!$v.fields.signUpSeed.required')
-    .ni-mw-footer
-      btn(@click='signUp' icon="check" value="Create Wallet")
+        form-msg(body='There is no such thing as seed recovery. You need to back up these words.')
 
-  .ni-mw(v-if="status == 'existing-wallet'"): form-struct(:submit='signIn').ni-mw-container
+      form-group(:error='$v.fields.signUpWarning.$error')
+        .ni-field-checkbox
+          .ni-field-checkbox-input
+            input#sign-up-warning(type="checkbox" v-model="fields.signUpWarning")
+          label.ni-field-checkbox-label(for="sign-up-warning")
+            | I understand that Cosmos cannot recover lost seed phrases.
+        form-msg(name='Warning' type='required' v-if='!$v.fields.signUpWarning.required')
+
+      form-group(:error='$v.fields.signUpBackup.$error')
+        .ni-field-checkbox(for="sign-up-backup")
+          .ni-field-checkbox-input
+            input#sign-up-backup(type="checkbox" v-model="fields.signUpBackup")
+          label.ni-field-checkbox-label(for="sign-up-backup")
+            | I have securely backed up my generated seed phrase.
+        form-msg(name='Backup' type='required' v-if='!$v.fields.signUpBackup.required')
+
+    .ni-mw-footer
+      btn(@click.native='signUp' icon="check" value="Create Wallet")
+
+  .ni-mw(v-if="status == 'sign-in-seed'"): form-struct(:submit='signIn').ni-mw-container
     .ni-mw-header
-      btn(@click.native="setStatus('welcome')" icon="arrow_back")
-      .ni-mw-title Existing wallet
-      div
+      a(@click="setStatus('welcome')"): i.material-icons arrow_back
+      .ni-mw-title Enter Seed
+      a: i.material-icons help_outline
     .ni-mw-main
     .ni-mw-footer
 
-  .ni-mw(v-if="status == 'hardware-wallet'"): .ni-mw-container
+  .ni-mw(v-if="status == 'sign-in-hardware'"): .ni-mw-container
     .ni-mw-header
-      btn(@click.native="setStatus('welcome')" icon="arrow_back")
-      .ni-mw-title Hardware Wallet
-      div
+      a(@click="setStatus('welcome')"): i.material-icons arrow_back
+      .ni-mw-title Plug In Hardware
+      a: i.material-icons help_outline
     .ni-mw-main
     .ni-mw-footer
 </template>
@@ -71,6 +90,8 @@ export default {
     status: 'welcome',
     fields: {
       signUpSeed: 'one two three four five six seven eight nine ten eleven twelve',
+      signUpWarning: false,
+      signUpBackup: false,
       signInSeed: ''
     }
   }),
@@ -87,9 +108,9 @@ export default {
   },
   validations: () => ({
     fields: {
-      signUpSeed: {
-        required
-      },
+      signUpSeed: { required },
+      signUpWarning: { required },
+      signUpBackup: { required },
       signInSeed: {
         required
       }
@@ -101,34 +122,58 @@ export default {
 <style lang="stylus">
 @import '~variables'
 
+.ni-field-checkbox
+  display flex
+  flex-flow row nowrap
+
+  .ni-field-checkbox-input
+    flex 0 0 3rem
+    height 3rem
+    display flex
+    align-items center
+    justify-content center
+    background app-fg
+
+  .ni-field-checkbox-label
+    line-height 1.5
+    padding-left 1rem
+
 .ni-mw
   position fixed
   top 0
   left 0
   z-index 1000
 
-  width 100vw
-  height 100vh
   background app-bg
 
-  display flex
-  justify-content center
-  align-items center
-
 .ni-mw-container
-  display flex
-  flex-flow column nowrap
+  &:not(.ni-form)
+    width 100vw
+    height 100vh
+    display flex
+    flex-flow column nowrap
+  &.ni-form
+    .ni-form-main
+      width 100vw
+      height 100vh
+      display flex
+      flex-flow column nowrap
 
-  max-width 40rem
-  max-height 40rem
-  
 .ni-mw-header
   display flex
   flex-flow row nowrap
+  justify-content space-between
   align-items center
   flex 0 0 3rem
-  margin-bottom 1rem
 
+  a
+    width 3rem
+    display flex
+    align-items center
+    justify-content center
+    i
+      color txt
+      font-size lg
   .ni-mw-title
     flex 1
     padding 0 1rem
@@ -141,14 +186,26 @@ export default {
   flex-flow column
   justify-content center
 
+  overflow-y scroll
+
   border px solid bc
   border-bottom none
 
   .ps-scrollbar-y-rail
     display none
 
+  > p
+    font-size sm
+    padding 1rem
+    border-bottom px solid bc
+
 .ni-mw-footer
-  flex 0 0 4.0625rem
+  border-top px solid bc
+  flex 0 0 4rem + px
+  display flex
+  align-items center
+  justify-content center
+
   &:empty
     display none
 
