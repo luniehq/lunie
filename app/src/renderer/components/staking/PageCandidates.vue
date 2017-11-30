ef16cca3b6bda5883e16189d1a5b933d111276af
Peng Zhong <peng@nylira.com>
2017-11-30 15:49:52 +0900

    simplify PageCandidates title

--- app/src/renderer/components/staking/PageCandidates.vue
+++ app/src/renderer/components/staking/PageCandidates.vue
@@ -44,8 +44,7 @@ export default {
   computed: {
     ...mapGetters(['candidates', 'filters', 'shoppingCart', 'user']),
     pageTitle () {
-      if (this.user.signedIn) return `Delegate (${this.candidatesNum} Candidates Selected)`
-      else return 'Delegate'
+      return `Delegate (${this.candidatesNum} Candidates Selected)`
     },
     filteredCandidates () {
       let query = this.filters.candidates.search.query
