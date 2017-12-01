7bd4eee20b3512c97d03c2ba65175ebd0f3fb9f3
Fabian <frznhope@gmail.com>
2017-12-01 11:32:11 +0100

    added some tests to CardTransaction

--- test/unit/specs/CardTransaction.spec.js
+++ test/unit/specs/CardTransaction.spec.js
@@ -39,4 +39,79 @@ describe('CardTransaction', () => {
   it('has the expected html structure', () => {
     expect(wrapper.vm.$el).toMatchSnapshot()
   })
+  
+  it('should show incoming transcations', () => {
+    expect(wrapper.find('.value').hasClass('positive')).toBe(true)
+  })
+  
+  it('should show outgoing transcations', () => {
+    wrapper.setProps({
+      transactionValue: {
+        tx: {
+          inputs: [
+            {
+              coins: [{
+                denom: 'jbcoins',
+                amount: 1234
+              }],
+              sender: 'myAddress'
+            }
+          ],
+          outputs: [
+            {
+              coins: [{
+                denom: 'jbcoins',
+                amount: 1234
+              }],
+              recipient: 'otherAddress'
+            }
+          ]
+        },
+        time: Date.now()
+      },
+      address: 'myAddress'
+    })
+    expect(wrapper.find('.value').hasClass('negative')).toBe(true)
+  })
+  
+  it('should show all coins of the transaction', () => {
+    wrapper.setProps({
+      transactionValue: {
+        tx: {
+          inputs: [
+            {
+              coins: [{
+                denom: 'jbcoins',
+                amount: 1234
+              }, {
+                denom: 'fabocoins',
+                amount: 1
+              }, {
+                denom: 'mattcoins',
+                amount: 42
+              }],
+              sender: 'otherAddress'
+            }
+          ],
+          outputs: [{
+            coins: [{
+              denom: 'jbcoins',
+              amount: 1234
+            }, {
+              denom: 'fabocoins',
+              amount: 1
+            }, {
+              denom: 'mattcoins',
+              amount: 42
+            }],
+            recipient: 'myAddress'
+          }]
+        },
+        time: Date.now()
+      },
+      address: 'myAddress'
+    })
+    expect(wrapper.findAll('.key-value').length).toBe(3)
+    expect(wrapper.findAll('.key-value').at(2).html()).toContain('42')
+  })
 })
