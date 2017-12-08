33fb4ad850938900256a7c6d656e8bf355b97362
Peng Zhong <peng@nylira.com>
2017-12-08 12:59:10 +0900

    remove empty template()

--- app/src/renderer/components/common/NiSessionWelcome.vue
+++ app/src/renderer/components/common/NiSessionWelcome.vue
@@ -5,29 +5,28 @@
     .ni-session-title Welcome to Cosmos!
     a(@click="help"): i.material-icons help_outline
   .ni-session-main
-    template()
-      li-session(
-        v-if="accountExists"
-        @click.native="setState('sign-in')"
-        icon="lock"
-        title="Sign in with password"
-        subtitle="If you have an account, choose this option")
-      li-session(
-        @click.native="setState('sign-up')"
-        icon="create"
-        title="Create new account"
-        subtitle="Generate a brand new seed and account")
-      li-session(
-        @click.native="setState('restore')"
-        icon="settings_backup_restore"
-        title="Restore account from seed"
-        subtitle="If you have a seed, choose this option")
-      li-session(
-        v-if="config.devMode"
-        @click.native="setState('hardware')"
-        icon="usb"
-        title="Sign in with hardware"
-        subtitle="If you have a Ledger Wallet, choose this option")
+    li-session(
+      v-if="accountExists"
+      @click.native="setState('sign-in')"
+      icon="lock"
+      title="Sign in with password"
+      subtitle="If you have an account, choose this option")
+    li-session(
+      @click.native="setState('sign-up')"
+      icon="create"
+      title="Create new account"
+      subtitle="Generate a brand new seed and account")
+    li-session(
+      @click.native="setState('restore')"
+      icon="settings_backup_restore"
+      title="Restore account from seed"
+      subtitle="If you have a seed, choose this option")
+    li-session(
+      v-if="config.devMode"
+      @click.native="setState('hardware')"
+      icon="usb"
+      title="Sign in with hardware"
+      subtitle="If you have a Ledger Wallet, choose this option")
   .ni-session-footer
 </template>
 
