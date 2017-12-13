e35217382e63237def8a753b46447c1d80156fe3
Fabian Weber <frznhope@gmail.com>
2017-12-13 15:10:13 +0100

    a little improvement to the setup

--- tasks/windows-installer.js
+++ tasks/windows-installer.js
@@ -5,8 +5,13 @@ var package = require('../package.json')
 let resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: './builds/Cosmos-win32-x64',
     outputDirectory: './builds/cosmos/Cosmos-win32',
+    title: 'Cosmos',
     authors: package.author,
-    exe: 'Cosmos.exe'
+    exe: 'Cosmos.exe',
+    setupExe: `Cosmos-Setup-v${package.version}.exe`,
+    iconUrl: 'https://github.com/cosmos/cosmos-ui/raw/develop/app/icons/icon.ico',
+    setupIcon: './app/icons/icon.ico',
+    noMsi: true
   });
 
 return resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
