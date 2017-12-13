1c28ac150b1bc3e4c8cb8b367b88a832d1f7674e
Fabian <frznhope@gmail.com>
2017-12-13 10:43:34 +0100

    made tests independent

--- test/unit/specs/NISessionWelcome.spec.js
+++ test/unit/specs/NISessionWelcome.spec.js
@@ -25,32 +25,41 @@ describe('NISessionWelcome', () => {
     })
   })
 
-  it('should open the help model on click', () => {
-    wrapper.findAll('.ni-session-header a').at(1).trigger('click')
-    expect(store.commit.mock.calls[0]).toEqual(['setModalHelp', true])
+  describe('without accounts', () => {
+    it('should open the help model on click', () => {
+      wrapper.findAll('.ni-session-header a').at(1).trigger('click')
+      expect(store.commit.mock.calls[0]).toEqual(['setModalHelp', true])
+    })
+
+    it('should not show sign-in link since we have no accounts', () => {
+      wrapper.find(LiSession).trigger('click')
+      expect(store.commit.mock.calls[0][1]).not.toEqual('sign-in')
+    })
+
+    it('has the expected html structure', () => {
+      expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+    })
   })
 
-  it('should not show sign-in link since we have no accounts', () => {
-    wrapper.find(LiSession).trigger('click')
-    expect(store.commit.mock.calls[0][1]).not.toEqual('sign-in')
-  })
+  describe('with accounts', () => {
+    beforeEach(() => {
+      wrapper.vm.user.accounts = ['foo', 'bar']
+      wrapper.update()
+    })
 
-  it('(set up accounts for following tests)', () => {
-    accounts.push('foo', 'bar')
-  })
+    it('should show sign-in link since we have accounts', () => {
+      wrapper.find(LiSession).trigger('click')
+      expect(store.commit.mock.calls[0][1]).toEqual('sign-in')
+    })
 
-  it('should show sign-in link since we have accounts', () => {
-    wrapper.find(LiSession).trigger('click')
-    expect(store.commit.mock.calls[0][1]).toEqual('sign-in')
-  })
+    it('sets desired login method', () => {
+      wrapper.findAll(LiSession).trigger('click')
+      expect(store.commit.mock.calls[0][0]).toBe('setModalSessionState')
+      expect(store.commit.mock.calls.map(args => args[1])).toEqual(['sign-in', 'sign-up', 'restore', 'hardware'])
+    })
 
-  it('sets desired login method', () => {
-    wrapper.findAll(LiSession).trigger('click')
-    expect(store.commit.mock.calls[0][0]).toBe('setModalSessionState')
-    expect(store.commit.mock.calls.map(args => args[1])).toEqual(['sign-in', 'sign-up', 'restore', 'hardware'])
-  })
-
-  it('has the expected html structure', () => {
-    expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+    it('has the expected html structure', () => {
+      expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+    })
   })
 })
