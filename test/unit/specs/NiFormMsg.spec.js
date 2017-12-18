d43febda306addfbbdc54646377a42728a9103c7
Peng Zhong <peng@nylira.com>
2017-12-18 12:48:46 +0900

    tests for NiFormMsg

---
+++ test/unit/specs/NiFormMsg.spec.js
@@ -0,0 +1,34 @@
+import { mount } from 'vue-test-utils'
+import htmlBeautify from 'html-beautify'
+import NiFormMsg from 'common/NiFormMsg'
+
+describe('NiFormMsg', () => {
+  let wrapper
+
+  let propsData = {
+    type: 'length',
+    body: '',
+    name: 'Password',
+    min: 16,
+    max: 255,
+    length: ''
+  }
+
+  beforeEach(() => {
+    wrapper = mount(NiFormMsg, { propsData })
+  })
+
+  it('has a type from props', () => {
+    expect(wrapper.vm.type).toBe('length')
+  })
+
+  it('has the expected html structure', () => {
+    expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+  })
+
+  it('shows the correct message', () => {
+    expect(wrapper.find('.ni-form-msg').html().toLowerCase())
+      .toContain('password must be between 16 and 255 characters')
+  })
+
+})
