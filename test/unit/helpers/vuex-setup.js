6e22cf7414204c9b520cb4d1e23781aa44cf360a
Fabian <frznhope@gmail.com>
2017-12-18 12:30:23 +0100

    extracted test setup for vuex tests

---
+++ test/unit/helpers/vuex-setup.js
@@ -0,0 +1,37 @@
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import { shallow, createLocalVue } from 'vue-test-utils'
+
+const Modules = require('renderer/vuex/modules').default
+const Getters = require('renderer/vuex/getters')
+
+export default function vuexSetup (getters = {}) {
+  const modules = Modules({
+    node: require('../helpers/node_mock')
+  })
+
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(VueRouter)
+
+  let store = new Vuex.Store({
+    getters: Object.assign({}, Getters, getters),
+    modules
+  })
+  store.commit('setDevMode', true)
+  let router = new VueRouter({})
+  jest.spyOn(store, 'dispatch')
+  jest.spyOn(store, 'commit')
+
+  return {
+    localVue,
+    store,
+    router,
+    new: (componentConstructor, testType = shallow) => {
+      return testType(componentConstructor, {
+        localVue, store, router
+      })
+    }
+  }
+}
+
