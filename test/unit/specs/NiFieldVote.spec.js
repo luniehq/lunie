39b8281ddd0bea5c928c921916fe4084891099e7
Peng Zhong <peng@nylira.com>
2017-12-18 12:19:13 +0900

    tests for NiFieldVote

---
+++ test/unit/specs/NiFieldVote.spec.js
@@ -0,0 +1,41 @@
+import { mount } from 'vue-test-utils'
+import htmlBeautify from 'html-beautify'
+import NiFieldVote from 'common/NiFieldVote'
+
+describe('NiFieldVote', () => {
+  let wrapper
+
+  let propsData = {
+    dt: 'No',
+    dd: 0.337,
+    color: 'hsl(0,50%,35%)',
+    active: true,
+    results: true
+  }
+
+  beforeEach(() => {
+    wrapper = mount(NiFieldVote, { propsData })
+  })
+
+  it('has a color from props', () => {
+    expect(wrapper.vm.color).toContain('hsl(0,50%,35%)')
+  })
+
+  it('has the expected html structure', () => {
+    expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+  })
+
+  it('shows the title of the vote option', () => {
+    expect(wrapper.find('.ni-field-vote-dt').html()).toContain('No')
+  })
+
+  it('has been selected by the user', () => {
+    expect(wrapper.find('.ni-field-vote-input i.material-icons').html())
+      .toContain('radio_button_checked')
+  })
+
+  it('shows the percentage who voted', () => {
+    expect(wrapper.find('.ni-field-vote-dd').html()).toContain('34%')
+  })
+
+})
