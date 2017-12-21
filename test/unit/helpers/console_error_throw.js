f96fbdb9d29d3186ee1799739b2e911af697f95c
Fabian <frznhope@gmail.com>
2017-12-21 17:14:59 +0100

    fixed unhandledRejection handler still being added

--- test/unit/helpers/console_error_throw.js
+++ test/unit/helpers/console_error_throw.js
@@ -2,11 +2,11 @@ global.console.error = (...args) => {
   throw new Error(args.join(' '))
 }
 
-if (!process.UNHANDLED_REJECTION_LISTENER_ADDED) {
+if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
   process.on('unhandledRejection', reason => {
     throw reason
   })
-} else {
-  process.UNHANDLED_REJECTION_LISTENER_ADDED = true
+  // Avoid memory leak by adding too many listeners
+  process.env.LISTENING_TO_UNHANDLED_REJECTION = true
 }
 
