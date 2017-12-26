25ab754a05a3e4cc707b87989613e9ddf3934093
Matt Bell <mappum@gmail.com>
2017-12-26 08:49:21 -0800

    Added NiTabBar spec

---
+++ test/unit/specs/NiTabBar.spec.js
@@ -0,0 +1,18 @@
+import { mount } from 'vue-test-utils'
+import NiTabBar from 'common/NiTabBar'
+
+describe('NiTabBar', () => {
+  let wrapper, close
+
+  beforeEach(() => {
+    wrapper = mount(NiTabBar, {
+      slots: {
+        default: '<main-slot />'
+      }
+    })
+  })
+
+  it('has the expected html structure', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
