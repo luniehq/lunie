4716a05b8435602fb37b6a35c79151110a03eceb
Fabian <frznhope@gmail.com>
2017-12-29 14:33:08 +0100

    update tests

--- test/unit/specs/AppFooter.spec.js
+++ test/unit/specs/AppFooter.spec.js
@@ -1,67 +1,16 @@
-import Vuex from 'vuex'
-import { mount, createLocalVue } from 'vue-test-utils'
-import htmlBeautify from 'html-beautify'
+import setup from '../helpers/vuex-setup'
 import AppFooter from 'renderer/components/common/AppFooter'
-const node = require('renderer/vuex/modules/node').default({
-  node: require('../helpers/node_mock')
-})
-
-const localVue = createLocalVue()
-localVue.use(Vuex)
 
 describe('AppFooter', () => {
-  let wrapper, store
+  let wrapper
+  let instance = setup()
 
-  it('has the expected html structure if connected', () => {
-    store = new Vuex.Store({
-      getters: {
-        nodeIP: () => node.state.nodeIP,
-        connected: () => node.state.connected,
-        lastHeader: () => node.state.lastHeader
-      }
-    })
-
-    node.state = {
-      nodeIP: '192.169.9.5',
-      connected: true,
-      lastHeader: {
-        chain_id: 'gaia-1',
-        height: '4555'
-      }
-    }
-
-    wrapper = mount(AppFooter, {
-      localVue,
-      store
-    })
-
-    expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+  beforeEach(() => {
+    let test = instance.shallow(AppFooter)
+    wrapper = test.wrapper
   })
 
-  it('has the expected html structure if not connected', () => {
-    store = new Vuex.Store({
-      getters: {
-        nodeIP: () => node.state.nodeIP,
-        connected: () => node.state.connected,
-        lastHeader: () => node.state.lastHeader
-      }
-    })
-
-    node.state = {
-      nodeIP: '192.169.9.5',
-      connected: false,
-      lastHeader: {
-        chain_id: 'gaia-1',
-        height: '4555'
-      }
-    }
-
-    wrapper = mount(AppFooter, {
-      localVue,
-      store
-    })
-
-    expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
-    expect(wrapper.contains('.afi-connecting')).toBe(true)
+  it('has the expected html structure if connected', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
   })
 })
