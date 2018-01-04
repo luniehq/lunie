130db6e78fbfd78d964942ee7e03815eca94a108
Fabian <frznhope@gmail.com>
2018-01-04 14:01:26 +0100

    updated and removed packages

--- app/src/renderer/routes.js
+++ app/src/renderer/routes.js
@@ -1,4 +1,4 @@
-function r (type, pageName) { return require(`./components/${type}/Page${pageName}`) }
+function r (type, pageName) { return require(`./components/${type}/Page${pageName}`).default }
 
 let common = r.bind(null, 'common')
 let govern = r.bind(null, 'govern')
