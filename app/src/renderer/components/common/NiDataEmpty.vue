3a580f3233104ad1b9fc8ca5621567183236ca7e
Jordan Bibla <jbibla@gmail.com>
2018-01-08 22:36:57 -0500

    improve NiDataEmpty to allow for props, improve NiListItem to show links

--- app/src/renderer/components/common/NiDataEmpty.vue
+++ app/src/renderer/components/common/NiDataEmpty.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
 data-msg(icon="info_outline")
-  div(slot="title") N/A
-  div(slot="subtitle") No data available yet.
+  h4(v-if="title" slot="title") {{ title }}
+  h4(v-else slot="title") N/A
+
+  h5(v-if="subtitle" slot="subtitle") {{ subtitle }}
+  h5(v-else slot="subtitle") No data available yet.
 </template>
 
 <script>
 import DataMsg from 'common/NiDataMsg'
 export default {
   name: 'ni-data-empty',
+  props: ['title', 'subtitle'],
   components: {
     DataMsg
   }
