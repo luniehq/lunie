63a223a5bd9600faef5cbcb3697e032eee8c9758
Matt Bell <mappum@gmail.com>
2018-01-11 19:16:29 -0800

    Require --binary flag when building, removed gaia build

--- tasks/release.js
+++ tasks/release.js
@@ -21,6 +21,13 @@ process.argv.forEach(function (val) {
   }
 })
 
+if (!binaryPath) {
+  console.error(`\x1b[31mPlease specify a gaia binary for this platform using the "--binary" flag
+    Example: npm run build:darwin -- --binary=./gaia
+    \x1b[0m`)
+  process.exit(1)
+}
+
 if (process.env.PLATFORM_TARGET === 'clean') {
   require('del').sync(['builds/*', '!.gitkeep'])
   console.log('\x1b[33m`builds` directory cleaned.\n\x1b[0m')
@@ -55,9 +62,7 @@ function build () {
   let options = require('../config').building
 
   options.afterCopy = [
-    binaryPath
-      ? copyBinary('gaia', binaryPath)
-      : buildGaiaBinary()
+    copyBinary('gaia', binaryPath)
   ]
   // prune installs the packages
   options.afterPrune = [
@@ -91,46 +96,5 @@ function copyBinary (name, binaryLocation) {
     }
     fs.copySync(binaryLocation, binPath)
     cb()
-  }
-}
-
-const GOARCH = {
-  'x64': 'amd64',
-  'ia32': '386'
-}
-
-function buildGaiaBinary () {
-  return function (buildPath, electronVersion, platform, arch, cb) {
-    if (platform === 'win32') platform = 'windows'
-    if (platform === 'mas') platform = 'darwin'
-    if (GOARCH[arch]) arch = GOARCH[arch]
-
-    console.log(`\x1b[34mBuilding gaia binary (${platform}/${arch})...\n\x1b[0m`)
-
-    let output = 'gaia'
-    if (platform === 'windows') output += '.exe'
-
-    let cmd = `
-      docker run -v "/tmp:/mnt" golang bash -c "
-        go get github.com/cosmos/gaia;
-        cd /go/src/github.com/cosmos/gaia && \
-        git checkout develop && \
-        make get_vendor_deps && \
-        GOOS=${platform} GOARCH=${arch} go build \
-          -o /mnt/${output} \
-          -ldflags '-s -w' \
-          ./cmd/gaia
-      "
-    `
-    let docker = exec(cmd)
-    docker.stdout.on('data', (data) => process.stdout.write(data))
-    docker.stderr.on('data', (data) => process.stderr.write(data))
-    docker.once('exit', (code) => {
-      if (code !== 0) return cb(Error('Build failed'))
-
-      let binPath = join(buildPath, 'bin')
-      mkdirp(binPath)
-      fs.copy(join('/tmp', output), join(binPath, output), cb)
-    })
   }
 }
