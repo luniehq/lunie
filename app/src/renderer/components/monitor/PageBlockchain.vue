017d694d59f8993f75bc6eb4b78dce028c3c395a
Jordan Bibla <jbibla@gmail.com>
2018-01-16 16:15:47 -0500

    subscribed to block explorer index

--- app/src/renderer/components/monitor/PageBlockchain.vue
+++ app/src/renderer/components/monitor/PageBlockchain.vue
@@ -3,10 +3,19 @@ page(title='Blocks')
   div(slot="menu"): tool-bar
   template
     part(title='Latest Block')
-      list-item(dt='Block Height' :dd='num.prettyInt(lastHeader.height)'
-        :to="{ name: 'block', params: { block: lastHeader.height} }")
+      list-item(dt='Block Height' :dd='num.prettyInt(lastHeader.height)' :to="{ name: 'block', params: { block: lastHeader.height} }")
       list-item(dt='Latest Block Time' :dd='latestBlockTime')
       list-item(dt='Latest Block Hash' :dd='status.latest_block_hash')
+
+    part(title='Block Explorer')
+      list-item.column-header(dt="Block Height" dd="# of Transactions")
+      list-item(
+        v-for="block in blocks"
+        :key="block.header.height"
+        :subtitle="block.data.txs.length"
+        :dt="block.header.height"
+        :dd="block.data.txs.length"
+        :to="{ name: 'block', params: { block: block.header.height} }")
 </template>
 
 <script>
@@ -34,11 +43,23 @@ export default {
     },
     latestBlockTime () {
       return moment(this.status.latest_block_time).format('MMMM Do YYYY — hh:mm:ss')
+    },
+    blocks () {
+      return this.blockchain.blocks
     }
-  },
+   },
   data: () => ({
     moment: moment,
     num: num
   })
 }
 </script>
+<style lang="stylus">
+@require '~variables'
+
+.column-header
+  .ni-li-dd,
+  .ni-li-dt
+    font-size sm
+    color dim
+</style>
