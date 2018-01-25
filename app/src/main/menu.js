f424cd66bef5a98df6098c633349c0a125f083f6
Matt Bell <mappum@gmail.com>
2018-01-25 07:51:58 -0800

    Fix lint

--- app/src/main/menu.js
+++ app/src/main/menu.js
@@ -15,7 +15,7 @@ module.exports = function () {
       submenu: [
         { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
         { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' }
       ]
     }
   ]
