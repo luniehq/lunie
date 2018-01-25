5fdc4b31f3161c0acb9452189918edb9e340cae2
Matt Bell <mappum@gmail.com>
2018-01-25 04:17:22 -0800

    Added notification component to loading session screen

--- app/src/renderer/components/common/NiSessionLoading.vue
+++ app/src/renderer/components/common/NiSessionLoading.vue
@@ -2,12 +2,18 @@
 .ni-session: .ni-session-container
   .ni-session-header
     .ni-session-title Loading&hellip;
-  .ni-session-main &nbsp;
+  .ni-session-main &nbsp
   .ni-session-footer &nbsp;
+  notifications(:notifications='notifications' theme='cosmos')
 </template>
 
 <script>
+import Notifications from '@nylira/vue-notifications'
+import { mapGetters } from 'vuex'
+import store from '../../vuex/store'
 export default {
-  name: 'ni-session-loading'
+  name: 'ni-session-loading',
+  components: { Notifications },
+  computed: mapGetters(['notifications'])
 }
 </script>
