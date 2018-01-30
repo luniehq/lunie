706d16bcca64594389fb3647d6daf941373389d6
Peng Zhong <peng@nylira.com>
2018-01-30 13:06:24 +0800

    fix test

--- test/unit/specs/components/monitor/PageBlocks.spec.js
+++ test/unit/specs/components/monitor/PageBlocks.spec.js
@@ -1,21 +1,19 @@
 import setup from '../../../helpers/vuex-setup'
 import htmlBeautify from 'html-beautify'
 import PageBlocks from 'renderer/components/monitor/PageBlocks'
-import moment from 'moment-timezone'
 
 describe('PageBlocks', () => {
   let wrapper, store
   let {mount} = setup()
 
   beforeEach(() => {
-    moment.tz.setDefault('EST')
     let instance = mount(PageBlocks, {
       stubs: {
         'modal-search': '<modal-search />'
       },
       getters: {
         lastHeader: () => ({
-          time: moment('2017-09-15 09:30:00').valueOf(),
+          time: 0,
           last_block_id: {
             hash: '123'
           },
