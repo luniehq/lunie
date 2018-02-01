c05707d0ace50722f2caf952fb4c33b87739e838
Jordan Bibla <jbibla@gmail.com>
2018-02-01 11:57:49 -0500

    added test

--- test/unit/specs/components/monitor/PageBlock.spec.js
+++ test/unit/specs/components/monitor/PageBlock.spec.js
@@ -2,18 +2,33 @@ import setup from '../../../helpers/vuex-setup'
 import htmlBeautify from 'html-beautify'
 import PageBlock from 'renderer/components/monitor/PageBlock'
 
+const $route = {
+  params: {
+    block: 1234
+  }
+}
+
 describe('PageBlock', () => {
-  let wrapper, instance
+  let wrapper, store
   let {mount} = setup()
 
   beforeEach(() => {
-    instance = mount(PageBlock)
+    let instance = mount(PageBlock, {
+      mocks: {
+        $route
+      }
+    })
     wrapper = instance.wrapper
+    store = instance.store
 
     wrapper.update()
   })
 
   it('has the expected html structure', () => {
     expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+  })
+
+  it('should dispatch getBlock when mounted', () => {
+    expect(store.dispatch).toHaveBeenCalledWith('getBlock', wrapper.vm.$route.params.block)
   })
 })
