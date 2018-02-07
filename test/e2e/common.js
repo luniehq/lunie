70f62298607edf1a59b08ab04c2a5e3e28f1ed04
Fabian <frznhope@gmail.com>
2018-02-07 19:22:55 +0000

    improved async behaviour of navigate

--- test/e2e/common.js
+++ test/e2e/common.js
@@ -13,22 +13,19 @@ module.exports = {
     } 
     // open menu 
     await client.$('#app-menu-button').click() 
-    t.test(await client.$('.app-menu').isVisible(), 'menu opened') 
+    await client.$('.app-menu').isVisible()
     // click link 
-    t.test(`navigate to "${linkText}"`, async function (t) {
-      await client.$(`a*=${linkText}`).click()
-      await client.waitUntilTextExists('.ni-page-header-title', titleText)
-      t.pass(`navigated to "${linkText}"`)
-      t.end()
-    })
+    await client.$(`a*=${linkText}`).click()
+    await client.waitUntilTextExists('.ni-page-header-title', titleText)
+    console.log(`navigated to "${linkText}"`)
   },
   newTempDir () {
     return join(tmpdir(), Math.random().toString(36).slice(2))
   },
   sleep,
-  async waitForText (el, text, timeout = 5000) {
+  async waitForText (elGetterFn, text, timeout = 5000) {
     let start = Date.now()
-    while (await el().getText() !== text) {
+    while (await elGetterFn().getText() !== text) {
       if (Date.now() - start >= timeout) {
         throw Error('Timed out waiting for text')
       }
