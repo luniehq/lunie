9cd7568c7897101d9fd6e7cbd4b3a8e27eb76b05
Fabian <frznhope@gmail.com>
2018-02-07 14:34:15 +0000

    fix launching of app

--- test/e2e/launch.js
+++ test/e2e/launch.js
@@ -4,53 +4,78 @@ let { Application } = require('spectron')
 let test = require('tape-promise/tape')
 let electron = require('electron')
 let { join } = require('path')
+let { spawn } = require('child_process')
 let { newTempDir } = require('./common.js')
 
 // re-use app instance
-let app
+let app, home, cliHome, started
+let binary = process.env.BINARY_PATH 
 
-module.exports = async function launch (t) {
-  if (app) return app
+module.exports = function launch (t) {
+  if (!started) {
+    started = new Promise(async (resolve, reject) => {
+      console.log('using binary', binary) 
+    
+      // TODO cleanup 
+      home = newTempDir() 
+      cliHome = join(newTempDir(), 'baseserver') 
+      console.error(`ui home: ${home}`) 
+      console.error(`node home: ${cliHome}`) 
+    
+      await startLocalNode()
+    
+      app = new Application({
+        path: electron,
+        args: [
+          join(__dirname, '../../app/dist/main.js'),
+          '--disable-gpu',
+          '--no-sandbox'
+        ],
+        startTimeout: 10000,
+        waitTimeout: 10000,
+        env: {
+            // COSMOS_UI_ONLY: 'true', 
+            // COSMOS_TEST: 'true', 
+            COSMOS_NODE: 'localhost', 
+            NODE_ENV: 'production', 
+            PREVIEW: 'true', 
+            COSMOS_DEVTOOLS: 0, // open devtools will cause issues with spectron, you can open them later manually 
+            COSMOS_HOME: home, 
+            COSMOS_NETWORK: 'test/e2e/localtestnet' 
+        }
+      })
 
-  let home = newTempDir()
-  console.error(`ui home: ${home}`)
+      await startApp(app)
+      t.ok(app.isRunning(), 'app is running')
 
-  app = new Application({
-    path: electron,
-    args: [
-      join(__dirname, '../../app/dist/main.js'),
-      '--disable-gpu',
-      '--no-sandbox'
-    ],
-    startTimeout: 10000,
-    waitTimeout: 10000,
-    env: {
-      COSMOS_UI_ONLY: 'true',
-      COSMOS_TEST: 'true',
-      COSMOS_HOME: home,
-      COSMOS_NETWORK: join(__dirname, 'gaia-1')
-    }
-  })
+      console.log('stopping app to test consecutive run')
+      await app.stop()
 
-  await app.start()
+      await new Promise((resolve, reject) => {
+        let child = spawn(binary, [
+          'client', 'keys', 'recover', 'testkey',
+          '--home', join(home, 'baseserver')
+        ])
+        child.stdin.write('1234567890\n')
+        child.stdin.write('chair govern physical divorce tape movie slam field gloom process pen universe allow pyramid private ability\n')
+        child.stderr.pipe(process.stdout)
+        child.once('exit', (code) => {
+          if (code === 0) resolve()
+          reject()
+        })
+      })
+      console.log('restored test account')
 
-  t.test('launch app', function (t) {
-    t.ok(app.isRunning(), 'app is running')
-    t.end()
-  })
+      await startApp(app)
+      t.ok(app.isRunning(), 'app is running')
 
-  t.test('wait for app to load', async function (t) {
-    await app.client.waitForExist('.header-item-logo', 5000)
-    .then(() => t.pass('app loaded'))
-    .catch(e => {
-      printAppLog(app)
-      t.fail()
-      throw e
+      await login(app)
+
+      resolve(app)
     })
-    t.end()
-  })
-
-  return app
+  }
+  
+  return started
 }
 
 test.onFinish(() => app ? app.stop() : null)
@@ -68,4 +93,56 @@ function printAppLog (app) {
       console.log(log.level)
     })
   })
+}
+
+async function login (app) {
+  await app.client.$('#sign-in-password').setValue('1234567890')
+  await app.client.$('.ni-session-footer button').click()
+  await app.client.waitForExist('#app-content', 5000)
+}
+
+async function startApp (app) {
+  await app.start()
+
+  await app.client.waitForExist('.ni-session', 5000)
+  .catch(e => {
+    printAppLog(app)
+    throw e
+  })
+} 
+
+async function startLocalNode () {
+  await new Promise((resolve, reject) => {
+    let child = spawn(binary, [ 
+      'node', 'init', 
+      '3F52AFC4FB737A0296EFE331885FCC476980B3BD', 
+      '--home', cliHome, 
+      '--chain-id'  , 'localtestnet' 
+    ]) 
+    child.once('exit', (code) => { 
+      if (code === 0) resolve()
+      reject()
+    }) 
+  })
+  console.log('inited local node')
+
+  await new Promise((resolve, reject) => {
+    // TODO cleanup
+    let localnodeProcess = spawn(binary, [ 
+      'node', 'start', 
+      '--home', cliHome 
+    ])
+    localnodeProcess.stderr.pipe(process.stderr) 
+    localnodeProcess.stdout.once('data', data => { 
+      let msg = data.toString()
+      if (!msg.includes('Failed') && !msg.includes('Error')) {
+        resolve()
+      }
+      reject()
+    }) 
+    localnodeProcess.once('exit', (code) => { 
+      reject()
+    })
+  })
+  console.log('started local node')
 }
