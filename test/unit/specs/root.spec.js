e9c6ee5c7c91e95edb8d5af3ae387c841806679d
Matt Bell <mappum@gmail.com>
2018-02-12 08:31:40 -0800

    Added root path test

---
+++ test/unit/specs/root.spec.js
@@ -0,0 +1,15 @@
+const { join } = require('path')
+const { homedir } = require('os')
+
+describe('Root UI Directory', () => {
+  Object.assign(process.env, {
+    NODE_ENV: 'development',
+    COSMOS_NETWORK: 'app/networks/gaia-2',
+    COSMOS_HOME: ''
+  })
+
+  it('should create the correct path', () => {
+    let root = require('../../../app/src/root.js')
+    expect(root).toBe(join(homedir(), '.cosmos-dev/gaia-2'))
+  })
+})
