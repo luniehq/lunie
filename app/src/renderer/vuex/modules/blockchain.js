82c48007269fc054767f6d17d5b632b52fb3778d
Fabian Weber <frznhope@gmail.com>
2018-02-23 15:07:27 +0100

    refactored block metas into blockchain + added tests

--- app/src/renderer/vuex/modules/blockchain.js
+++ app/src/renderer/vuex/modules/blockchain.js
@@ -6,7 +6,8 @@ export default ({ commit, node }) => {
       block_id: {}
     },
     blockHeight: null, // we remember the height so we can requery the block, if querying failed
-    blockLoading: false
+    blockLoading: false,
+    blockMetas: []
   }
 
   const mutations = {
@@ -24,50 +25,65 @@ export default ({ commit, node }) => {
         dispatch('getBlock', state.blockHeight)
       }
     },
-    async getBlock ({ state, commit }, height) {
+    async getBlock ({ state, commit, dispatch }, height) {
       state.blockLoading = true
       state.blockHeight = height
-      Promise.all(
-        new Promise(resolve => {
-          node.rpc.block({ minHeight: height, maxHeight: height }, (err, data) => {
-            if (err) {
-              commit('notifyError', {title: `Couldn't query block`, body: err.message})
-              resolve({})
-            } else {
-              resolve(data.block)
-            }
-          })
-        }).then(block => commit('setBlock', block)),
-        new Promise((resolve, reject) => {
-          node.rpc.blockchain({ minHeight: height, maxHeight: height }, (err, data) => {
-            if (err) {
-              commit('notifyError', {title: `Couldn't query block`, body: err.message})
-              reject({block_id: {}})
-            } else {
-              resolve(data.block_metas[0])
-            }
-          })
-        }).then(blockMetaInfo => commit('setBlockMetaInfo', blockMetaInfo))
+      return Promise.all(
+        dispatch('queryBlock', height)
+        .then(block => commit('setBlock', block)),
+        dispatch('queryBlockInfo', height)
+        .then(blockMetaInfo => commit('setBlockMetaInfo', blockMetaInfo))
       ).then(() => {
         state.blockLoading = false
       }, () => {
         state.blockLoading = false
       })
+    },
+    async queryBlock ({ state, commit }, height) {
+      return new Promise(resolve => {
+        node.rpc.block({ minHeight: height, maxHeight: height }, (err, data) => {
+          if (err) {
+            commit('notifyError', {title: `Couldn't query block`, body: err.message})
+            resolve({})
+          } else {
+            resolve(data.block)
+          }
+        })
+      })
+    },
+    async queryBlockInfo ({ state, commit }, height) {
+      let blockMetaInfo = state.blockMetas.find(b => b.header.height === height)
+      if (blockMetaInfo) {
+        return blockMetaInfo
+      }
+      blockMetaInfo = await new Promise((resolve, reject) => {
+        node.rpc.blockchain({ minHeight: height, maxHeight: height }, (err, data) => {
+          if (err) {
+            commit('notifyError', {title: `Couldn't query block`, body: err.message})
+            resolve(null)
+          } else {
+            resolve(data.block_metas[0])
+          }
+        })
+      })
+      blockMetaInfo && state.blockMetas.push(blockMetaInfo)
+      return blockMetaInfo
+    },
+    subscribeToBlocks ({commit}) {
+      node.rpc.subscribe({ query: "tm.event = 'NewBlock'" }, (err, event) => {
+        if (err) {
+          commit('notifyError', {title: `Error subscribing to new blocks`, body: err.message})
+          return
+        }
+  
+        state.blocks.unshift(event.data.data.block)
+  
+        if (state.blocks.length === 20) {
+          state.blocks.pop()
+        }
+      })
     }
   }
-
-  function subscribe () {
-    node.rpc.subscribe({ query: "tm.event = 'NewBlock'" }, (err, event) => {
-      if (err) return console.error('error subscribing to new block headers', err)
-
-      state.blocks.unshift(event.data.data.block)
-
-      if (state.blocks.length === 20) {
-        state.blocks.pop()
-      }
-    })
-  }
-  subscribe()
 
   return { state, mutations, actions }
 }
