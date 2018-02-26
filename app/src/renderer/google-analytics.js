2f281a91c7a4618de191efdc30319fb2b3f8edcd
Fabian Weber <frznhope@gmail.com>
2018-02-26 14:43:47 +0100

    switched to a google analytics plugin

--- app/src/renderer/google-analytics.js
+++ app/src/renderer/google-analytics.js
@@ -1,14 +1,7 @@
-module.exports = function (gaUID) {
-  window.onload = function () {
-    let gaLoadScript = document.createElement('script')
-    gaLoadScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + gaUID
-    document.body.appendChild(gaLoadScript)
+import Analytics from 'electron-ga'
 
-    window.dataLayer = window.dataLayer || []
-    function gtag () {
-      window.dataLayer.push(arguments)
-    }
-    gtag('js', new Date())
-    gtag('config', gaUID)
-  }
+module.exports = function (gaUID) {
+  const analytics = new Analytics(gaUID)
+  window.analytics = analytics
+  analytics.send('screenview', { cd: 'Login' })
 }
