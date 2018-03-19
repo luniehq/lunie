be4f855b2a7a1b5723d60f56afbecd11e32c94b6
Fabian Weber <frznhope@gmail.com>
2018-03-19 13:38:25 +0100

    fixed blocks in rpc mocks

--- app/src/renderer/connectors/rpcWrapperMock.js
+++ app/src/renderer/connectors/rpcWrapperMock.js
@@ -1,13 +1,21 @@
 const mockValidators = require('../../helpers/json/mock_validators.json')
 
-let state = { blockMetas: createBlockMetas() }
+let state = { blockMetas: [], blocks: [] }
+createBlockMetas(state)
 
 const RpcClientMock = {
   on: () => {},
-  subscribe: () => {},
+  subscribe: (args, cb) => {
+    if (args.query === "tm.event = 'NewBlock'") {
+      produceBlocks(cb)
+    }
+    if (args.query === "tm.event = 'NewBlockHeader'") {
+      produceBlockHeaders(cb)
+    }
+  },
   validators: () => mockValidators,
-  block: (args, cb) => cb({}),
-  blockchain: ({ minHeight, maxHeight }, cb) => cb(null, { block_metas: state.blockMetas.filter(b => b.header.height === minHeight) }),
+  block: ({ minHeight, maxHeight }, cb) => cb(null, { block: state.blocks.find(b => b.header.height === minHeight) }),
+  blockchain: ({ minHeight, maxHeight }, cb) => cb(null, { block_metas: state.blockMetas.filter(b => b.height === minHeight) }),
   status: (cb) => cb(null, {
     latest_block_height: 42,
     node_info: { network: 'mock-chain' }
@@ -34,18 +42,76 @@ module.exports = function setRPCWrapperMock (container) {
   return rpcWrapper
 }
 
-function createBlockMetas () {
+function createBlockMeta (time, height) {
+  return {
+    header: { time, height },
+    block_id: { hash: makeBlockHash() },
+    height,
+    chain_id: 'mock-chain',
+    last_block_id: { hash: makeBlockHash() }
+  }
+}
+
+function createBlock (height) {
+  return {
+    hash: makeBlockHash(),
+    header: {
+      height,
+      chain_id: 'mock-chain',
+      last_block_id: {
+        hash: makeBlockHash(),
+        parts: { total: 0, hash: makeBlockHash() }
+      },
+      num_txs: 0,
+      last_commit_hash: makeBlockHash(),
+      validators_hash: makeBlockHash(),
+      app_hash: makeBlockHash()
+    },
+    last_commit: { precommits: [] },
+    data: { txs: [] }
+  }
+}
+
+function createBlockMetas (state) {
   let now = new Date()
-  let blockMetas = (new Array(200)).fill(null).map((_, i) => {
+  new Array(200).fill(null).forEach((_, i) => {
     let time = new Date(now)
     time.setMinutes(time.getMinutes() - i)
-    return {
-      header: {
-        height: 200 - i,
-        time
-      }
-    }
-  })
 
-  return blockMetas
+    state.blockMetas.push(createBlockMeta(time, i))
+    state.blocks.push(createBlock(i))
+  })
+}
+
+async function produceBlockHeaders (cb) {
+  let height = 200
+  while (true) {
+    let newBlockHeader = createBlockMeta(Date.now(), ++height)
+    state.blockMetas.push(newBlockHeader)
+    cb(null, { data: { data: { header: newBlockHeader } } })
+    await sleep(1000)
+  }
+}
+
+async function produceBlocks (cb) {
+  let height = 200
+  while (true) {
+    let newBlock = createBlock(++height)
+    state.blocks.push(newBlock)
+    cb(null, { data: { data: { block: newBlock } } })
+    await sleep(1000)
+  }
+}
+
+function makeBlockHash () {
+  var text = ''
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+  for (var i = 0; i < 40; i++) { text += possible.charAt(Math.floor(Math.random() * possible.length)) }
+
+  return text
+}
+
+function sleep (ms = 0) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
