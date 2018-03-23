3b7d14231c784644edfe00a1d212ccc44198ad3a
Fabian Weber <frznhope@gmail.com>
2018-03-23 17:26:51 +0100

    changed let to const

--- tasks/testnet.js
+++ tasks/testnet.js
@@ -15,30 +15,30 @@ async function get (url) {
 }
 
 async function main () {
-  let network = process.argv[2] || 'gaia-2'
+  const network = process.argv[2] || 'gaia-2'
 
   // fetch genesis.json and config.toml from github testnets repo,
   // save to tmp dir and pass to app dev runner
   console.log(`fetching genesis for network "${network}"`)
-  let genesisJson = await get(`https://github.com/tendermint/testnets/raw/master/${network}/gaia/genesis.json`)
+  const genesisJson = await get(`https://github.com/tendermint/testnets/raw/master/${network}/gaia/genesis.json`)
     .catch(e => {
       throw new Error(`Can't load genesis.json: ${e.message}`)
     })
-  let configToml = await get(`https://github.com/tendermint/testnets/raw/master/${network}/gaia/config.toml`)
+  const configToml = await get(`https://github.com/tendermint/testnets/raw/master/${network}/gaia/config.toml`)
     .catch(e => {
       throw new Error(`Can't load config.toml: ${e.message}`)
     })
-  let gaiaVersionTxt = await get(`https://github.com/tendermint/testnets/raw/master/${network}/gaia/gaiaversion.txt`)
+  const gaiaVersionTxt = await get(`https://github.com/tendermint/testnets/raw/master/${network}/gaia/gaiaversion.txt`)
     .catch(e => {
       throw new Error(`Can't load config.toml: ${e.message}`)
     })
-  let path = join(tmpdir(), Math.random().toString(36).slice(2))
+  const path = join(tmpdir(), Math.random().toString(36).slice(2))
   mkdirp(path)
   write(join(path, 'genesis.json'), genesisJson)
   write(join(path, 'config.toml'), configToml)
   write(join(path, 'gaiaversion.txt'), gaiaVersionTxt)
 
-  // run Voyager in a developmnet environment
+  // run Voyager in a development environment
   runDev(path)
 }
 
