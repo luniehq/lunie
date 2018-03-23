3999299c059ad144de4320d8ed46ca4c7bb6367a
David Braun <David@Tendermint.com>
2018-03-23 17:55:51 -0400

    Configure use of Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,19 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
+    ecmaVersion: 2017,
     sourceType: 'module'
   },
   env: {
     browser: true,
+    es6: true,
     node: true
   },
-  extends: 'standard',
   plugins: [
     'html'
   ],
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // unify spacing in objects
-    'object-curly-spacing': ["error", "always"],
-    'object-curly-newline': ["error", {
-      "ObjectExpression": { "multiline": true, "minProperties": 3 },
-      "ImportDeclaration": "never"
-    }]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
