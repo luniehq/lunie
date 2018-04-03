39e14566b0724bef72c050d85318a56e6b222387
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-04-03 23:09:26 +0100

    start implementing dark/light switch

--- app/src/renderer/components/common/PageProfile.vue
+++ app/src/renderer/components/common/PageProfile.vue
@@ -59,10 +59,30 @@ export default {
       }
     },
     setAppThemeLight () {
+      this.setCssVar('app-fg', '#eee')
       this.setCssVar('app-bg', '#fff')
+      this.setCssVar('app-bg-alpha', 'a(233, 36%, 13%, 95%)')
+      this.setCssVar('bright', '#000')
+      this.setCssVar('txt', '#333')
+      this.setCssVar('dim', '#666')
+      this.setCssVar('bc', '#ddd')
+      this.setCssVar('bc-dim', '#eee')
+      this.setCssVar('hover-bg', 'hsl(233, 43%, 10%)')
+      this.setCssVar('input-bc', 'hsl(233, 22%, 33%)')
+      this.setCssVar('input-bc-hover', 'hsl(233, 22%, 40%)')
     },
     setAppThemeDark () {
-      this.setCssVar('app-bg', '#000')
+      this.setCssVar('app-fg', 'hsl(233, 33%, 16%)')
+      this.setCssVar('app-bg', 'hsl(233, 36%, 13%)')
+      this.setCssVar('app-bg-alpha', 'hsla(233, 36%, 13%, 95%)')
+      this.setCssVar('bright', '#fff')
+      this.setCssVar('txt', 'hsl(233, 13%, 85%)')
+      this.setCssVar('dim', 'hsl(233, 13%, 60%)')
+      this.setCssVar('bc', 'hsl(233, 22%, 23%)')
+      this.setCssVar('bc-dim', 'hsl(233, 33%, 16%)')
+      this.setCssVar('hover-bg', 'hsl(233, 43%, 10%)')
+      this.setCssVar('input-bc', 'hsl(233, 22%, 33%)')
+      this.setCssVar('input-bc-hover', 'hsl(233, 22%, 40%)')
     },
     setCssVar (key, value) {
       document.documentElement.style.setProperty(`--${key}`, value)
