914817d9831fe0c426949d4d97e5a6d886310bbc
Fabian <frznhope@gmail.com>
2018-04-03 14:02:13 +0200

    added tests for approval modal

---
+++ test/unit/specs/components/common/NiModalLCDApproval.spec.js
@@ -0,0 +1,33 @@
+import setup from '../../../helpers/vuex-setup'
+import NiModalLCDApproval from 'common/NiModalLCDApproval'
+
+let instance = setup()
+
+describe('NiModalLCDApproval', () => {
+  let wrapper, store
+
+  beforeEach(() => {
+    let test = instance.mount(NiModalLCDApproval, { propsData: { hash: 'this_is_a_hash' } })
+    wrapper = test.wrapper
+    store = test.store
+  })
+
+  it('has the expected html structure', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+
+  it('shows the hash', () => {
+    expect(wrapper.vm.hash).toBe('this_is_a_hash')
+    expect(wrapper.vm.$el.querySelector('textarea').value).toBe('this_is_a_hash')
+  })
+
+  it('allows to approve a hash', () => {
+    wrapper.vm.approve()
+    expect(store.dispatch).toHaveBeenCalledWith('approveNodeHash', 'this_is_a_hash')
+  })
+
+  it('allows to disapprove a hash', () => {
+    wrapper.vm.newNode()
+    expect(store.dispatch).toHaveBeenCalledWith('disapproveNodeHash', 'this_is_a_hash')
+  })
+})
