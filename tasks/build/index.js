42b4e3f67ba4a3c35ab7f77850a5c986dc47e7eb
David Braun <David@Tendermint.com>
2018-04-04 19:24:57 -0400

    Allow specification of default network for production build.

--- tasks/build/index.js
+++ tasks/build/index.js
@@ -1,36 +1,56 @@
 "use strict"
 
 const { cli } = require(`@nodeguy/cli`)
-const options = require(`./options.json`)
+const fp = require(`lodash/fp`)
+const optionsSpecification = require(`./optionsSpecification.json`)
 const path = require(`path`)
 const shell = require(`shelljs`)
 const untildify = require(`untildify`)
 
-cli(options, async ({ commit, gaia, platform, "skip-pack": skipPack }) => {
+cli(optionsSpecification, options => {
+  const { commit, gaia, network } = options
+
   shell.exec(`docker build --tag cosmos/voyager-builder .`, {
     cwd: __dirname
   })
 
-  const builds = path.resolve(__dirname, "../../builds")
-  shell.mkdir(`-p`, builds)
+  // Expand '~' if preset and resolve to absolute pathnames for Docker.
+  const resolved = fp.mapValues(fp.pipe(untildify, path.resolve), {
+    gaia,
+    git: path.join(__dirname, "../../.git"),
+    network,
+    builds: path.join(__dirname, "../../builds")
+  })
 
-  const resolved = {
-    gaia: parsePath(path.resolve(untildify(gaia))),
-    git: parsePath(path.resolve(__dirname, "../../.git")),
-    builds: parsePath(builds)
-  }
+  // Make the 'builds' directory if not present.
+  shell.mkdir(`-p`, resolved.builds)
 
+  // Override the gaia option before passing the options into the container.
+  const nextOptions = Object.assign({}, options, {
+    gaia: `/mnt/gaia`
+  })
+
+  const nextOptionsString = Object.entries(nextOptions)
+    .map(([key, value]) => `--${key}=${value}`)
+    .join(` `)
+
+  // inputs: The Gaia binary and the .git directory.
+  // inputs:
+  //   gaia
+  //   .git/
+  //   default network
+  //
+  // output: the builds directory
   shell.exec(`docker run \
       --interactive \
       --mount type=bind,readonly,source=${resolved.gaia},target=/mnt/gaia \
       --mount type=bind,readonly,source=${resolved.git},target=/mnt/.git \
+      --mount type=bind,readonly,source=${
+        resolved.network
+      },target=/mnt/network \
       --mount type=bind,source=${resolved.builds},target=/mnt/builds \
       --rm \
-      cosmos/voyager-builder \
-        "${commit}" \
-        --gaia=/mnt/gaia \
-        --platform=${platform} \
-        --skip-pack=${skipPack}
+      cosmos/voyager-builder "${commit}" ${nextOptionsString}
   `)
 })
 
