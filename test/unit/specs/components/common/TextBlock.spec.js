0236623eebfeb72df928e8579a5f82b570bbf191
Fabian Weber <frznhope@gmail.com>
2018-04-05 11:30:03 +0200

    increase coverage

---
+++ test/unit/specs/components/common/TextBlock.spec.js
@@ -0,0 +1,18 @@
+import { mount } from "@vue/test-utils"
+import TextBlock from "common/TextBlock"
+
+describe("TextBlock", () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(TextBlock, {propsData: {
+      content: `## Hello
+      ### Worl`,
+      author: 'faboweb'
+    }})
+  })
+
+  it("has the expected html structure", () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
