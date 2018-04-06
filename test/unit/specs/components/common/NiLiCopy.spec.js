617bc51ff35e04126bb633a3a1594d27125636d6
Jordan Bibla <jbibla@gmail.com>
2018-04-06 16:01:07 -0400

    unit tests for NiLiCopy and NiModalReceive

--- test/unit/specs/components/common/NiLiCopy.spec.js
+++ test/unit/specs/components/common/NiLiCopy.spec.js
@@ -1,11 +1,15 @@
-import { mount } from "@vue/test-utils"
+import setup from "../../../helpers/vuex-setup"
 import NiLiCopy from "common/NiLiCopy"
 
 describe("NiLiCopy", () => {
-  let wrapper
+  let wrapper, store
+  let { mount } = setup()
 
   beforeEach(() => {
-    wrapper = mount(NiLiCopy)
+    let instance = mount(NiLiCopy)
+    wrapper = instance.wrapper
+    store = instance.store
+    store.commit("setModalReceive", true)
   })
 
   it("has the expected html structure", () => {
@@ -15,5 +19,16 @@ describe("NiLiCopy", () => {
   it("should show the provided value", () => {
     wrapper.setProps({ value: "HALLO" })
     expect(wrapper.html()).toContain("HALLO")
+  })
+
+  it("should show the receive button", () => {
+    wrapper.setProps({ value: "HALLO", receive: true })
+    expect(wrapper.html()).toContain("Receive")
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+
+  it("should commit the receive modal", () => {
+    wrapper.vm.enableModalReceive()
+    expect(store.commit).toHaveBeenCalledWith("setModalReceive", true)
   })
 })
