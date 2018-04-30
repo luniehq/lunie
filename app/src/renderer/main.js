796b6fbb79c6ffaf25d33fb829fdd97261e515c6
Matt Bell <mappum@gmail.com>
2018-04-30 16:19:58 -0700

    Get enable renderer mock option based on config

--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -12,6 +12,7 @@ import App from "./App"
 import routes from "./routes"
 import Node from "./connectors/node"
 import Store from "./vuex/store"
+import config from "../config"
 
 // exporting this for testing
 let store
@@ -42,7 +43,7 @@ Vue.use(Vuelidate)
 async function main() {
   let lcdPort = getQueryParameter("lcd_port")
   console.log("Expecting lcd-server on port:", lcdPort)
-  node = Node(lcdPort, true)
+  node = Node(lcdPort, config.mocked)
 
   const router = new Router({
     scrollBehavior: () => ({ y: 0 }),
