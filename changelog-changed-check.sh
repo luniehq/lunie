81dc4dd3a7011081d72b78fb2d55d3ecea83f920
Fabian <frznhope@gmail.com>
2018-05-16 17:29:09 +0200

    using circle branch

--- changelog-changed-check.sh
+++ changelog-changed-check.sh
@@ -1,3 +1,4 @@
+branch=$CIRCLE_BRANCH || "$(git rev-parse --abbrev-ref HEAD)"
 if [ "$(git diff --name-only develop $(git rev-parse --abbrev-ref HEAD) | grep -c CHANGELOG.md)" -ge 1 ]; then
     echo "CHANGELOG updated"
     exit 0;
