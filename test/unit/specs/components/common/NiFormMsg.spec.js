a467dd0a65e94707e922e4b438326c242eb37437
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-05-29 10:47:32 +0200

    more tests

--- test/unit/specs/components/common/NiFormMsg.spec.js
+++ test/unit/specs/components/common/NiFormMsg.spec.js
@@ -4,18 +4,37 @@ import NiFormMsg from "common/NiFormMsg"
 
 describe("NiFormMsg", () => {
   let wrapper
-
-  let propsData = {
-    type: "length",
-    body: "",
-    name: "Password",
-    min: 16,
-    max: 255,
-    length: ""
-  }
+  let propsData = [
+    {
+      type: "length",
+      body: "",
+      name: "Password",
+      min: 16,
+      max: 255,
+      length: ""
+    },
+    {
+      type: "words16",
+      name: "Seed"
+    },
+    {
+      type: "alphaNum",
+      name: "Asdf"
+    },
+    {
+      type: "asdf",
+      name: "Asdf"
+    }
+  ]
+  let propsErrors = [
+    "Password must be between 16 and 255 characters",
+    "Seed phrase must be 16 words",
+    "Asdf must contain only alphanumeric characters",
+    "Asdf must be valid"
+  ]
 
   beforeEach(() => {
-    wrapper = mount(NiFormMsg, { propsData })
+    wrapper = mount(NiFormMsg, { propsData: propsData[0] })
   })
 
   it("has a type from props", () => {
@@ -32,6 +51,16 @@ describe("NiFormMsg", () => {
         .find(".ni-form-msg")
         .text()
         .trim()
-    ).toContain("Password must be between 16 and 255 characters")
+    ).toContain(propsErrors[0])
+
+    for (let i = 1; i < propsData.length; i++) {
+      wrapper.setData(propsData[i])
+      expect(
+        wrapper
+          .find(".ni-form-msg")
+          .text()
+          .trim()
+      ).toContain(propsErrors[i])
+    }
   })
 })
