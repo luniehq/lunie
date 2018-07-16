98cb67602aefa3ceeb77a202d0f03268042d1915
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-07-16 12:10:44 +0200

    more debug

--- test/unit/helpers/fixed_time.js
+++ test/unit/helpers/fixed_time.js
@@ -3,7 +3,7 @@ require("moment")
 const mockMomentTz = require("moment-timezone")
 jest.mock("moment", () => time => mockMomentTz(time).tz("Europe/Berlin"))
 
-const DATE_TO_USE = new Date(1608)
+const DATE_TO_USE = new Date(Date.UTC(1988, 0, 5, 8, 0, 0))
 global._Date = Date
 console.log("1 - date says", DATE_TO_USE.toISOString())
 global.Date = jest.fn(() => new global._Date(DATE_TO_USE.toISOString()))
