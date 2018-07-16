69daa289f52f720067163d3fb4db14fd72e2ad3a
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-07-16 16:18:36 +0200

    tm updates remove commments

--- app/src/renderer/components/monitor/PageBlock.vue
+++ app/src/renderer/components/monitor/PageBlock.vue
@@ -45,20 +45,6 @@ export default {
     block() {
       return this.blockchain.block
     },
-    blockHeaderTime() {
-      if (this.block.header) {
-        console.log("1 - moment", moment)
-        console.log("1 - now", moment().format())
-        console.log("1 - time", this.block.header.time)
-        console.log(
-          moment(this.block.header.time).format("MMMM Do YYYY — hh:mm:ss")
-        )
-
-        return moment(this.block.header.time).format("MMMM Do YYYY — hh:mm:ss")
-      } else {
-        return "Loading..."
-      }
-    },
     blockMeta() {
       return this.blockchain.blockMetaInfo
     },
--- test/unit/helpers/fixed_time.js
+++ test/unit/helpers/fixed_time.js
@@ -6,8 +6,5 @@ jest.mock("moment", () => time => mockMomentTz(time).tz("Etc/UTC"))
 const DATE_TO_USE = new Date(Date.UTC(1970, 0, 1, 0, 0, 0))
 global._Date = Date
 console.log("1 - date says", DATE_TO_USE.toISOString())
-global.Date = jest.fn(() => {
-  // console.log("1 - calling mocked date")
-  return new global._Date(DATE_TO_USE.toISOString())
-})
+global.Date = jest.fn(() => new global._Date(DATE_TO_USE.toISOString()))
 Date.now = jest.genMockFunction().mockReturnValue(DATE_TO_USE.getTime())
