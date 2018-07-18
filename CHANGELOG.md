4e454cce80d698477e21d03a3b4b6955cf252c8e
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-07-18 10:16:29 +0200

    added back time

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -23,7 +23,6 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 * Moved the changelog check to a GitHub status check @faboweb
 * Moved to SDK v0.21.1 @faboweb
 * Using vue-jest for transpiling in tests for better code coverage output @faboweb
-* Moved to SDK v0.21.1 @faboweb
 
 ### Fixed
 
--- test/unit/helpers/fixed_time.js
+++ test/unit/helpers/fixed_time.js
@@ -3,7 +3,7 @@ require("moment")
 const mockMomentTz = require("moment-timezone")
 jest.mock("moment", () => time => mockMomentTz(time).tz("Etc/UTC"))
 
-const DATE_TO_USE = new Date(Date.UTC(1970, 0, 1, 0, 0, 0))
+const DATE_TO_USE = new Date(Date.UTC(1970, 0, 1, 0, 0, 42))
 global._Date = Date
 global.Date = jest.fn(() => new global._Date(DATE_TO_USE.toISOString()))
 Date.now = jest.genMockFunction().mockReturnValue(DATE_TO_USE.getTime())
