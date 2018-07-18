432c462ec802cc44cedbbaca20d85dc7d311ed47
David Braun <David@Tendermint.com>
2018-07-18 12:33:52 -0400

    Update version of Gaia to fix broken build.

--- tasks/build/Gaia/COMMIT.sh
+++ tasks/build/Gaia/COMMIT.sh
@@ -2,4 +2,4 @@
 
 # This is the commit of the SDK version to use for building Gaia.  We use an
 # explicit hash instead of a tag so we don't have to trust GitHub.
-export COMMIT=080dd5b97a90005cac0c730a81fd1d6864b9fd3e
+export COMMIT=28a1b5ebc0e1c030ebc1fb90413691b967e9a9c2
