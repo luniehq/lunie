57ec55403658877615734ba4e43c4afdf1658585
Federico Kunze <fekunze@berkeley.edu>
2018-08-06 17:58:15 +0200

    validatorUpdate event subscription

--- app/src/renderer/vuex/modules/validators.js
+++ app/src/renderer/vuex/modules/validators.js
@@ -2,12 +2,19 @@ export default ({ node }) => {
   const state = {
     validators: [],
     loading: false,
+    subscription: false,
     validatorHash: null
   }
 
   const mutations = {
     setValidators(state, validators) {
       state.validators = validators
+    },
+    updateValidator(state, index, updatedValidator) {
+      state.validators[index] = updatedValidator
+    },
+    addValidator(state, validator) {
+      state.validators.push(validator)
     },
     setValidatorHash(state, validatorHash) {
       state.validatorHash = validatorHash
@@ -39,6 +46,41 @@ export default ({ node }) => {
       if (validatorHash === state.validatorHash) return
       commit("setValidatorHash", validatorHash)
       dispatch("getValidators")
+    },
+    validatorUpdateSubscribe({ state, commit, dispatch }) {
+      // Return if already subscribed to updates
+      if (state.subscription) return
+
+      // TODO move to common file since subscribeToBlock also uses it
+      function error(err) {
+        if (err.data === "already subscribed") {
+          console.error("Tryed to subscribe to an already subscribed rpc event")
+          return
+        }
+        dispatch("nodeHasHalted")
+        console.error(
+          `Error subscribing to new validator set updates: ${
+            err.message
+          } ${err.data || ""}`
+        )
+      }
+      node.rpc.subscribe(
+        { query: "tm.event = 'ValidatorSetUpdates'" },
+        (err, event) => {
+          if (err) return error(err)
+          // https://github.com/tendermint/tendermint/pull/2161/files#diff-76d6626766e8d862e9acb34c166664b5
+          for (var validator in event.data.value.validator_updates) {
+            if (validator.power == 0) {
+              let dumpedValIdx = state.validators.findIndex(
+                v => v.pubkey == validator.pubkey
+              )
+              commit("updateValidator", dumpedValIdx, validator)
+            } else {
+              commit("addValidator", validator)
+            }
+          }
+        }
+      )
     }
   }
 
