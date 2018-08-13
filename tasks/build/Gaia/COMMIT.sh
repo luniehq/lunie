ff0047cc3c66d990cfe16bfcee116ac59177cfd4
Fabian <frznhope@gmail.com>
2018-08-13 18:42:37 +0200

    using latest release candidate

--- tasks/build/Gaia/COMMIT.sh
+++ tasks/build/Gaia/COMMIT.sh
@@ -2,4 +2,4 @@
 
 # This is the commit of the SDK version to use for building Gaia.  We use an
 # explicit hash instead of a tag so we don't have to trust GitHub.
-export COMMIT=6d95d1cef47ec6806fba26e17ac63c7d387bb771
+export COMMIT=0b2bf8f3a0e02844fb959099a27969186ab6fead
