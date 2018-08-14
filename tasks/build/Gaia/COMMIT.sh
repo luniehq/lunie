c687d45a64aea231e9e7e0a9396c71433d8ab82f
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-08-14 12:30:56 +0200

    back to github/tendermint/ui

--- tasks/build/Gaia/COMMIT.sh
+++ tasks/build/Gaia/COMMIT.sh
@@ -2,4 +2,4 @@
 
 # This is the commit of the SDK version to use for building Gaia.  We use an
 # explicit hash instead of a tag so we don't have to trust GitHub.
-export COMMIT=23e3d5ac12145c02fcb4b4767d7dfccad782aee5
+export COMMIT=68c921472182b5f3a3ecc4dc184ed90b44ff2624
