a8db29ab618e8b83f80e7b301bd93d206f9c2a8f
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-08-16 17:58:47 +0200

    e2es failing because of local storage

--- app/src/renderer/components/govern/PageProposalsNew.vue
+++ app/src/renderer/components/govern/PageProposalsNew.vue
@@ -6,16 +6,20 @@ tm-page(title='New Proposal')
   tm-part(title='Choose a Type')
     tm-list-item(title='Adjust Parameters' to="/proposals/new/adjust")
     tm-list-item(title='Amend Constitution' to="/proposals/new/amend")
-    tm-list-item(title='Create Atoms' to="/proposals/new/create")
+    tm-list-item(:title='`Create ${bondingDenom}s`' to="/proposals/new/create")
     tm-list-item(title='Plain Text' to="/proposals/new/text")
     tm-list-item(title='Upgrade Code' to="/proposals/new/upgrade")
 </template>
 
 <script>
+import { mapGetters } from "vuex"
 import { TmListItem, TmPage, TmPart } from "@tendermint/ui"
 import ToolBar from "common/TmToolBar"
 export default {
   name: "page-proposals-new",
+  computed: {
+    ...mapGetters(["bondingDenom"])
+  },
   components: {
     TmListItem,
     TmPage,
