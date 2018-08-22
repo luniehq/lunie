bd898b85d9cc7a47e8a2828b006e63c91a3c71de
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-08-22 16:20:36 +0200

    new docker flow

--- tasks/build/testnets/localBuild.sh
+++ tasks/build/testnets/localBuild.sh
@@ -4,9 +4,14 @@ if [ ! -f $(pwd)/../../../builds/gaia/linux_amd64/gaiad ]; then
   printf "Gaia missing; building first.\n"
   (cd ../../.. && yarn build:gaia)
 fi
-
-docker run \
+docker create \
+  --name=builder \
   --interactive \
-  --mount type=bind,source="$(pwd)/../../../builds",target=/go/builds \
-  --rm \
-  golang:1.10.2 < build.sh
+  --rm	\
+  golang:1.10.2
+docker cp "$(pwd)/../../../builds" builder:/go/builds
+docker cp build.sh builder:/go/builds/
+docker start builder
+docker exec builder /go/builds/build.sh
+docker cp builder:/go/builds/testnets "$(pwd)/../../../builds/testnets"
+docker stop builder
