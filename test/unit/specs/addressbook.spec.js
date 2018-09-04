eb87529f23ea6babe0a49523d8a04a98e76721bb
Federico Kunze <fekunze@berkeley.edu>
2018-09-04 17:21:09 +0200

    updated spec test

--- test/unit/specs/addressbook.spec.js
+++ test/unit/specs/addressbook.spec.js
@@ -12,7 +12,7 @@ describe("Addressbook", () => {
     Addressbook = require("src/main/addressbook.js")
 
     jest.mock("axios", () => ({
-      getNetInfo: async () => {
+      get: async () => {
         return {
           data: {
             result: {
@@ -44,43 +44,6 @@ describe("Addressbook", () => {
                   }
                 }
               ]
-            }
-          }
-        }
-      },
-      getStatus: async () => {
-        return {
-          result: {
-            node_info: {
-              id: "562dd7f579f0ecee8c94a11a3c1e378c1876f433",
-              listen_addr: "192.168.1.2:26656",
-              network: "test-chain-I6zScH",
-              version: "0.19.0",
-              channels: "4020212223303800",
-              moniker: "Ethans-MacBook-Pro.local",
-              other: [
-                "amino_version=0.9.8",
-                "p2p_version=0.5.0",
-                "consensus_version=v1/0.2.2",
-                "rpc_version=0.7.0/3",
-                "tx_index=on",
-                "rpc_addr=tcp://0.0.0.0:26657"
-              ]
-            },
-            sync_info: {
-              latest_block_hash: "2D4D7055BE685E3CB2410603C92AD37AE557AC59",
-              latest_app_hash: "0000000000000000",
-              latest_block_height: 231,
-              latest_block_time: "2018-04-27T23:18:08.459766485-04:00",
-              syncing: false
-            },
-            validator_info: {
-              address: "5875562FF0FFDECC895C20E32FC14988952E99E7",
-              pub_key: {
-                type: "AC26791624DE60",
-                value: "PpDJRUrLG2RgFqYYjawfn/AcAgacSXpLFrmfYYQnuzE="
-              },
-              voting_power: 10
             }
           }
         }
