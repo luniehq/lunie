1faf0a60798beddb16c1c80e74a3e521e0e4c6b3
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-09-10 13:36:33 +0200

    increase coverage

--- test/unit/specs/scripts/common.spec.js
+++ test/unit/specs/scripts/common.spec.js
@@ -1,26 +1,26 @@
 import { shortAddress } from "renderer/scripts/common"
 
 describe("shortAddress", () => {
+  const bech32ish = "asdf1asdfghjkl"
+  const longString = "qwertyuiopasdfghjklzxcvbnm"
+
   it("should treat bech32ish like bech32", () => {
-    const bech32ish = "asdf1asdfghjkl"
     const shortString = shortAddress(bech32ish)
     expect(shortString.toLowerCase()).toBe("asdf…hjkl")
   })
 
   it("should shorten string by default", () => {
-    const longString = "qwertyuiopasdfghjklzxcvbnm"
     const shortString = shortAddress(longString)
     expect(shortString.toLowerCase()).toBe("qwer…vbnm")
   })
   it("should shorten string by 5", () => {
-    const longString = "qwertyuiopasdfghjklzxcvbnm"
     const shortString = shortAddress(longString, 5)
     expect(shortString.toLowerCase()).toBe("qwert…cvbnm")
   })
 
   it("should return already short string", () => {
-    const longString = "qwe"
-    const shortString = shortAddress(longString)
+    const tinyString = "qwe"
+    const shortString = shortAddress(tinyString)
     expect(shortString.toLowerCase()).toBe("qwe")
   })
 })
