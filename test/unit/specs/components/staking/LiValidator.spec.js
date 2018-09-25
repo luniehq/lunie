00bc8182da017d98ce60d8cc867899924c7e4bab
Federico Kunze <fekunze@berkeley.edu>
2018-09-25 17:25:04 +0200

    Added LiValidator tests

--- test/unit/specs/components/staking/LiValidator.spec.js
+++ test/unit/specs/components/staking/LiValidator.spec.js
@@ -21,6 +21,12 @@ describe("LiValidator", () => {
     await store.dispatch("getDelegates")
     delegate = store.state.delegates.delegates[0]
     delegate.percent_of_vote = "22%"
+    delegate.signing_info = {
+      start_height: 0,
+      index_offset: 465400,
+      jailed_until: "1970-01-01T00:00:00Z",
+      signed_blocks_counter: 9878
+    }
     wrapper.setData({ delegate })
   })
 
@@ -30,6 +36,15 @@ describe("LiValidator", () => {
 
   it("should show the voting power", () => {
     expect(wrapper.html()).toContain("22%")
+  })
+
+  it("should show the validator's uptime", () => {
+    expect(wrapper.vm.delegate.signing_info).toBeDefined()
+    expect(wrapper.html()).toContain("98.78%")
+  })
+
+  it("should show the validator's comission", () => {
+    expect(wrapper.html()).toContain("0%")
   })
 
   it("should show the type of the candidate", () => {
