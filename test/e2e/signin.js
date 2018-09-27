7c914b7f0e706f7c179c9a7595e79cb48864fc34
Jordan Bibla <jbibla@gmail.com>
2018-09-27 11:32:33 -0400

    fixed lint

--- test/e2e/signin.js
+++ test/e2e/signin.js
@@ -1,6 +1,6 @@
 let test = require(`tape-promise/tape`)
 let { getApp, refresh } = require(`./launch.js`)
-let { openMenu, login, sleep, navigateToPreferences } = require(`./common.js`)
+let { login, sleep, navigateToPreferences } = require(`./common.js`)
 
 /*
 * NOTE: For some strange reason element.click() does not always work. In some cases I needed to use client.leftClick(selector). But this will be deprecated and pollutes the console with a deprecation warning.
