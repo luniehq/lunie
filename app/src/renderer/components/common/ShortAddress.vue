71003bef947fdcca746518588c28ff9b918f2516
Jordan Bibla <jbibla@gmail.com>
2018-10-04 17:54:32 -0400

    created short address component

---
+++ app/src/renderer/components/common/ShortAddress.vue
@@ -0,0 +1,76 @@
+<template lang="pug">
+.short-address
+  .address(@click.prevent.stop="copy" v-tooltip.top="address")#address {{ shortAddress(address) }}
+  .success(:class="{showSuccess:showSuccess}")
+    i.material-icons check
+    span Copied
+</template>
+
+<script>
+import { clipboard } from "electron"
+import { shortAddress } from "scripts/common"
+export default {
+  name: `short-address`,
+  computed: {
+    notifyTitle() {
+      if (this.title) return this.title
+      else return `Copy Success!`
+    },
+    notifyBody() {
+      if (this.body) return this.body
+      else return `"${this.value}" has been copied to your clipboard.`
+    }
+  },
+  data: () => ({
+    shortAddress,
+    showSuccess: false
+  }),
+  methods: {
+    copy() {
+      clipboard.writeText(this.address)
+      this.showSuccess = true
+      setTimeout(() => {
+        this.showSuccess = false
+      }, 3000)
+    }
+  },
+  props: [`address`]
+}
+</script>
+<style lang="stylus">
+@import '~variables'
+
+.short-address
+  align-items flex-start
+  display flex
+  padding 0
+  margin 0
+
+  .address
+    color var(--dim)
+    cursor pointer
+    font-size 14px
+    line-height 14px
+
+    &:hover
+      color var(--link)
+
+  .success
+    align-items flex-end
+    display flex
+    font-size sm
+    opacity 0
+    padding-left 10px
+    transition opacity 500ms ease
+    position relative
+    top -2px
+
+    &.showSuccess
+      opacity 1
+
+    i
+      color var(--success)
+      font-size m
+      padding-bottom 2px
+      padding-right 0
+</style>
