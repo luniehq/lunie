fac7e094e3e669b9f6eba696b86a5ca650f7b036
Jordan Bibla <jbibla@gmail.com>
2018-10-09 23:53:18 -0400

    removed a lot of unused code from TabMyDelegations, increased test coverage

--- app/src/renderer/components/staking/TabMyDelegations.vue
+++ app/src/renderer/components/staking/TabMyDelegations.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div
-    h3
+    h3.tab-header
       | Your Validators
       |
       i.material-icons.info-button(v-tooltip.top="bondInfo") info_outline
@@ -21,45 +21,33 @@
       | to spread some of your Atoms around.
 
     div(v-if="undelegatedValidators.length")
-      h3
+      h3.tab-header
         | Your Undelegated Validators
         |
         i.material-icons.info-button(v-tooltip.top="unbondInfo") info_outline
-      li-validator(
-        v-for='validator in undelegatedValidators'
-        :key='`Undelegated/${validator.id}`'
-        :validator='validator'
-      )
+      ol
+        li-validator(
+          v-for='validator in undelegatedValidators'
+          :key='`Undelegated/${validator.id}`'
+          :validator='validator'
+        )
 </template>
 
 <script>
 import { mapGetters } from "vuex"
-import num from "scripts/num"
-import { includes, orderBy } from "lodash"
-import Mousetrap from "mousetrap"
 import LiValidator from "staking/LiValidator"
-import { TmBtn, TmPage, TmDataEmpty, TmDataLoading } from "@tendermint/ui"
+import { TmDataEmpty } from "@tendermint/ui"
 import DataEmptySearch from "common/TmDataEmptySearch"
-import { calculateTokens } from "scripts/common"
-import ModalSearch from "common/TmModalSearch"
 import PanelSort from "staking/PanelSort"
-import VmToolBar from "common/VmToolBar"
 export default {
   name: `tab-my-delegations`,
   components: {
     LiValidator,
-    TmBtn,
     TmDataEmpty,
     DataEmptySearch,
-    TmDataLoading,
-    ModalSearch,
-    TmPage,
-    PanelSort,
-    VmToolBar
+    PanelSort
   },
   data: () => ({
-    num: num,
-    query: ``,
     sort: {
       property: `percent_of_vote`,
       order: `desc`
@@ -71,59 +59,12 @@ export default {
     ...mapGetters([
       `delegates`,
       `delegation`,
-      `filters`,
-      `shoppingCart`,
       `committedDelegations`,
-      `config`,
       `user`,
-      `connected`,
-      `bondingDenom`,
-      `keybase`
+      `bondingDenom`
     ]),
-    address() {
-      return this.user.address
-    },
-    somethingToSearch() {
-      return !!this.delegates.delegates.length
-    },
-    vpTotal() {
-      return this.delegates.delegates
-        .slice(0)
-        .map(v => {
-          v.voting_power = v.voting_power ? Number(v.voting_power) : 0
-          return v
-        })
-        .sort((a, b) => b.voting_power - a.voting_power)
-        .slice(0, 100)
-        .reduce((sum, v) => sum + v.voting_power, 0)
-    },
-    enrichedDelegates() {
-      return !this.somethingToSearch
-        ? []
-        : this.delegates.delegates.map(v => {
-            v.small_moniker = v.description.moniker.toLowerCase()
-            v.percent_of_vote = num.percent(v.voting_power / this.vpTotal)
-            v.your_votes = this.num.full(
-              calculateTokens(v, this.committedDelegations[v.id])
-            )
-            v.keybase = this.keybase[v.description.identity]
-            return v
-          })
-    },
-    sortedFilteredEnrichedDelegates() {
-      let query = this.filters.delegates.search.query || ``
-      let sortedEnrichedDelegates = orderBy(
-        this.enrichedDelegates.slice(0),
-        [this.sort.property, `small_moniker`],
-        [this.sort.order, `asc`]
-      )
-      if (this.filters.delegates.search.visible) {
-        return sortedEnrichedDelegates.filter(i =>
-          includes(JSON.stringify(i).toLowerCase(), query.toLowerCase())
-        )
-      } else {
-        return sortedEnrichedDelegates
-      }
+    address({ user } = this) {
+      return user.address
     },
     undelegatedValidators(
       { delegates: { delegates }, delegation: { unbondingDelegations } } = this
@@ -131,13 +72,7 @@ export default {
       const unbonding = new Set(Object.keys(unbondingDelegations))
       return delegates.filter(({ id }) => unbonding.has(id))
     },
-    userCanDelegate() {
-      return (
-        (this.shoppingCart.length > 0 || this.user.atoms > 0) &&
-        this.delegation.loadedOnce
-      )
-    },
-    properties() {
+    properties({ bondingDenom } = this) {
       return [
         {
           title: `Moniker`,
@@ -146,27 +81,21 @@ export default {
           class: `name`
         },
         {
-          title: `Bonded ${this.bondingDenom}`,
+          title: `Bonded ${bondingDenom}`,
           value: `your_votes`,
-          tooltip: `Number of ${
-            this.bondingDenom
-          } you have delegated to the validator`,
+          tooltip: `Number of ${bondingDenom} you have delegated to the validator`,
           class: `your-votes`
         },
         {
           title: `Rewards`,
           value: `your_rewards`, // TODO: use real rewards
-          tooltip: `Rewards of ${
-            this.bondingDenom
-          } you have gained from the validator`,
+          tooltip: `Rewards of ${bondingDenom} you have gained from the validator`,
           class: `your-rewards` // TODO: use real rewards
         },
         {
           title: `Voting Power`,
           value: `percent_of_vote`,
-          tooltip: `Percentage of ${
-            this.bondingDenom
-          } the validator has on The Cosmos Hub`,
+          tooltip: `Percentage of ${bondingDenom} the validator has on The Cosmos Hub`,
           class: `percent_of_vote`
         },
         {
@@ -193,35 +122,17 @@ export default {
       const committed = new Set(Object.keys(committedDelegations))
       return delegates.filter(({ id }) => committed.has(id))
     }
-  },
-  watch: {
-    address: function(address) {
-      address && this.updateDelegates()
-    }
-  },
-  methods: {
-    updateDelegates() {
-      this.$store.dispatch(`updateDelegates`)
-    },
-    setSearch(bool = !this.filters[`delegates`].search.visible) {
-      if (!this.somethingToSearch) return false
-      this.$store.commit(`setSearchVisible`, [`delegates`, bool])
-    }
-  },
-  async mounted() {
-    Mousetrap.bind([`command+f`, `ctrl+f`], () => this.setSearch(true))
-    Mousetrap.bind(`esc`, () => this.setSearch(false))
-
-    // XXX temporary because querying the shares shows old shares after bonding
-    // this.updateDelegates()
   }
 }
 </script>
 <style lang="stylus">
 @require '~variables'
 
-h3
-  margin 1em auto
+.tab-header
+  margin 1rem 1rem 0 2rem
+  font-size 14px
+  color var(--dim)
+  font-weight 500
 
 .info-button
   color var(--link)
@@ -235,14 +146,4 @@ h3
   margin-left 2rem
   padding 0.5rem
   text-align center
-
-@media screen and (min-width: 768px)
-  padding-bottom 4rem
-
-.fixed-button-bar
-  padding 1rem 1rem 1rem 2rem
-
-@media screen and (min-width: 1024px)
-  .fixed-button-bar
-    margin-left width-side
 </style>
