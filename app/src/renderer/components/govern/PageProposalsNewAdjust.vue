7bdb87a5cbd6c09500b4824f7aa6da0260ed14a2
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2018-10-17 16:22:23 +0200

    Update app/src/renderer/components/govern/PageProposalsNewAdjust.vue

--- app/src/renderer/components/govern/PageProposalsNewAdjust.vue
+++ app/src/renderer/components/govern/PageProposalsNewAdjust.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-tm-page(data-title="Proposal: Adjust Parameters")
+tm-page(data-title="Parameters Change Proposal")
   div(slot="menu"): vm-tool-bar
     router-link(to="/proposals/new" exact v-tooltip.bottom="'Back'")
       i.material-icons arrow_back
