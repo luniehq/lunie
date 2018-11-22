4ade5d9ad7494cf560f0ec350a791b18ae071e8a
Jordan Bibla <jbibla@gmail.com>
2018-11-22 17:26:07 -0500

    cleaned up sidebar and fixed active classes

--- app/src/renderer/components/common/AppMenu.vue
+++ app/src/renderer/components/common/AppMenu.vue
@@ -1,27 +1,33 @@
 <template lang="pug">
 menu.app-menu
   .app-menu-main
-    tm-list-item#app-menu__wallet(
+    router-link.app-menu-item#app-menu__wallet(
       to="/"
       exact
       @click.native="close"
       title="Wallet")
-    tm-list-item#app-menu__transactions(
-      to="/wallet/transactions"
+      h2.app-menu-title Wallet
+      i.material-icons chevron_right
+    router-link.app-menu-item#app-menu__transactions(
+      v-if="config.devMode || mockedConnector"
+      to="/transactions"
       exact
       @click.native="close"
-      title="Transactions"
-      v-if="config.devMode || mockedConnector")
-    tm-list-item#app-menu__staking(
+      title="Transactions")
+      h2.app-menu-title Transactions
+      i.material-icons chevron_right
+    router-link.app-menu-item#app-menu__staking(
       to="/staking"
-      exact
-      @click.native="close" title="Staking"
-      v-bind:class="{ 'active': isValidatorPage }")
-    tm-list-item#app-menu__proposals(
+      @click.native="close"
+      title="Staking")
+      h2.app-menu-title Staking
+      i.material-icons chevron_right
+    router-link.app-menu-item#app-menu__proposals(
       to="/governance"
-      exact @click.native="close"
-      title="Governance"
-      v-if="config.devMode || mockedConnector")
+      @click.native="close"
+      title="Governance")
+      h2.app-menu-title Governance
+      i.material-icons chevron_right
   connected-network
 </template>
 
@@ -30,23 +36,18 @@ import { mapGetters } from "vuex"
 import PerfectScrollbar from "perfect-scrollbar"
 import noScroll from "no-scroll"
 import ConnectedNetwork from "common/TmConnectedNetwork"
-import { TmListItem } from "@tendermint/ui"
-import UserPane from "common/TmUserPane"
+import { TmBtn, TmListItem } from "@tendermint/ui"
 export default {
   name: `app-menu`,
   components: {
     ConnectedNetwork,
-    TmListItem,
-    UserPane
+    TmListItem
   },
   data: () => ({
     ps: {}
   }),
   computed: {
-    ...mapGetters([`validators`, `config`, `lastHeader`, `mockedConnector`]),
-    isValidatorPage() {
-      return this.$route.params.validator
-    }
+    ...mapGetters([`validators`, `config`, `lastHeader`, `mockedConnector`])
   },
   mounted() {
     this.ps = new PerfectScrollbar(this.$el.querySelector(`.app-menu-main`))
@@ -74,45 +75,27 @@ export default {
     flex 1
     position relative // for perfect-scrollbar
 
-    .tm-li
-      border-bottom 1px solid var(--bc-dim)
-
-  .tm-user
-    border-top 1px solid var(--bc)
-    padding 1rem
+  .app-menu-item
     display flex
+    justify-content space-between
+    align-items center
+    border-bottom 1px solid var(--bc-dim)
+    padding 1rem
+    color var(--dim)
 
-    .tm-user-info
-      flex 1
-      display flex
+    &:hover
+      color var(--bright)
+      background var(--hover-bg)
 
-    .avatar
-      background var(--link)
-      width 2rem
-      height 2rem
-      border-radius 1rem
-      display flex
-      align-items center
-      justify-content center
+  .router-link-active
+    background var(--hover-bg)
 
-      i
-        color var(--txt)
+    i
+      color var(--tertiary)
 
-    .text
-      padding 0 0.5rem
-
-    .title
-      color var(--txt)
-
-    .subtitle
-      font-size xs
-      color var(--dim)
-
-    .tm-btn
-      margin-right 0.5rem
-
-.tm-li.tm-li-link.router-link-exact-active
-  color var(--tertiary)
+    h2
+      color var(--bright)
+      font-weight 500
 
 @media screen and (max-width: 1023px)
   .app-menu
