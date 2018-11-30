e0a26aa34e8128b82c86519b4b99daf266cd245b
Federico Kunze <fekunze@berkeley.edu>
2018-11-30 11:47:15 +0100

    submit proposals e2e done

--- test/e2e/governance.js
+++ test/e2e/governance.js
@@ -2,23 +2,13 @@
 
 let test = require(`tape-promise/tape`)
 let { getApp, restart } = require(`./launch.js`)
-let {
-  navigate,
-  // waitForText,
-  login
-  // closeNotifications
-} = require(`./common.js`)
-// let {
-//   addresses
-// } = require(`../../app/src/renderer/connectors/lcdClientMock.js`)
-
+let { navigate, login } = require(`./common.js`)
 /*
  * NOTE: don't use a global `let client = app.client` as the client object changes when restarting the app
  */
 
 test(`Governance`, async function(t) {
   let { app } = await getApp(t)
-  // app.env.COSMOS_MOCKED = false
   await restart(app)
   let $ = (...args) => app.client.$(...args)
 
@@ -33,6 +23,36 @@ test(`Governance`, async function(t) {
     )
     await $(`div*=No Governance Proposals`).waitForExist()
     t.pass(`correct message when there are no proposals`)
+    t.end()
+  })
+
+  t.test(`submit proposal`, async function(t) {
+    await app.client.$(`#propose-btn`).click()
+    await t.ok(
+      await app.client.$(`#modal-propose`).isVisible(),
+      `opens modal proposal`
+    )
+    await app.client.setValue(`#title`, `E2E test proposal title`)
+    await app.client.setValue(`#description`, `E2E test proposal title`)
+    await app.client.setValue(`#amount`, `10`)
+    await t.ok(
+      await app.client
+        .click(`//button/*[. = 'Submit proposal']`)
+        .waitForVisible(
+          `//*[. = 'You have successfully submitted a new text proposal']`,
+          5 * 1000
+        ),
+      `successful proposal submission`
+    )
+    t.equal(
+      (await app.client.$$(`.li-proposal`)).length,
+      1,
+      `displays the proposal table with one element`
+    )
+    await t.ok(
+      await app.client.$(`a*=E2E test proposal title`).isVisible(),
+      `shows the newly created proposal`
+    )
     t.end()
   })
 
