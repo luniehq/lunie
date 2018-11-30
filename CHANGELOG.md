2944906ff13ba6d4a5e03c6e02d48dda60461d29
Federico Kunze <fekunze@berkeley.edu>
2018-11-30 13:34:04 +0100

    deposit e2e and open vote modal

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -38,6 +38,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - [\#1198](https://github.com/cosmos/voyager/pull/1198) added e2e tests for wallet and staking transactions @faboweb
 - [\#1578](https://github.com/cosmos/voyager/issues/1578) added e2e tests for submit governance proposals @fedekunze
 - [\#1672](https://github.com/cosmos/voyager/issues/1672) added e2e tests for staking parameters tab @fedekunze
+- [\#1550](https://github.com/cosmos/voyager/issues/1578) added e2e tests for voting on governance proposals @fedekunze
+- [\#1555](https://github.com/cosmos/voyager/issues/1578) added e2e tests for submitting a deposit on a governance proposal @fedekunze
 
 ### Changed
 
--- test/e2e/governance.js
+++ test/e2e/governance.js
@@ -2,7 +2,7 @@
 
 let test = require(`tape-promise/tape`)
 let { getApp, restart } = require(`./launch.js`)
-let { navigate, login } = require(`./common.js`)
+let { navigate, login, closeNotifications } = require(`./common.js`)
 /*
  * NOTE: don't use a global `let client = app.client` as the client object changes when restarting the app
  */
@@ -34,13 +34,13 @@ test(`Governance`, async function(t) {
     )
     await app.client.setValue(`#title`, `E2E test proposal title`)
     await app.client.setValue(`#description`, `E2E test proposal title`)
-    await app.client.setValue(`#amount`, `10`)
+    await app.client.setValue(`#amount`, 5)
     await t.ok(
       await app.client
         .click(`//button/*[. = 'Submit proposal']`)
         .waitForVisible(
           `//*[. = 'You have successfully submitted a new text proposal']`,
-          5 * 1000
+          4 * 1000
         ),
       `successful proposal submission`
     )
@@ -53,6 +53,63 @@ test(`Governance`, async function(t) {
       await app.client.$(`a*=E2E test proposal title`).isVisible(),
       `shows the newly created proposal`
     )
+    await closeNotifications(app)
+    t.end()
+  })
+
+  t.test(`proposal page`, async function(t) {
+    await app.client.$(`a*=E2E test proposal title`).click()
+    await t.ok(
+      await app.client.$(`.proposal`).isVisible(),
+      `shows the proposal page`
+    )
+    t.end()
+  })
+
+  t.test(`deposit`, async function(t) {
+    await t.ok(
+      await app.client.$(`.validator-profile__status.yellow`).isVisible(),
+      `the proposal is on deposit period`
+    )
+    await app.client.$(`#deposit-btn`).click()
+    await t.ok(
+      await app.client.$(`#modal-deposit`).isVisible(),
+      `opens modal deposit`
+    )
+    await app.client.setValue(`#amount`, 10)
+    await t.ok(
+      await app.client
+        .$(`#submit-deposit`)
+        .click()
+        .waitForVisible(`.tm-notification`, 4 * 1000),
+      `successful deposit`
+    )
+    // TODO: check if deposit increased
+    // let deposit = app.client.$(`//dd/*[. = 'Deposit']`)
+    await closeNotifications(app)
+    t.end()
+  })
+
+  t.test(`vote`, async function(t) {
+    await t.ok(
+      await app.client.$(`.validator-profile__status.blue`).isVisible(),
+      `the proposal is on voting period`
+    )
+    await app.client.$(`#vote-btn`).click()
+    await t.ok(
+      await app.client.$(`#modal-vote`).isVisible(),
+      `opens modal vote`
+    )
+    // await t.ok(
+    //   await app.client
+    //     .click(`//button/*[. = 'Submit proposal']`)
+    //     .waitForVisible(
+    //       `//*[. = 'You have successfully submitted a new text proposal']`,
+    //       5 * 1000
+    //     ),
+    //   `successful proposal submission`
+    // )
+    // TODO: check if tally increased, blocked by SDK v.0.27
     t.end()
   })
 
