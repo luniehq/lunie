1736732d59dbf9a7b7f556ba11ef9aee7c1e5ead
Jordan Bibla <jbibla@gmail.com>
2018-12-04 10:12:06 +0100

    Update app/src/renderer/components/common/TmDataConnecting.vue
    Co-Authored-By: faboweb <frznhope@gmail.com>

--- app/src/renderer/components/common/TmDataConnecting.vue
+++ app/src/renderer/components/common/TmDataConnecting.vue
@@ -2,7 +2,7 @@
   <tm-data-msg>
     <div slot="title">Waiting for connection</div>
     <div slot="subtitle">
-      You are currently not connected to a node. Voyager will load the data when
+      You are currently not connected to a node. Voyager will load this data when
       the connection is established again.
     </div>
   </tm-data-msg>
