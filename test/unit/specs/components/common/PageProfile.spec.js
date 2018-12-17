204134571ac98e2b12d8aceeaad596f1b394ae1f
Jordan Bibla <jbibla@gmail.com>
2018-12-17 14:11:26 -0500

    lint

--- test/unit/specs/components/common/PageProfile.spec.js
+++ test/unit/specs/components/common/PageProfile.spec.js
@@ -1,13 +1,13 @@
 import { mount } from "@vue/test-utils"
 import PageProfile from "renderer/components/common/PageProfile"
 
-describe("PageProfile", () => {
+describe(`PageProfile`, () => {
   let wrapper
   beforeEach(() => {
     wrapper = mount(PageProfile)
   })
 
-  it("has the expected html structure", () => {
+  it(`has the expected html structure`, () => {
     expect(wrapper.vm.$el).toMatchSnapshot()
   })
 })
