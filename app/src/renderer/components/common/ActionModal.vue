159ef6fd20599f8a0b54e174b45923d068c86e89
Jordan Bibla <jbibla@gmail.com>
2019-01-07 11:04:48 -0500

    fix shadow

--- app/src/renderer/components/common/ActionModal.vue
+++ app/src/renderer/components/common/ActionModal.vue
@@ -59,7 +59,7 @@ export default {
   z-index: var(--z-modal);
   border-top-left-radius: 0.25rem;
   border-top-right-radius: 0.25rem;
-  box-shadow: 0 8px 16px rgba(200, 200, 200, 0.1);
+  box-shadow: 0 2px 8px rgba(200, 200, 200, 0.1);
 }
 
 .action-modal-header {
