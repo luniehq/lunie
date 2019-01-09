bcdcbeab24131824edde770c4f6c4c5601a8ba42
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2019-01-09 10:49:56 -0300

    Delete COMMIT.sh

--- tasks/build/Gaia/COMMIT.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# This is the commit of the SDK version to use for building Gaia.  We use an
-# explicit hash instead of a tag so we don't have to trust GitHub.
-export COMMIT=bb54a0de127e45713f272217f578c0abe53a5b21
-
