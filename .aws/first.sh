0a7feb47f3f3a3b74e1918db89f44e8387983b25
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-01-24 18:35:04 +0100

    working boot process
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- .aws/first.sh
+++ .aws/first.sh
@@ -5,6 +5,7 @@ ACCOUNT=main_account
 PORT=26657
 API_PORT=8080
 NETWORK=testnet
+AMOUNT=100stake
 
 ./gaiad init --home . --moniker ${ACCOUNT} --chain-id ${NETWORK}
 NODEID=$(./gaiad tendermint show-node-id --home .)
@@ -16,16 +17,34 @@ echo ${PASSWORD} | ./gaiad gentx --name ${ACCOUNT} --home . --home-client .
 
 aws s3 cp config/genesis.json s3://cosmos-gaia/genesis.json
 
+# boot proper nodes in reachable detached sessions
 screen -dmS gaia ./gaiad start --home .
 screen -dmS rest ./gaiacli rest-server --laddr tcp://0.0.0.0:${API_PORT} --home . --node http://localhost:${PORT} --chain-id ${NETWORK} --trust-node true
 
+# get my address to use it as source of richness for others
 ADDRESS=$(./gaiacli keys show ${ACCOUNT} --home . --address)
 
 while true
 do
-    aws s3 ls s3://cosmos-gaia/addresses/ | while IFS= read -r row || [[ -n "$row" ]]; do
-        DESTINATION=$(echo $row | awk '{print $4}')
-        echo ${PASSWORD} | ./gaiacli tx send --home . --from ${ADDRESS} --amount=100stake  --to=${DESTINATION} --chain-id=${NETWORK}
-        aws s3 rm s3://cosmos-gaia/addresses/${DESTINATION}
-    done
+    # Is anyone asking for money
+    LIST=$(aws s3 ls s3://cosmos-gaia/addresses/)
+    if [[ -n "${LIST}" ]];
+    then
+        PID=$(lsof -ti tcp:${API_PORT})
+        if [[ -n "${PID}" ]];
+        then
+            # we cannot use gaiacli while the rest server is running, it locks the resources
+            kill -9 ${PID}
+        fi
+
+        echo ${LIST} | while IFS= read -r row || [[ -n "$row" ]]; do
+            # for every file in the list pick the address and give money to it, then delete the file
+            DESTINATION=$(echo $row | awk '{print $4}')
+            echo ${PASSWORD} | ./gaiacli tx send --home . --from ${ADDRESS} --amount=${AMOUNT}  --to=${DESTINATION} --chain-id=${NETWORK}
+            aws s3 rm s3://cosmos-gaia/addresses/${DESTINATION}
+        done
+
+        #restore the server
+        screen -dmS rest ./gaiacli rest-server --laddr tcp://0.0.0.0:${API_PORT} --home . --node http://localhost:${PORT} --chain-id ${NETWORK} --trust-node true
+    fi
 done
--- .aws/others.sh
+++ .aws/others.sh
@@ -7,29 +7,34 @@ MAINNODEID=d08c69fa85969f3d0173f23aadc40268559d0f66
 MAINNODEIP=172.31.35.89
 MAINACCOUNT=main_account
 NETWORK=testnet
+VALIDATOR_AMOUNT=10stake
 
 ./gaiad init --home . --moniker ${ACCOUNT} --chain-id ${NETWORK}
 
 # GET Genesis file into config/genesis.json
 NODEID=$(./gaiad tendermint show-node-id --home .)
 
+# boot referring to the remote node
 screen -dmS gaia ./gaiad start --home . --p2p.laddr=tcp://0.0.0.0:$((PORT)) --address=tcp://0.0.0.0:$((PORT+1)) --rpc.laddr=tcp://0.0.0.0:$((PORT+2)) --p2p.persistent_peers="$MAINNODEID@$MAINNODEIP:$((PORT))"
 
+# get the key to make my node validator
 PUBKEY=$(./gaiad tendermint show-validator --home .)
 echo ${PASSWORD} | ./gaiacli keys add ${ACCOUNT} --home . > secondary_node.log
 ADDRESS=$(./gaiacli keys show ${ACCOUNT} --home . --address)
 echo ${PUBKEY} > ./${ADDRESS}
 
+# ask money by declaring my node
 aws s3 cp ${ADDRESS} s3://cosmos-gaia/addresses/${ADDRESS}
 
 poor=true
 while ${poor}
 do
-    ACCOUNT_INFO=$(./gaiacli query account cosmos1484awfj0nmqtp7fwwsfpf480xwpmj7c4zr3wl8 --chain-id testnet --trust-node --home .)
+    #
+    ACCOUNT_INFO=$(./gaiacli query account ${ADDRESS} --chain-id ${NETWORK} --trust-node --home .)
     if [[ ${ACCOUNT_INFO} == *"auth/Account"* ]]; then
         echo "Address funded, thanks main node!"
         poor=false
     fi
 done
 
-echo ${PASSWORD} | ./gaiacli tx staking create-validator --home . --from ${ACCOUNT} --amount=10stake --pubkey=${PUBKEY} --address-delegator=${ADDRESS} --moniker=${ACCOUNT} --chain-id=${NETWORK} --commission-max-change-rate=0 --commission-max-rate=0 --commission-rate=0 --json
+echo ${PASSWORD} | ./gaiacli tx staking create-validator --home . --from ${ACCOUNT} --amount=${VALIDATOR_AMOUNT} --pubkey=${PUBKEY} --address-delegator=${ADDRESS} --moniker=${ACCOUNT} --chain-id=${NETWORK} --commission-max-change-rate=0 --commission-max-rate=0 --commission-rate=0 --json
