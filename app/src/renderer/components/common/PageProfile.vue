9b58e239a38b09f3fe40476211cdab9dc72bcd6e
Jordan Bibla <jbibla@gmail.com>
2019-01-29 00:06:12 -0500

    PageProfile styling updates

--- app/src/renderer/components/common/PageProfile.vue
+++ app/src/renderer/components/common/PageProfile.vue
@@ -20,17 +20,11 @@ export default {
 </script>
 
 <style>
-.page-profile__section {
-  background-color: var(--app-fg);
-  display: flex;
-  margin-bottom: 1rem;
-  padding: 2rem;
-}
-
 .column {
   display: flex;
-  flex-flow: column;
+  flex-direction: column;
   position: relative;
+  width: 100%;
 }
 
 .row {
@@ -39,24 +33,54 @@ export default {
   width: 100%;
 }
 
+.row-unjustified {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+
+.page-profile__section {
+  margin-bottom: 1rem;
+}
+
+.page-profile__section--between > .row {
+  justify-content: space-between;
+}
+
+.page-profile__header {
+  background-color: var(--app-fg);
+}
+
 .page-profile__header .avatar {
   background: var(--app-nav);
-  border-radius: 50%;
-  height: 155px;
-  margin-right: 2rem;
+  height: 8rem;
+  width: 8rem;
+  margin: 1rem 2rem 1rem 1rem;
   padding: 1rem;
-  width: 155px;
 }
 
 .page-profile__header__info {
-  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 }
 
-.page-profile__header__name {
-  margin-bottom: 2rem;
+.page-profile__status-and-title {
+  position: relative;
 }
 
-.page-profile__header__name__title {
+.page-profile__status {
+  border-radius: 50%;
+  display: inline-block;
+  height: 0.5rem;
+  left: -1rem;
+  top: 1rem;
+  position: absolute;
+  width: 0.5rem;
+}
+
+.page-profile__title {
   color: #fff;
   display: inline-block;
   font-size: var(--h1);
@@ -65,31 +89,14 @@ export default {
   padding: 0 0.5rem 0.5rem 0;
 }
 
-.page-profile__header__name__address {
-  font-size: var(--sm);
-}
-
 .page-profile__header__actions {
-  flex-flow: column;
-  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  margin-right: 2rem;
 }
 
-.page-profile__header__actions button:not(:last-child) {
+.page-profile__header__actions button:first-child {
   margin-bottom: 0.5rem;
-}
-
-.page-profile__header__data__break {
-  border-right: 1px solid var(--bc-dim);
-  margin-right: 1rem;
-}
-
-.page-profile__status {
-  border-radius: 50%;
-  display: inline-block;
-  height: 0.5rem;
-  left: -1rem;
-  position: absolute;
-  width: 0.5rem;
 }
 
 .page-profile__status.red {
@@ -108,85 +115,27 @@ export default {
   background: var(--primary);
 }
 
-.page-profile__status-and-title {
-  align-items: center;
-  display: flex;
-}
-
-.page-profile__details > .row > .column {
-  flex: 1;
+.colored_dl {
+  width: 100%;
 }
 
 .info_dl {
-  display: flex;
-  flex-flow: column;
-  margin-bottom: 1.5rem;
-  margin-right: 1rem;
+  padding: 1rem;
+  border: 1px solid var(--bc-dim);
+  background-color: var(--app-fg);
+  line-height: 1.25rem;
 }
 
 .info_dl dt {
   color: var(--dim);
   font-size: var(--sm);
-  margin-bottom: 4px;
+  margin-bottom: 2px;
+  font-weight: 500;
 }
 
 .info_dl dd {
-  border: solid 1px #31354e;
-  border-radius: 2px;
   font-size: 1rem;
   line-height: 1rem;
-  padding: 0.5rem;
-}
-
-.info_dl dd.info_dl__text-box {
-  min-height: 6.91rem;
-}
-
-.colored_dl {
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-}
-
-.colored_dl:not(:last-child) {
-  margin-right: 1rem;
-}
-
-.colored_dl dt {
-  color: var(--dim);
-  font-size: var(--sm);
-  margin-bottom: 0.5rem;
-  text-align: center;
-}
-
-.colored_dl dd {
-  background-color: var(--white-fade-1);
-  border: 1px solid var(--white-fade-2);
-  border-radius: 4px;
-  color: var(--dim);
-  display: block;
-  font-size: h6;
-  line-height: h6;
-  padding: 4px 4px;
-  text-align: right;
-  width: 100%;
-}
-
-.colored_dl dd.red {
-  background-color: rgba(209, 2, 0, 0.15);
-  border: solid 0.5px rgba(209, 2, 0, 0.25);
-  color: #ff0200;
-}
-
-.colored_dl dd.yellow {
-  background-color: rgba(255, 149, 2, 0.15);
-  border: solid 0.5px rgba(255, 149, 2, 0.25);
-  color: #ff9502;
-}
-
-.colored_dl dd.green {
-  background-color: rgba(46, 164, 45, 0.15);
-  border: solid 0.5px rgba(46, 164, 45, 0.25);
-  color: #2ea42d;
+  color: var(--bright);
 }
 </style>
