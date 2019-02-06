3cebce65b5f2e03a655385f9375b3d948ba97965
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-02-06 17:31:31 +0000

    keep old versions if available, otherwise build it
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- tasks/build/Gaia/localBuild.sh
+++ tasks/build/Gaia/localBuild.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
 . ./COMMIT.sh
+version="$(pwd)/../../../builds/GaiaVersions/$COMMIT"
 source="$(pwd)/../../../builds/Gaia"
-mkdir -p "$source"
-export TARGET=/mnt
+mkdir -p "$version"
+rm -rf "$source"
+if [[ -f "$version/linux/gaiad" ]]; then
+    echo "Already built"
+else
+    echo "building new hash"
+    export TARGET=/mnt
 
-docker run \
-  --interactive \
-  --env COMMIT \
-  --env TARGET \
-  --mount type=bind,source="$source",target="$TARGET" \
-  --rm \
-  golang:1.11.1 < build.sh
+    docker run \
+      --interactive \
+      --env COMMIT \
+      --env TARGET \
+      --mount type=bind,source="$version",target="$TARGET" \
+      --rm \
+      golang:1.11.5 < build.sh
+fi
+
+ln -s ${version} ${source}
