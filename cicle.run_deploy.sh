bb6bd6c1b750fb22b9ab88ebffe00daa1c6d4e21
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-02-12 16:59:43 +0100

    Document build command with parameters, remove deploy bash script
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- cicle.run_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -xeuo pipefail
-
-# TODO: region should be ENV determined
-aws s3 sync ${CIRCLE_ARTIFACTS}/dist/ ${AWS_S3_BUCKET_VOYAGER} --delete --region eu-central-1 > /dev/null
-
-# aws cloudfront create-invalidation --distribution-id ${AWS_DISTRIBUTION_ID_VOYAGER} --paths '/*'
