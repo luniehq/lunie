17509f83983475032c582293cfbc96850d7d16a1
Federico Kunze <fekunze@berkeley.edu>
2019-02-13 12:18:18 +0100

    revert app menu link order

--- app/src/renderer/components/common/AppMenu.vue
+++ app/src/renderer/components/common/AppMenu.vue
@@ -2,17 +2,6 @@
   <menu class="app-menu">
     <div class="app-menu-main">
       <router-link
-        id="app-menu__network"
-        class="app-menu-item"
-        to="/"
-        exact="exact"
-        title="Network"
-        @click.native="close"
-      >
-        <h2 class="app-menu-title">Network</h2>
-        <i class="material-icons">chevron_right</i>
-      </router-link>
-      <router-link
         id="app-menu__wallet"
         class="app-menu-item"
         to="/wallet"
@@ -52,6 +41,17 @@
         @click.native="close"
       >
         <h2 class="app-menu-title">Governance</h2>
+        <i class="material-icons">chevron_right</i>
+      </router-link>
+      <router-link
+        id="app-menu__network"
+        class="app-menu-item"
+        to="/"
+        exact="exact"
+        title="Network"
+        @click.native="close"
+      >
+        <h2 class="app-menu-title">Network</h2>
         <i class="material-icons">chevron_right</i>
       </router-link>
     </div>
