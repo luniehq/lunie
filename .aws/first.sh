d9b0451bd532761978b4a1d0f0105aecf717f91b
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-02-18 16:09:34 +0100

    split faucet into separate file, it may even run in a separate machine
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- .aws/first.sh
+++ .aws/first.sh
@@ -5,7 +5,6 @@ ACCOUNT=main_account
 PORT=26657
 API_PORT=8080
 NETWORK=testnet
-AMOUNT=100stake
 
 # first of all remove old genesis, we do not want other node to boot with the wrong stuff
 aws s3 rm s3://cosmos-gaia/genesis.json
@@ -27,41 +26,5 @@ aws s3 cp config/genesis.json s3://cosmos-gaia/genesis.json
 # boot proper nodes in reachable detached sessions
 screen -dmS gaia ./gaiad start --home .
 screen -dmS rest ./gaiacli rest-server --laddr tcp://0.0.0.0:${API_PORT} --home . --node http://localhost:${PORT} --chain-id ${NETWORK} --trust-node true
+screen -dmS faucet ./faucet.sh ${ACCOUNT} ${PASSWORD} ${NETWORK}
 
-# get my address to use it as source of richness for others
-ADDRESS=$(./gaiacli keys show ${ACCOUNT} --home . --address)
-
-while true
-do
-    # Is anyone asking for money
-    LIST=$(aws s3 ls s3://cosmos-gaia/addresses/)
-    if [[ -n "${LIST}" ]];
-    then
-        # TODO: SRE team is working on a clean solution.
-        # This hacky version is temporary and will be replaced by a stargate running on a separate machine
-        # Remove this logic as soon as we put back the deploy-gaia automation
-
-        # Check if a stargate is locally running
-        PID=$(lsof -ti tcp:${API_PORT})
-        if [[ -n "${PID}" ]];
-        then
-            # we cannot use gaiacli while the rest server is running, it locks the resources
-            kill -9 ${PID}
-        fi
-
-        echo ${LIST} | while IFS= read -r row || [[ -n "$row" ]]; do
-            # for every file in the list pick the address and give money to it, then delete the file
-            DESTINATION=$(echo $row | awk '{print $4}')
-
-            # Just in case we were running this command with rest-server switched on, get again the address
-            ADDRESS=$(./gaiacli keys show ${ACCOUNT} --home . --address)
-            echo ${PASSWORD} | ./gaiacli tx send ${DESTINATION} ${AMOUNT} --home . --from ${ADDRESS} --chain-id=${NETWORK}
-
-            # Remove this address from the ones that needs money
-            aws s3 rm s3://cosmos-gaia/addresses/${DESTINATION}
-        done
-
-        #restore the rest server
-        screen -dmS rest ./gaiacli rest-server --laddr tcp://0.0.0.0:${API_PORT} --home . --node http://localhost:${PORT} --chain-id ${NETWORK} --trust-node true
-    fi
-done
