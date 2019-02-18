1c2112a04ccca2c093970a612153423fe1793648
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-02-18 18:04:53 +0100

    Fix ec2 scripts with new cli commands versions (#2002)
    * fix ec2 scripts with new cli commands versions
    * split faucet into separate file, it may even run in a separate machine

--- .aws/others.sh
+++ .aws/others.sh
@@ -4,14 +4,14 @@ PASSWORD=1234567890
 ACCOUNT=operator_account
 PORT=26656
 # TODO: hardcoded temporary, this will become a parameter coming from the first ECS instance
-MAINNODEID=d08c69fa85969f3d0173f23aadc40268559d0f66
+MAINNODEID=a27e12bd071dc2eda3117a5a255743103259e184
 MAINNODEIP=172.31.35.89
 MAINACCOUNT=main_account
 NETWORK=testnet
 VALIDATOR_AMOUNT=10stake
 
 # Initialize local node with a secondary account
-./gaiad init --home . --moniker ${ACCOUNT} --chain-id ${NETWORK}
+./gaiad init ${ACCOUNT} --home . --chain-id ${NETWORK}
 
 GENESIS=`aws s3 ls s3://cosmos-gaia/genesis.json | grep genesis.json`
 while [[ -z "$GENESIS" ]]; do
