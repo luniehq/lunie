8a03291a04dcd783a71ac5f4296e3ed91ea82798
Jordan Bibla <jbibla@gmail.com>
2019-02-26 16:03:28 -0500

    simplify modal component for sessions

--- app/src/renderer/components/common/TmModal.vue
+++ app/src/renderer/components/common/TmModal.vue
@@ -1,24 +1,12 @@
 <template>
   <div :class="cssClass" @click.self="close()">
-    <div class="tm-modal-container">
-      <header class="tm-modal-header">
-        <div v-if="icon" class="tm-modal-icon">
-          <i class="material-icons">{{ icon }}</i>
-        </div>
-        <div class="tm-modal-title">
-          <slot name="title" />
-        </div>
-        <div class="tm-modal-icon tm-modal-close" @click="close()">
-          <i v-if="close" class="material-icons">close</i>
-        </div>
-      </header>
-      <main class="tm-modal-main">
-        <slot />
-        <footer class="tm-modal-footer">
-          <slot name="footer" />
-        </footer>
-      </main>
-    </div>
+    <main class="tm-modal-main">
+      <slot name="main" />
+    </main>
+
+    <footer class="tm-modal-footer">
+      <slot name="footer" />
+    </footer>
   </div>
 </template>
 
