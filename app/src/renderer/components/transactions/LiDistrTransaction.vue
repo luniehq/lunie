29505ed8f381f898ae8527b08eff69015a50dca2
Federico Kunze <fekunze@berkeley.edu>
2019-03-05 10:35:17 +0100

    lint

--- app/src/renderer/components/transactions/LiDistrTransaction.vue
+++ app/src/renderer/components/transactions/LiDistrTransaction.vue
@@ -44,151 +44,151 @@ import LiTransaction from "./LiTransaction"
 import { pretty, atoms } from "../../scripts/num.js"
 
 export default {
-	name: `li-distr-transaction`,
-	components: { LiTransaction },
-	props: {
-		transaction: {
-			type: Object,
-			required: true
-		},
-		url: {
-			type: String,
-			required: true
-		},
-		bondingDenom: {
-			type: String,
-			required: true
-		},
-		txType: {
-			type: String,
-			required: true
-		},
-		validators: {
-			type: Array,
-			required: true
-		}
-	},
-	data: () => ({
-		atoms,
-		pretty
-	}),
-	computed: {
-		tx() {
-			return this.transaction.tx.value.msg[0].value
-		}
-	},
-	methods: {
-		moniker(validatorAddr) {
-			const validator = this.validators.find(
-				c => c.operator_address === validatorAddr
-			)
-			return validator ? validator.description.moniker : validatorAddr
-		}
-	}
+  name: `li-distr-transaction`,
+  components: { LiTransaction },
+  props: {
+    transaction: {
+      type: Object,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    bondingDenom: {
+      type: String,
+      required: true
+    },
+    txType: {
+      type: String,
+      required: true
+    },
+    validators: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    atoms,
+    pretty
+  }),
+  computed: {
+    tx() {
+      return this.transaction.tx.value.msg[0].value
+    }
+  },
+  methods: {
+    moniker(validatorAddr) {
+      const validator = this.validators.find(
+        c => c.operator_address === validatorAddr
+      )
+      return validator ? validator.description.moniker : validatorAddr
+    }
+  }
 }
 </script>
 
 <style>
 .li-tx {
-	display: flex;
-	font-size: sm;
+  display: flex;
+  font-size: sm;
 }
 
 .li-tx .tx-icon {
-	padding: 0 0.5rem;
-	background: var(--app-fg);
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  padding: 0 0.5rem;
+  background: var(--app-fg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .li-tx .tx-container {
-	flex-direction: column;
-	flex-wrap: nowrap;
-	padding: 0.5rem 0;
-	margin: 0.5rem 0;
-	display: flex;
-	width: 100%;
-	min-width: 0;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  padding: 0.5rem 0;
+  margin: 0.5rem 0;
+  display: flex;
+  width: 100%;
+  min-width: 0;
 }
 
 .li-tx .tx-element {
-	padding: 0 2rem 0 1.5rem;
-	line-height: 1.5rem;
+  padding: 0 2rem 0 1.5rem;
+  line-height: 1.5rem;
 }
 
 .li-tx .tx-coin .value {
-	flex: 0 0 100%;
-	font-size: sm;
-	color: var(--dim);
+  flex: 0 0 100%;
+  font-size: sm;
+  color: var(--dim);
 }
 
 .li-tx .tx-coin .value::before {
-	content: "";
-	display: inline;
+  content: "";
+  display: inline;
 }
 
 .li-tx .tx-coin .key {
-	font-weight: 500;
-	font-size: m;
+  font-weight: 500;
+  font-size: m;
 }
 
 .li-tx .tx-coin .value,
 .li-tx .tx-coin .key {
-	line-height: 1.5rem;
+  line-height: 1.5rem;
 }
 
 .li-tx .tx-address {
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	color: var(--dim);
-	font-size: sm;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: var(--dim);
+  font-size: sm;
 }
 
 .li-tx.li-tx-sent .tx-coin .value::before {
-	content: "-";
+  content: "-";
 }
 
 .li-tx.li-tx-received .tx-icon {
-	background: var(--app-fg);
+  background: var(--app-fg);
 }
 
 .li-tx.li-tx-received .tx-coin .value {
-	color: success;
+  color: success;
 }
 
 .li-tx.li-tx-received .tx-coin .value::before {
-	content: "+";
+  content: "+";
 }
 
 .li-tx:hover {
-	cursor: pointer;
+  cursor: pointer;
 }
 
 @media screen and (min-width: 700px) {
-	.li-tx {
-		font-size: 0.875rem;
-	}
+  .li-tx {
+    font-size: 0.875rem;
+  }
 
-	.li-tx .tx-container {
-		flex-direction: row;
-	}
+  .li-tx .tx-container {
+    flex-direction: row;
+  }
 
-	.li-tx .tx-container .tx-coins {
-		flex: 0 0 9rem;
-		padding: 0;
-		min-width: 0;
-	}
+  .li-tx .tx-container .tx-coins {
+    flex: 0 0 9rem;
+    padding: 0;
+    min-width: 0;
+  }
 
-	.li-tx .tx-container .tx-coins .tx-coin {
-		padding: 0 1.5rem 0;
-	}
+  .li-tx .tx-container .tx-coins .tx-coin {
+    padding: 0 1.5rem 0;
+  }
 
-	.li-tx .tx-container .tx-coins .tx-coin .key {
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
+  .li-tx .tx-container .tx-coins .tx-coin .key {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 }
 </style>
