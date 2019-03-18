31cea9345e3fa97909d051e571a853dc2f983611
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2019-03-18 12:23:51 +0100

    Update tasks/build/Gaia/COMMIT.sh
    Co-Authored-By: faboweb <frznhope@gmail.com>

--- tasks/build/Gaia/COMMIT.sh
+++ tasks/build/Gaia/COMMIT.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# This is the commit of the SDK version to use for building Gaia.  We use an
+# This is the SDK release version to use for building Gaia.
 # explicit hash instead of a tag so we don't have to trust GitHub.
 export COMMIT=v0.33.0
