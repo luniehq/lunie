beae12433765a395ccad8a4240e6190b7dece395
Fabian Weber <frznhope@gmail.com>
2019-03-20 13:05:29 +0100

    throttle keybase requests

---
+++ PENDING.md
@@ -0,0 +1,3 @@
+### Fixed
+
+- [\#2301](https://github.com/cosmos/voyager/issues/2301) throttle requests for keybase identities @faboweb
--- app/src/renderer/vuex/modules/keybase.js
+++ app/src/renderer/vuex/modules/keybase.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import moment from "moment"
 import * as Sentry from "@sentry/browser"
 
 export default () => {
@@ -16,7 +17,9 @@ export default () => {
   const mutations = {
     setKeybaseIdentities(state, identities) {
       identities.forEach(identity => {
-        state.identities[identity.keybaseId] = identity
+        state.identities[identity.keybaseId] = identity || {}
+        state.identities[identity.keybaseId].lastUpdated =
+          new Date().toUTCString()
       })
     }
   }
@@ -24,22 +27,17 @@ export default () => {
   const actions = {
     async getKeybaseIdentity({ state }, keybaseId) {
       if (!/.{16}/.test(keybaseId)) return // the keybase id is not correct
-      if (state.identities[keybaseId]) return state.identities[keybaseId] // we already have this identity
-
-      const urlPrefix = `https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=`
-      const fullUrl = urlPrefix + keybaseId
-      const json = await state.externals.axios(fullUrl)
-      if (json.data.status.name === `OK`) {
-        const user = json.data.them[0]
-        if (user && user.pictures && user.pictures.primary) {
-          return {
-            keybaseId,
-            avatarUrl: user.pictures.primary.url,
-            userName: user.basics.username,
-            profileUrl: `https://keybase.io/` + user.basics.username
-          }
+      if (state.identities[keybaseId]) { // we already have this identity
+        // check if the last check is more then 2 days ago
+        if (moment.duration(moment(state.identities[keybaseId].lastUpdated).diff(moment(), `days`)) >= 2) {
+          return state.identities[keybaseId]
         }
+
+        return lookupUsername(state, state.identities[keybaseId].userName)
+        // https://keybase.io/_/api/1.0/user/lookup.json?usernames=chris,max
       }
+
+      return lookupId(state, keybaseId)
     },
     async getKeybaseIdentities({ dispatch, commit, state }, validators) {
       try {
@@ -59,10 +57,6 @@ export default () => {
         state.loaded = true
         commit(`setKeybaseIdentities`, identities.filter(x => !!x))
       } catch (error) {
-        commit(`notifyError`, {
-          title: `Error fetching keybase information for validators`,
-          body: error.message
-        })
         Sentry.captureException(error)
         state.error = error
       }
@@ -73,5 +67,37 @@ export default () => {
     state,
     actions,
     mutations
+  }
+}
+
+async function lookupId(state, keybaseId) {
+  const urlPrefix = `https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=`
+  const fullUrl = urlPrefix + keybaseId
+  const json = await state.externals.axios(fullUrl)
+  if (json.data.status.name === `OK`) {
+    const user = json.data.them[0]
+    if (user && user.pictures && user.pictures.primary) {
+      return {
+        avatarUrl: user.pictures.primary.url,
+        userName: user.basics.username,
+        profileUrl: `https://keybase.io/` + user.basics.username
+      }
+    }
+  }
+}
+
+async function lookupUsername(state, username) {
+  const urlPrefix = `https://keybase.io/_/api/1.0/user/lookup.json?usernames=`
+  const fullUrl = urlPrefix + username
+  const json = await state.externals.axios(fullUrl)
+  if (json.data.status.name === `OK`) {
+    const user = json.data.them[0]
+    if (user && user.pictures && user.pictures.primary) {
+      return {
+        avatarUrl: user.pictures.primary.url,
+        userName: user.basics.username,
+        profileUrl: `https://keybase.io/` + user.basics.username
+      }
+    }
   }
 }
