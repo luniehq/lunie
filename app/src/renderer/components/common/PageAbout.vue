8297950f65604cd5f654e276836a19acca6fbbb5
Fabian <frznhope@gmail.com>
2019-03-30 13:57:24 +0100

    changed styling

--- app/src/renderer/components/common/PageAbout.vue
+++ app/src/renderer/components/common/PageAbout.vue
@@ -1,31 +1,33 @@
 <template>
   <tm-page data-title="About" hide-header>
     <div class="container">
-      <h1>About</h1>
-      <p>
-        Lunie was the <b>first</b> visual interface ever build for the Cosmos
-        network. We develope Lunie putting the <b>user first</b>. We try to <b>
-          reduce the complexity
-        </b> of blockchain to minimum so you can <b>focus</b> on what you really
-        aim to do. We want to keep Lunie <b>Open Source</b> so the community can
-        grow from the learnings we have had already.
-        The developers of Lunie accompanied the Cosmos SDK team to build
-        the amazing
-        tooling that exist today for building visual interfaces for Cosmos.
-        We want to continue assisting developement in the network so we all
-        can benefit. We want to keep Lunie <b>independent</b> from validators so
-        there is an entity you can trust.
-      </p>
-      <br>
-      <br>
-      <h1>Contact</h1>
-      <p>
-        Lunie International Software Systems inc.<br>
-        Sesame Street 1<br>
-        123 Funland<br>
-        Canada<br>
-        contact@lunie.io<br>
-      </p>
+      <div class="card">
+        <h1>About</h1>
+        <p>
+          Lunie was the <b>first</b> visual interface ever build for the Cosmos
+          network. We develope Lunie putting the <b>user first</b>. We try to <b>
+            reduce the complexity
+          </b> of blockchain to minimum so you can <b>focus</b> on what you really
+          aim to do. We want to keep Lunie <b>Open Source</b> so the community can
+          grow from the learnings we have had already.
+          The developers of Lunie accompanied the Cosmos SDK team to build
+          the amazing
+          tooling that exist today for building visual interfaces for Cosmos.
+          We want to continue assisting developement in the network so we all
+          can benefit. We want to keep Lunie <b>independent</b> from validators so
+          there is an entity you can trust.
+        </p>
+      </div>
+      <div class="card">
+        <h1>Contact</h1>
+        <p>
+          Lunie International Software Systems inc.<br>
+          Sesame Street 1<br>
+          123 Funland<br>
+          Canada<br>
+          contact@lunie.io<br>
+        </p>
+      </div>
     </div>
   </tm-page>
 </template>
@@ -49,7 +51,10 @@ b {
   font-weight: 700;
   color: var(--link);
 }
-.container {
-  padding: 1rem;
+.card {
+  background: var(--app-fg);
+  padding: 4rem;
+  margin: 1rem;
+  border-radius: 0.25rem;
 }
 </style>
