fb8dc4e77cf17e29e3ed4c44e925acf556ada8c0
Fabian <frznhope@gmail.com>
2019-04-01 15:29:35 +0200

    resolving errors

--- app/src/renderer/vuex/modules/send.js
+++ app/src/renderer/vuex/modules/send.js
@@ -202,7 +202,8 @@ export default ({ node }) => {
 
       // Sometimes we get back failed transactions, which shows only by them having a `code` property
       if (res.code) {
-        throw new Error(`Something went wrong while sending the transaction.`)
+        // TODO get message from SDK: https://github.com/cosmos/cosmos-sdk/issues/4013
+        throw new Error(`Error sending: ${getErrorMessage(Number(res.code))}`)
       }
 
       // sync success
@@ -269,4 +270,45 @@ function handleSDKError(err) {
     message = err.message
   }
   throw new Error(message)
+}
+
+function getErrorMessage(code) {
+  switch (code) {
+    case 1:
+      return "internal error"
+    case 2:
+      return "tx parse error"
+    case 3:
+      return "invalid sequence"
+    case 4:
+      return "unauthorized"
+    case 5:
+      return "insufficient funds"
+    case 6:
+      return "unknown request"
+    case 7:
+      return "invalid address"
+    case 8:
+      return "invalid pubkey"
+    case 9:
+      return "unknown address"
+    case 10:
+      return "insufficient coins"
+    case 11:
+      return "invalid coins"
+    case 12:
+      return "out of gas"
+    case 13:
+      return "memo too large"
+    case 14:
+      return "insufficient fee"
+    case 15:
+      return "maximum number of signatures exceeded"
+    case 16:
+      return "amount of gas exceeded maximum allowed size"
+    case 17:
+      return "no signatures supplied"
+    default:
+      return "unknown error"
+  }
 }
