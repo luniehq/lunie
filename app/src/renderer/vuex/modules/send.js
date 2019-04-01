51a7ac24de9326127f3d0650528b08d21348e828
Fabian <frznhope@gmail.com>
2019-04-01 15:47:51 +0200

    refactored

--- app/src/renderer/vuex/modules/send.js
+++ app/src/renderer/vuex/modules/send.js
@@ -7,6 +7,7 @@ import {
 } from "../../scripts/wallet.js"
 import { getKey } from "../../scripts/keystore"
 import { signatureImport } from "secp256k1"
+import { getErrorMessage } from "../../scripts/sdk-errors"
 
 export default ({ node }) => {
   const state = {
@@ -270,45 +271,4 @@ function handleSDKError(err) {
     message = err.message
   }
   throw new Error(message)
-}
-
-function getErrorMessage(code) {
-  switch (code) {
-    case 1:
-      return "internal error"
-    case 2:
-      return "tx parse error"
-    case 3:
-      return "invalid sequence"
-    case 4:
-      return "unauthorized"
-    case 5:
-      return "insufficient funds"
-    case 6:
-      return "unknown request"
-    case 7:
-      return "invalid address"
-    case 8:
-      return "invalid pubkey"
-    case 9:
-      return "unknown address"
-    case 10:
-      return "insufficient coins"
-    case 11:
-      return "invalid coins"
-    case 12:
-      return "out of gas"
-    case 13:
-      return "memo too large"
-    case 14:
-      return "insufficient fee"
-    case 15:
-      return "maximum number of signatures exceeded"
-    case 16:
-      return "amount of gas exceeded maximum allowed size"
-    case 17:
-      return "no signatures supplied"
-    default:
-      return "unknown error"
-  }
 }
