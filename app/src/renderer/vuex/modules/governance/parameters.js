32e61a29f7c6a1038d632c6b7b60b45467d2ea4f
Fabian <frznhope@gmail.com>
2019-04-06 22:53:02 +0200

    fixed issues

--- app/src/renderer/vuex/modules/governance/parameters.js
+++ app/src/renderer/vuex/modules/governance/parameters.js
@@ -20,6 +20,10 @@ export default ({ node }) => {
   }
 
   const actions = {
+    initializeWallet({ dispatch }) {
+      // needed for deposit denom for governance
+      dispatch(`getGovParameters`)
+    },
     async getGovParameters({ state, commit, rootState }) {
       state.loading = true
 
