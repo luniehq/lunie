8ea469bf871da5febfdb554bf44f3e289abe934d
Fabian <frznhope@gmail.com>
2019-04-24 11:15:11 +0200

    added test for alert on remote set in prod

--- app/src/helpers/url.js
+++ app/src/helpers/url.js
@@ -1,4 +1,4 @@
-export function getURLParams(window) {
+export function getURLParams(window, env = process.env.NODE_ENV) {
   const queries = window.location.search.slice(1).split(`&`)
   const parameters = queries.reduce((config, current) => {
     const [name, value] = current.split(`=`)
@@ -11,7 +11,7 @@ export function getURLParams(window) {
     return config
   }, {})
 
-  if (process.env.NODE_ENV === `production` && (parameters.stargate || parameters.rpc)) {
+  if (env === `production` && (parameters.stargate || parameters.rpc)) {
     alert(`The ability to set the remote stargate and full node was removed in production to improve security.`)
   }
 
