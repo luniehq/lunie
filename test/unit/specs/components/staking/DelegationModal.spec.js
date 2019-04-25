f0b80b58d2e148b2f561ad23bd859294449af8cc
Fabian <frznhope@gmail.com>
2019-04-25 04:29:31 +0200

    fix test

--- test/unit/specs/components/staking/DelegationModal.spec.js
+++ test/unit/specs/components/staking/DelegationModal.spec.js
@@ -270,10 +270,7 @@ describe(`DelegationModal`, () => {
 
       expect($store.dispatch).toHaveBeenCalledWith(`simulateRedelegation`,
         {
-          amount: {
-            amount: `50000000`,
-            denom: `uatom`,
-          },
+          amount: `50000000`,
           validatorSrc: delegates.delegates[0],
           validatorDst: validator
         }
