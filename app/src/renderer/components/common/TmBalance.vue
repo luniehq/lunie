259c297eed2ca270db31fa49c8374c50c467630c
Jordan Bibla <jbibla@gmail.com>
2019-04-25 16:42:01 -0400

    removed "pending" from balance header because it's kind of misleading

--- app/src/renderer/components/common/TmBalance.vue
+++ app/src/renderer/components/common/TmBalance.vue
@@ -13,7 +13,7 @@
         <h2>{{ unbondedAtoms }}</h2>
       </div>
       <div v-if="rewards" class="top-section">
-        <h3>Pending Rewards</h3>
+        <h3>Rewards</h3>
         <h2>{{ rewards }}</h2>
         <tm-btn
           id="withdraw-btn"
@@ -156,7 +156,7 @@ export default {
 .withdraw-rewards {
   font-size: var(--sm);
   position: absolute;
-  font-weight: 300;
+  font-weight: 500;
 }
 
 @media screen and (max-width: 767px) {
