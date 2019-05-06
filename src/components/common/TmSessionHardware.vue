5e60db1810cbf7f961a1cdb2a10b63594d8ad2f5
Fabian <frznhope@gmail.com>
2019-05-06 14:53:32 +0200

    linted

--- src/components/common/TmSessionHardware.vue
+++ src/components/common/TmSessionHardware.vue
@@ -19,11 +19,13 @@
           </template>
           <template v-if="status === `confirmAddress`">
             Signin in with the address
-            <span class="address">{{ address }}</span
-            >.<br />
+            <span class="address">{{ address }}</span>.<br />
             Please confirm on your Ledger.
           </template>
-          <p v-if="connectionError" class="error-message">
+          <p
+            v-if="connectionError"
+            class="error-message"
+          >
             {{ connectionError }}
           </p>
         </HardwareState>
@@ -36,8 +38,7 @@
             target="_blank"
             rel="noopener noreferrer"
           >
-            here </a
-          >.
+            here </a>.
         </p>
         <TmBtn
           :value="submitCaption"
@@ -54,7 +55,7 @@ import TmBtn from "common/TmBtn"
 import HardwareState from "common/TmHardwareState"
 export default {
   name: `tm-session-hardware`,
-  components: { TmBtn, HardwareState, ShortBech32 },
+  components: { TmBtn, HardwareState },
   data: () => ({
     status: `connect`,
     connectionError: null,
