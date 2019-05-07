de9d699f67a80d23ed86f3ee3a8360eee61abe38
Fabian <frznhope@gmail.com>
2019-05-07 12:53:23 +0200

    sign in always

--- test/e2e/globals.js
+++ test/e2e/globals.js
@@ -21,7 +21,7 @@ module.exports = {
     }
   },
   async after() {
-    console.log('after global');
+    console.log('taring down testnet');
     await exec("npm run testnet:stop")
   },
 
@@ -39,6 +39,20 @@ module.exports = {
       )
       return true
     }, [])
+
+    // sign in to rich account
+    browser
+      .url(browser.launch_url + "?insecure=true")
+      .waitForElementVisible(`body`)
+      .waitForElementVisible(`#app-content`)
+      .click(".sign-in-button")
+      .waitForElementVisible("#session-welcome")
+      .click("#sign-in-with-account")
+      .waitForElementVisible("#sign-in-name")
+      .click("#sign-in-name option[value=rich_account]")
+      .setValue("#sign-in-password", "1234567890")
+      .click(".tm-session-footer button")
+      .waitForElementVisible("#signOut-btn")
     done()
   },
   /**
