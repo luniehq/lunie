0e5ed065f7f265a4643a9484c60b39a1b0b01a7c
colw <colw@outlook.com>
2019-05-10 17:26:55 +0200

    Improve readability of rounding / sum code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,31 +1,40 @@
 // This will take an object and for each (k,v) will return
 // v rounded such that the sum of all v is 100.
+
+// Used the following as a reference:
+// https://stackoverflow.com/questions/13483430/how-to-make-rounded-percentages-add-up-to-100
+// Note: We pass an object, and want to keep the (key, value) association.
+
 export const roundObjectPercentages = dataMap => {
   // This algorithm workson integers and we want 2 decimal places.
   // So round up first.
+  const scale = 100
   let asArray = Object.entries(dataMap).map(([key, value]) => {
-    const newValue = value * 100
-    return [key, newValue]
+    return [key, value * scale]
   })
 
   const sumRounded = (acc, x) => {
     return acc + Math.round(x[1])
   }
 
-  // Calculate difference from 100%
-  const scale = 100
-  var margin = scale * 100 - asArray.reduce(sumRounded, 0)
+  // The leftOver is the difference beween 100 and
+  // the sum of the rounded values.
+  var leftOver = scale * 100 - asArray.reduce(sumRounded, 0)
 
+  //
   const cmpNumberValue = (a, b) => {
     return b[1] - Math.round(a[1]) - a[1]
   }
 
-  // Sort and distribute difference amongst values
+  // Here we distribute the leftOver as evenly as possible amongst the rounded values.
+  // The values are sorted first.
   asArray.sort(cmpNumberValue)
   const result = asArray.map(function(x, i) {
+    // Note: leftOver can be negative.
     const rounded = [
       x[0],
-      (Math.round(x[1]) + (margin > i) - (i >= asArray.length + margin)) / 100.0
+      (Math.round(x[1]) + (leftOver > i) - (i >= asArray.length + leftOver)) /
+        100.0
     ]
     return rounded
   })
