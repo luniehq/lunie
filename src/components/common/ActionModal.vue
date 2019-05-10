cf433f667b9df310f9be6d43032fc8b391247ee9
Fabian <frznhope@gmail.com>
2019-05-10 18:25:55 +0200

    fix action modal never disabled cross browser

--- src/components/common/ActionModal.vue
+++ src/components/common/ActionModal.vue
@@ -1,6 +1,14 @@
 <template>
-  <transition v-if="show" name="slide-fade">
-    <div v-focus-last class="action-modal" tabindex="0" @keyup.esc="close">
+  <transition
+    v-if="show"
+    name="slide-fade"
+  >
+    <div
+      v-focus-last
+      class="action-modal"
+      tabindex="0"
+      @keyup.esc="close"
+    >
       <div class="action-modal-header">
         <img
           class="icon action-modal-atom"
@@ -17,13 +25,22 @@
           <i class="material-icons">close</i>
         </div>
       </div>
-      <div v-if="requiresSignIn" class="action-modal-form">
+      <div
+        v-if="requiresSignIn"
+        class="action-modal-form"
+      >
         <p>You need to sign in to submit a transaction.</p>
       </div>
-      <div v-else-if="step === `txDetails`" class="action-modal-form">
+      <div
+        v-else-if="step === `txDetails`"
+        class="action-modal-form"
+      >
         <slot />
       </div>
-      <div v-else-if="step === `fees`" class="action-modal-form">
+      <div
+        v-else-if="step === `fees`"
+        class="action-modal-form"
+      >
         <TmFormGroup
           v-if="session.experimentalMode"
           :error="$v.gasPrice.$error && $v.gasPrice.$invalid"
@@ -71,7 +88,10 @@
           :max="atoms(balance)"
         />
       </div>
-      <div v-else-if="step === `sign`" class="action-modal-form">
+      <div
+        v-else-if="step === `sign`"
+        class="action-modal-form"
+      >
         <TmFormGroup
           v-if="signMethods.length > 1"
           class="action-modal-form-group"
@@ -92,9 +112,9 @@
         >
           <div v-if="session.browserWithLedgerSupport">
             {{
-              sending
-                ? `Please verify and sign the transaction on your Ledger`
-                : `Please plug in your Ledger&nbsp;Nano&nbsp;S and open
+            sending
+            ? `Please verify and sign the transaction on your Ledger`
+            : `Please plug in your Ledger&nbsp;Nano&nbsp;S and open
             the Cosmos app`
             }}
           </div>
@@ -160,7 +180,7 @@
                 v-else
                 color="primary"
                 value="Submit"
-                :disabled="!browserWithLedgerSupport"
+                :disabled="!session.browserWithLedgerSupport"
                 @click.native="validateChangeStep"
               />
             </div>
