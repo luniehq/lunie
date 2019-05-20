b5c9d136ea9433643e135f3cfd8d295ac09ae236
Fabian <frznhope@gmail.com>
2019-05-20 22:57:13 +0200

    remove dead wallet code

--- test/unit/specs/scripts/wallet.spec.js
+++ test/unit/specs/scripts/wallet.spec.js
@@ -1,13 +1,10 @@
 import {
   createCosmosAddress,
-  sign,
-  createSignature,
   createSignMessage,
   generateWalletFromSeed,
   generateSeed,
   generateWallet,
-  createSignedTx,
-  createBroadcastBody
+  signWithPrivateKey
 } from "scripts/wallet.js"
 
 describe(`Key Generation`, () => {
@@ -125,123 +122,24 @@ describe(`Signing`, () => {
     memo: ``
   }
 
-  it(`should create an Object containing the signature and pubkey`, () => {
+  it(`should create a correct signature`, () => {
     const vectors = [
       {
-        sequence: `0`,
-        account_number: `1`,
-        signature: `MEQCIE2f8y5lVAOZu/MDZX3aH+d0sgvTRVrEzdP60NHr7lKJAiBexCiaAsh35R25IhgJMBIp/AD2Lfuk57suV8gnqOSfzg==`,
-        publicKey: `03ab1ebbb21aee35154e36aaebc25067177f783f7e967c9d6493e8920c05e40eb5`
-      },
-      {
-        sequence: `1`,
-        account_number: `1`,
-        signature: `MEQCIE2f8y5lVAOZu/MDZX3aH+d0sgvTRVrEzdP60NHr7lKJAiBexCiaAsh35R25IhgJMBIp/AD2Lfuk57suV8gnqOSfzg==`,
-        publicKey: `0243311589af63c2adda04fcd7792c038a05c12a4fe40351b3eb1612ff6b2e5a0e`
-      }
-    ]
-
-    vectors.forEach(({ signature, sequence, account_number, publicKey }) =>
-      expect(
-        createSignature(signature, sequence, account_number, publicKey)
-      ).toMatchObject({
-        signature: signature.toString(`base64`),
-        account_number,
-        sequence,
-        pub_key: {
-          type: `tendermint/PubKeySecp256k1`,
-          value: publicKey.toString(`base64`)
-        }
-      })
-    )
-  })
-
-  it(`should create the correct string to sign`, () => {
-    const vectors = [
-      {
-        tx,
-        sequence: `0`,
-        account_number: `1`,
-        chain_id: `tendermint_test`,
-        signMessage: `{"account_number":"1","chain_id":"tendermint_test","fee":{"amount":[{"amount":"0","denom":""}],"gas":"21906"},"memo":"","msgs":[{"type":"cosmos-sdk/Send","value":{"inputs":[{"address":"cosmos1qperwt9wrnkg5k9e5gzfgjppzpqhyav5j24d66","coins":[{"amount":"1","denom":"STAKE"}]}],"outputs":[{"address":"cosmos1yeckxz7tapz34kjwnjxvmxzurerquhtrmxmuxt","coins":[{"amount":"1","denom":"STAKE"}]}]}}],"sequence":"0"}`
-      },
-      {
-        tx: txWithNulls,
-        sequence: `0`,
-        account_number: `1`,
-        chain_id: `tendermint_test`,
-        signMessage: `{"account_number":"1","chain_id":"tendermint_test","fee":{"amount":[{"amount":"0","denom":""}],"gas":"21906"},"memo":"","msgs":[{"type":"cosmos-sdk/Send","value":{"inputs":[{"address":"cosmos1qperwt9wrnkg5k9e5gzfgjppzpqhyav5j24d66","coins":[{"amount":"1","denom":"STAKE"}]}],"outputs":[{"address":"cosmos1yeckxz7tapz34kjwnjxvmxzurerquhtrmxmuxt","coins":[{"amount":"1","denom":"STAKE"}]}]}}],"sequence":"0"}`
-      }
-    ]
-
-    vectors.forEach(
-      ({ tx, sequence, account_number, chain_id, signMessage }) => {
-        expect(
-          createSignMessage(tx, { sequence, account_number, chain_id })
-        ).toBe(signMessage)
-      }
-    )
-  })
-
-  it(`should create a correct tx signature object`, () => {
-    const vectors = [
-      {
-        wallet: {
-          privateKey: `2afc5a66b30e7521d553ec8e6f7244f906df97477248c30c103d7b3f2c671fef`,
-          publicKey: `03ab1ebbb21aee35154e36aaebc25067177f783f7e967c9d6493e8920c05e40eb5`
-        },
-        tx,
-        signature: `YjJhlAf7aCnUtLyBNDp9e6LKuNgV7hJC3rmm0Wro5nBsIPVtWzjuobsp/AhR5Kht+HcRF2zBq4AfoNQMIbY6fw==`,
-        sequence: `0`,
-        account_number: `1`,
-        chain_id: `tendermint_test`,
-        pub_key: {
-          type: `tendermint/PubKeySecp256k1`,
-          value: `A6seu7Ia7jUVTjaq68JQZxd/eD9+lnydZJPokgwF5A61`
-        }
+        privateKey: `2afc5a66b30e7521d553ec8e6f7244f906df97477248c30c103d7b3f2c671fef`,
+        signMessage: `{"account_number":"1","chain_id":"tendermint_test","fee":{"amount":[{"amount":"0","denom":""}],"gas":"21906"},"memo":"","msgs":[{"type":"cosmos-sdk/Send","value":{"inputs":[{"address":"cosmos1qperwt9wrnkg5k9e5gzfgjppzpqhyav5j24d66","coins":[{"amount":"1","denom":"STAKE"}]}],"outputs":[{"address":"cosmos1yeckxz7tapz34kjwnjxvmxzurerquhtrmxmuxt","coins":[{"amount":"1","denom":"STAKE"}]}]}}],"sequence":"0"}`,
+        signature: `YjJhlAf7aCnUtLyBNDp9e6LKuNgV7hJC3rmm0Wro5nBsIPVtWzjuobsp/AhR5Kht+HcRF2zBq4AfoNQMIbY6fw==`
       }
     ]
 
     vectors.forEach(
       ({
-        wallet,
-        tx,
-        pub_key: expectedPubKey,
-        signature: expectedSignature,
-        sequence,
-        account_number,
-        chain_id
+        privateKey,
+        signMessage,
+        signature: expectedSignature
       }) => {
-        const sigObject = sign(tx, wallet, {
-          sequence,
-          account_number,
-          chain_id
-        })
-        expect(sigObject).toEqual({
-          signature: expectedSignature,
-          pub_key: expectedPubKey,
-          sequence,
-          account_number
-        })
+        const signature = signWithPrivateKey(signMessage, privateKey)
+        expect(signature.toString("base64")).toEqual(expectedSignature)
       }
-    )
-  })
-
-  it(`attaches a signature`, () => {
-    const tx = {}
-    const sig = { x: 1 }
-    expect(createSignedTx(tx, sig)).toEqual({
-      signatures: [{ x: 1 }]
-    })
-  })
-
-  it(`creates a broadcast body`, () => {
-    const signedTx = { x: 1 }
-    expect(createBroadcastBody(signedTx)).toEqual(
-      JSON.stringify({
-        tx: { x: 1 },
-        mode: `sync`
-      })
     )
   })
 })
