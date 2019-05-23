243307290ddb7521413a0a8fee97ce2d526dbc57
Fabian <frznhope@gmail.com>
2019-05-23 09:55:08 +0200

    linted

--- src/components/common/ActionModal.vue
+++ src/components/common/ActionModal.vue
@@ -1,14 +1,6 @@
 <template>
-  <transition
-    v-if="show"
-    name="slide-fade"
-  >
-    <div
-      v-focus-last
-      class="action-modal"
-      tabindex="0"
-      @keyup.esc="close"
-    >
+  <transition v-if="show" name="slide-fade">
+    <div v-focus-last class="action-modal" tabindex="0" @keyup.esc="close">
       <div class="action-modal-header">
         <span class="action-modal-title">
           {{ requiresSignIn ? `Sign in required` : title }}
@@ -21,26 +13,14 @@
           <i class="material-icons">close</i>
         </div>
       </div>
-      <Steps
-        :steps="['Details', 'Fees', 'Sign']"
-        :active="step"
-      />
-      <div
-        v-if="requiresSignIn"
-        class="action-modal-form"
-      >
+      <Steps :steps="['Details', 'Fees', 'Sign']" :active="step" />
+      <div v-if="requiresSignIn" class="action-modal-form">
         <p>You need to sign in to submit a transaction.</p>
       </div>
-      <div
-        v-else-if="step === `details`"
-        class="action-modal-form"
-      >
+      <div v-else-if="step === `details`" class="action-modal-form">
         <slot />
       </div>
-      <div
-        v-else-if="step === `fees`"
-        class="action-modal-form"
-      >
+      <div v-else-if="step === `fees`" class="action-modal-form">
         <TmFormGroup
           v-if="session.experimentalMode"
           :error="$v.gasPrice.$error && $v.gasPrice.$invalid"
@@ -88,10 +68,7 @@
           :max="atoms(balance)"
         />
       </div>
-      <div
-        v-else-if="step === `sign`"
-        class="action-modal-form"
-      >
+      <div v-else-if="step === `sign`" class="action-modal-form">
         <TmFormGroup
           v-if="signMethods.length > 1"
           class="action-modal-form-group"
@@ -112,9 +89,9 @@
         >
           <div v-if="session.browserWithLedgerSupport">
             {{
-            sending
-            ? `Please verify and sign the transaction on your Ledger`
-            : `Please plug in your Ledger&nbsp;Nano&nbsp;S and open
+              sending
+                ? `Please verify and sign the transaction on your Ledger`
+                : `Please plug in your Ledger&nbsp;Nano&nbsp;S and open
             the Cosmos app`
             }}
           </div>
