f669ff83f7222f2a70420df2b173d43ec6d6d5f6
Benji <macbook@MacBooks-MacBook-Pro.local>
2019-06-06 15:24:43 -0400

    Adds e2e test for 404ing on an invalid validator address

--- test/unit/specs/components/staking/PageValidator.spec.js
+++ test/unit/specs/components/staking/PageValidator.spec.js
@@ -152,6 +152,12 @@ describe(`PageValidator`, () => {
       expect(wrapper.exists(`tm-data-error-stub`)).toBe(true)
     })
 
+    it(`shows 404 page if invalid validator address used`, () => {
+      $store.getters.delegates.delegates = []
+
+      expect(wrapper.exists(`four-oh-four`)).toBe(true)
+    })
+
     it(`shows the selfBond`, () => {
       expect(wrapper.find(`#page-profile__self-bond`).text()).toBe(`1.00%`)
     })
