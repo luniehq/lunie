c469209fd240c351a74d3fb66f68f4c0aaa4de9c
Fabian <frznhope@gmail.com>
2019-06-19 13:23:43 +0200

    added message handling

--- src/background.js
+++ src/background.js
@@ -1,14 +1,70 @@
-const { loadKeys, importKey, testPassword } = require('scripts/keystore.js');
-const { getSeed } = require('@lunie/cosmos-keys');
+const { getWalletIndex, getStoredWallet, signWithPrivateKey, getNewWallet, storeWallet, getNewWalletFromSeed, removeWallet } = require('@lunie/cosmos-keys');
+const { createSignMessage } = require('@lunie/cosmos-api');
 
 global.browser = require('webextension-polyfill');
-
-// TODO intialise/load database on boot (put keystore into a module and make it use leveldb?)
 
 // TODO handle requests from sign-request-popup (message, password)
 
 // TODO handle requests from account-management-popup (create account | delete account | import account [| rename account])
 
+// main message handler
+chrome.runtime.onMessage.addListener((message, callback) => {
+  signMessageHandler(message, callback);
+  walletMessageHandler(message, callback);
+});
+
 // open request popup
 // chrome.tabs.create({ url: chrome.extension.getURL('./request/request.html') }, function(tab) {
 // });
+
+function signMessageHandler(message, callback) {
+  switch (message.type) {
+    case 'SIGN_REQUEST': {
+      const { stdTx, senderAddress } = message.payload;
+      const wallet = getWalletFromIndex(getWalletIndex(), senderAddress);
+      // ... Open or signal sign request
+      break;
+    }
+    case 'SIGN': {
+      const { stdTx, senderAddress, password, chainId, sequence, accountNumber } = message.payload;
+      const wallet = getStoredWallet(senderAddress, password);
+
+      const signMessage = createSignMessage(stdTx, { sequence, accountNumber, chainId });
+      const signature = signWithPrivateKey(signMessage, Buffer.from(wallet.privateKey, 'hex'));
+      // ... Send back to Lunie
+      break;
+    }
+  }
+}
+function walletMessageHandler(message, callback) {
+  switch (message.type) {
+    case 'GET_WALLETS': {
+      callback(getWalletIndex());
+      break;
+    }
+    case 'CREATE_WALLET': {
+      const { name, password } = message.payload;
+      const wallet = getNewWallet();
+      storeWallet(wallet, name, password);
+      callback();
+      break;
+    }
+    case 'IMPORT_WALLET': {
+      const { name, password, mnemonic } = message.payload;
+      const wallet = getNewWalletFromSeed(mnemonic);
+      storeWallet(wallet, name, password);
+      callback();
+      break;
+    }
+    case 'DELETE_WALLET': {
+      const { address, password } = message.payload;
+      removeWallet(address, password);
+      callback();
+      break;
+    }
+  }
+}
+
+function getWalletFromIndex(walletIndex, address) {
+  return walletIndex.find(({ address: storedAddress }) => storedAddress === address);
+}
