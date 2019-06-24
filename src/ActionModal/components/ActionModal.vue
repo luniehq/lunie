c7fa3ec93fdfbec1ab3459340292e0c9b2026d24
Fabian <frznhope@gmail.com>
2019-06-24 19:41:00 +0200

    linted

--- src/ActionModal/components/ActionModal.vue
+++ src/ActionModal/components/ActionModal.vue
@@ -1,14 +1,6 @@
 <template>
-  <transition
-    v-if="show"
-    name="slide-fade"
-  >
-    <div
-      v-focus-last
-      class="action-modal"
-      tabindex="0"
-      @keyup.esc="close"
-    >
+  <transition v-if="show" name="slide-fade">
+    <div v-focus-last class="action-modal" tabindex="0" @keyup.esc="close">
       <div
         id="closeBtn"
         class="action-modal-icon action-modal-close"
@@ -20,27 +12,15 @@
         <span class="action-modal-title">
           {{ requiresSignIn ? `Sign in required` : title }}
         </span>
-        <Steps
-          :steps="['Details', 'Fees', 'Sign']"
-          :active-step="step"
-        />
+        <Steps :steps="['Details', 'Fees', 'Sign']" :active-step="step" />
       </div>
-      <div
-        v-if="requiresSignIn"
-        class="action-modal-form"
-      >
+      <div v-if="requiresSignIn" class="action-modal-form">
         <p>You need to sign in to submit a transaction.</p>
       </div>
-      <div
-        v-else-if="step === `details`"
-        class="action-modal-form"
-      >
+      <div v-else-if="step === `details`" class="action-modal-form">
         <slot />
       </div>
-      <div
-        v-else-if="step === `fees`"
-        class="action-modal-form"
-      >
+      <div v-else-if="step === `fees`" class="action-modal-form">
         <TmFormGroup
           v-if="session.experimentalMode"
           :error="$v.gasPrice.$error && $v.gasPrice.$invalid"
@@ -88,10 +68,7 @@
           :max="balanceInAtoms"
         />
       </div>
-      <div
-        v-else-if="step === `sign`"
-        class="action-modal-form"
-      >
+      <div v-else-if="step === `sign`" class="action-modal-form">
         <TmFormGroup
           v-if="signMethods.length > 1"
           class="action-modal-form-group"
@@ -112,9 +89,9 @@
         >
           <div v-if="session.browserWithLedgerSupport">
             {{
-            sending
-            ? `Please verify and sign the transaction on your Ledger`
-            : `Please plug in your Ledger&nbsp;Nano and open
+              sending
+                ? `Please verify and sign the transaction on your Ledger`
+                : `Please plug in your Ledger&nbsp;Nano and open
             the Cosmos app`
             }}
           </div>
