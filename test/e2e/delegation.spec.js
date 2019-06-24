1c50f3ee3f68a57687d6f8fcce7e971a6bb019a0
Fabian <frznhope@gmail.com>
2019-06-24 12:12:25 +0200

    updated nightwatch and signup test

--- test/e2e/delegation.spec.js
+++ test/e2e/delegation.spec.js
@@ -6,14 +6,14 @@ module.exports = {
     browser.url(browser.launch_url + "/#/staking/validators")
 
     // move to validator page
-    browser
-      .waitForElementVisible(".li-validator")
-      .click(
-        ".li-validator[data-moniker=main_account] .data-table__row__info__container__name"
-      )
+    browser.expect.element(".li-validator").to.be.visible.before(10000)
+    browser.click(
+      ".li-validator[data-moniker=main_account] .data-table__row__info__container__name"
+    )
 
     // open modal and enter amount
-    browser.waitForElementVisible(`#delegation-btn`).click("#delegation-btn")
+    browser.expect.element(`#delegation-btn`).to.be.visible.before(10000)
+    browser.click("#delegation-btn")
 
     const value = "10.42"
     await actionModalCheckout(
@@ -28,9 +28,9 @@ module.exports = {
     )
 
     // check if tx shows
-    browser
-      .url(browser.launch_url + "/#/transactions")
-      .expect.element(".li-tx__content__caption__title")
+    browser.url(browser.launch_url + "/#/transactions")
+    browser.expect
+      .element(".li-tx__content__caption__title")
       .text.to.contain(`Delegated ${value} STAKE`)
       .before(10 * 1000)
   },
@@ -39,14 +39,14 @@ module.exports = {
     browser.url(browser.launch_url + "/#/staking/validators")
 
     // move to validator page
-    browser
-      .waitForElementVisible(".li-validator")
-      .click(
-        ".li-validator[data-moniker=operator_account_1] .data-table__row__info__container__name"
-      )
+    browser.expect.element(".li-validator").to.be.visible.before(10000)
+    browser.click(
+      ".li-validator[data-moniker=operator_account_1] .data-table__row__info__container__name"
+    )
 
     // open modal and enter amount
-    browser.waitForElementVisible(`#delegation-btn`).click("#delegation-btn")
+    browser.expect.element(`#delegation-btn`).to.be.visible.before(10000)
+    browser.click("#delegation-btn")
 
     const value = "5.53"
     await actionModalCheckout(
@@ -75,16 +75,14 @@ module.exports = {
     browser.url(browser.launch_url + "/#/staking/validators")
 
     // move to validator page
-    browser
-      .waitForElementVisible(".li-validator")
-      .click(
-        ".li-validator[data-moniker=operator_account_1] .data-table__row__info__container__name"
-      )
+    browser.expect.element(".li-validator").to.be.visible.before(10000)
+    browser.click(
+      ".li-validator[data-moniker=operator_account_1] .data-table__row__info__container__name"
+    )
 
     // open modal and enter amount
-    browser
-      .waitForElementVisible(`#undelegation-btn`)
-      .click("#undelegation-btn")
+    browser.expect.element(`#undelegation-btn`).to.be.visible.before(10000)
+    browser.click("#undelegation-btn")
 
     const value = "4.2"
     await actionModalCheckout(
--- test/e2e/signin.spec.js
+++ test/e2e/signin.spec.js
@@ -1,24 +1,70 @@
 module.exports = {
   "Sign in with local account": async function(browser) {
+    browser.url(browser.launch_url + "?insecure=true")
+    browser.waitForElementVisible(`body`)
+    browser.waitForElementVisible(`#app-content`)
     browser
-      .url(browser.launch_url + "?insecure=true")
-      .waitForElementVisible(`body`)
-      .waitForElementVisible(`#app-content`)
       // we are by default signed in so we first need to sign out
       .waitForElementVisible("#signOut-btn")
-      .click("#signOut-btn")
-      // sign in
-      .waitForElementVisible(".sign-in-button")
-      .pause(1000) // if not waiting here, the session modal closes immediatly // TODO
-      .click(".sign-in-button")
-      .waitForElementVisible("#session-welcome")
-      .click("#use-an-existing-address")
-      .waitForElementVisible("#session-existing")
-      .click("#sign-in-with-account")
-      .waitForElementVisible("#sign-in-name")
-      .click("#sign-in-name option[value=rich_account]")
-      .setValue("#sign-in-password", "1234567890")
-      .click(".session-footer button")
-      .waitForElementVisible("#signOut-btn")
+    browser.click("#signOut-btn")
+    // sign in
+    browser.waitForElementVisible(".sign-in-button")
+    browser.pause(1000) // if not waiting here, the session modal closes immediatly // TODO
+    browser.click(".sign-in-button")
+    browser.waitForElementVisible("#session-welcome")
+    browser.click("#use-an-existing-address")
+    browser.waitForElementVisible("#session-existing")
+    browser.click("#sign-in-with-account")
+    browser.waitForElementVisible("#sign-in-name")
+    browser.click("#sign-in-name option[value=rich_account]")
+    browser.setValue("#sign-in-password", "1234567890")
+    browser.click(".session-footer button")
+    browser.waitForElementVisible("#signOut-btn")
+  },
+  "Create local account": async function(browser) {
+    browser.url(browser.launch_url + "?insecure=true")
+    browser.waitForElementVisible(`body`)
+    browser.waitForElementVisible(`#app-content`)
+    // we are by default signed in so we first need to sign out
+    browser.waitForElementVisible("#signOut-btn")
+    browser.click("#signOut-btn")
+    // sign in
+    browser.waitForElementVisible(".sign-in-button")
+    browser.pause(1000) // if not waiting here, the session modal closes immediatly // TODO
+    browser.click(".sign-in-button")
+    browser.waitForElementVisible("#session-welcome")
+    browser.click("#creat-new-address")
+    browser.waitForElementVisible("#sign-up-seed")
+    browser.expect
+      .element("#sign-up-seed")
+      .value.to.match(/\w+( \w+){23}/)
+      .before(10000)
+
+    next(browser)
+    browser.expect.elements(".tm-form-msg--error").count.to.equal(4)
+
+    browser.setValue("#sign-up-name", "demo-account")
+    next(browser)
+    browser.expect.elements(".tm-form-msg--error").count.to.equal(3)
+
+    browser.setValue("#sign-up-password", "1234567890")
+    next(browser)
+    browser.expect
+      .elements(".tm-form-msg--error")
+      // the error on the initial password vanishes but the password confirmation appears
+      .count.to.equal(3)
+
+    browser.setValue("#sign-up-password-confirm", "1234567890")
+    next(browser)
+    browser.expect.elements(".tm-form-msg--error").count.to.equal(2)
+
+    browser.setValue("#sign-up-warning", true)
+    next(browser)
+    // signs in
+    browser.waitForElementVisible("#signOut-btn")
   }
+}
+
+function next(browser) {
+  return browser.click(".session-footer button")
 }
