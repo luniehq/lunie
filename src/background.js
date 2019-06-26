15a2cb402ab5893ba2e33aa3db08f585456f9316
colw <colw@outlook.com>
2019-06-26 16:23:03 +0200

    Update with website <> extension comms

--- src/background.js
+++ src/background.js
@@ -3,12 +3,29 @@ import { signMessageHandler, walletMessageHandler } from './messageHandlers';
 
 global.browser = require('webextension-polyfill');
 
+// DEBUG
+const wallets = {
+  cosmos1: 100,
+  cosmos2: 90,
+  cosmos3: 80,
+};
+
 // main message handler
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'TEST_REQUEST') {
+    sendResponse(`hello ${sender}`);
+  }
+
+  if (message.type === 'GET_WALLETS') {
+    // DEBUG
+    sendResponse({ wallets });
+    return;
+  }
   try {
     signMessageHandler(message, sender, sendResponse);
     walletMessageHandler(message, sender, sendResponse);
   } catch (e) {
+    // Return this as rejected
     console.error('Error with request', e);
   }
 });
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,19 +1,61 @@
-console.log('Hello Lunie Toonie');
+console.log('EXT Content Script Loaded');
+
+const LUNIE_EXT_TYPE = 'FROM_LUNIE_EXTENSION';
+const LUNIE_WEBSITE_TYPE = 'FROM_LUNIE_IO';
 
 // var port = chrome.runtime.connect();
 // port.postMessage({ type: 'LUNIE_EXTENSION', text: 'hello' }, '*');
-window.postMessage({ type: 'LUNIE_EXTENSION', enabled: true }, '*');
 
-// window.addEventListener(
-//   'message',
-//   function(event) {
-//     // We only accept messages from ourselves
-//     if (event.source !== window) return;
-
-//     if (event.data.type && event.data.type === 'FROM_PAGE') {
-//       console.log('Content script received: ' + event.data.text);
-//       port.postMessage({ type: 'FROM_EXTENSION', text: 'Hello from the extension!' }, '*');
-//     }
+// const msg = {
+//   type: LUNIE_EXT_TYPE,
+//   message: {
+//     type: 'GET_WALLETS_RESPONSE',
+//     payload: {
+//       wallets: {
+//         cosmos1: 100,
+//         cosmos2: 90,
+//         cosmos3: 80,
+//       },
+//     },
 //   },
-//   false
-// );
+// };
+
+const wrapMessageForLunie = (type, payload) => {
+  return {
+    type: LUNIE_EXT_TYPE,
+    message: {
+      type,
+      payload,
+    },
+  };
+};
+
+function enableExtension() {
+  const message = wrapMessageForLunie('INIT_EXTENSION', { extension_enabled: true });
+  window.postMessage(message, '*');
+}
+
+enableExtension();
+
+// Listen to Lunie.io
+window.addEventListener(
+  'message',
+  function(event) {
+    // We only accept messages from ourselves
+    if (event.source !== window) return;
+
+    if (event.data.type && event.data.type === LUNIE_WEBSITE_TYPE) {
+      const { type, payload } = event.data;
+
+      // Forward request to backgroud
+      chrome.runtime.sendMessage(payload, function(response) {
+        const data = { responseType: `${payload.type}_RESPONSE`, payload: response };
+        const wrappedMessage = wrapMessageForLunie(data.responseType, data.payload);
+
+        // Post reply to Lunie.io
+        window.postMessage(wrappedMessage, '*');
+      });
+    }
+  },
+  false
+);
