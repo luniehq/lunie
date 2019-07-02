0ff264e57d7755343bed0acb578f546637650a09
Jordan Bibla <jbibla@gmail.com>
2019-07-02 17:41:39 -0400

    use SessionFrame with TmSessionWelcome

--- src/components/common/TmSessionWelcome.vue
+++ src/components/common/TmSessionWelcome.vue
@@ -1,49 +1,53 @@
 <template>
-  <div id="session-welcome" class="session">
-    <div class="session-header">
-      <a @click="closeSession">
-        <i class="material-icons session-close">close</i>
-      </a>
+  <session-frame>
+    <div id="session-welcome" class="session">
+      <div class="session-header">
+        <a @click="closeSession">
+          <i class="material-icons session-close">close</i>
+        </a>
+      </div>
+
+      <img class="lunie-logo" src="~assets/images/cosmos-wallet-logo.svg" />
+
+      <h2 class="session-title">
+        Welcome to Lunie
+      </h2>
+
+      <p class="session-paragraph">
+        The easiest way to send, stake, and vote on the Cosmos Hub.
+      </p>
+
+      <div class="session-list">
+        <LiSession
+          id="creat-new-address"
+          icon="person_add"
+          title="Create a new address"
+          @click.native="() => setState('sign-up')"
+        />
+        <LiSession
+          id="use-an-existing-address"
+          icon="person"
+          title="Use an existing address"
+          @click.native="() => setState('existing')"
+        />
+      </div>
+
+      <p class="footnote">
+        By using Lunie, you accept our
+        <router-link to="/terms" class="link">Terms of Service</router-link> and
+        <router-link to="/privacy" class="link">Privacy Policy</router-link>.
+      </p>
     </div>
-
-    <img class="lunie-logo" src="~assets/images/cosmos-wallet-logo.svg" />
-
-    <h2 class="session-title">
-      Welcome to Lunie
-    </h2>
-
-    <p class="session-paragraph">
-      The easiest way to send, stake, and vote on the Cosmos Hub.
-    </p>
-
-    <div class="session-list">
-      <LiSession
-        id="creat-new-address"
-        icon="person_add"
-        title="Create a new address"
-        @click.native="() => setState('sign-up')"
-      />
-      <LiSession
-        id="use-an-existing-address"
-        icon="person"
-        title="Use an existing address"
-        @click.native="() => setState('existing')"
-      />
-    </div>
-
-    <p class="footnote">
-      By using Lunie, you accept our
-      <router-link to="/terms" class="link">Terms of Service</router-link> and
-      <router-link to="/privacy" class="link">Privacy Policy</router-link>.
-    </p>
-  </div>
+  </session-frame>
 </template>
 
 <script>
+import SessionFrame from "common/SessionFrame"
 import LiSession from "common/TmLiSession"
 export default {
   name: `session-welcome`,
   components: {
+    SessionFrame,
     LiSession
   },
   methods: {
