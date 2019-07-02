083b8a82013d9abf3e3504a8048325e5d1bab768
Jordan Bibla <jbibla@gmail.com>
2019-07-02 17:38:26 -0400

    declaring new routes for session screens

--- src/routes.js
+++ src/routes.js
@@ -82,6 +82,55 @@ export default [
     name: `block`,
     component: require(`./components/network/PageBlock`).default
   },
+  {
+    path: `/welcome`,
+    name: `welcome`,
+    components: {
+      session: require(`./components/common/TmSessionWelcome`).default
+    }
+  },
+  {
+    path: `/login`,
+    name: `login`,
+    components: {
+      session: require(`./components/common/TmSessionSignIn`).default
+    }
+  },
+  {
+    path: `/create`,
+    name: `create`,
+    components: {
+      session: require(`./components/common/TmSessionSignUp`).default
+    }
+  },
+  {
+    path: `/recover`,
+    name: `recover`,
+    components: {
+      session: require(`./components/common/TmSessionImport`).default
+    }
+  },
+  {
+    path: `/explore`,
+    name: `explore`,
+    components: {
+      session: require(`./components/common/TmSessionExplore`).default
+    }
+  },
+  {
+    path: `/ledger`,
+    name: `ledger`,
+    components: {
+      session: require(`./components/common/TmSessionHardware`).default
+    }
+  },
+  {
+    path: `/extension`,
+    name: `extension`,
+    components: {
+      session: require(`./components/common/TmSessionExtension`).default
+    }
+  },
   { path: `/404`, component: require(`./components/common/Page404`).default },
   {
     path: `/privacy`,
