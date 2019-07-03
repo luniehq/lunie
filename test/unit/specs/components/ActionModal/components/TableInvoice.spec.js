f929ee5d1f8d831588fdd7fb5287bbbb638077e5
colw <colw@outlook.com>
2019-07-03 18:05:59 +0200

    Add tests for correct number output
    - Use specific numbers that can not be rendered by accident
    - Match using regex
    - Check loosely that.number exists in html. Not as concerned with it's
      containing element, and possible if it is followed by the denom.
    - New snapshot included

--- test/unit/specs/components/ActionModal/components/TableInvoice.spec.js
+++ test/unit/specs/components/ActionModal/components/TableInvoice.spec.js
@@ -10,7 +10,7 @@ describe(`TableInvoice`, () => {
     wrapper = shallowMount(TableInvoice, {
       localVue,
       propsData: {
-        amount: 1,
+        amount: 17.2,
         gasEstimate: 1234567,
         gasPrice: 2.5e-8,
         bondDenom: `STAKE`
@@ -18,7 +18,19 @@ describe(`TableInvoice`, () => {
     })
   })
 
-  it(`should show the transaction invoice summary`, async () => {
+  it(`should render correctly`, async () => {
     expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+
+  it(`should display the correct subtotal`, async () => {
+    expect(wrapper.text()).toMatch(/17.2[0]+ STAKE/)
+  })
+
+  it(`should display the correct network fee`, async () => {
+    expect(wrapper.text()).toMatch(/0.03086[0-9]+/)
+  })
+
+  it(`should display the correct total`, async () => {
+    expect(wrapper.text()).toMatch(/17.23086[0-9]+ STAKE/)
   })
 })
