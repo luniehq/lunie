2937a743a998f95729e76742e2e4f6fbc0fadedf
Fabian <frznhope@gmail.com>
2019-07-03 15:19:35 +0200

    whitelist sender

--- src/background.js
+++ src/background.js
@@ -3,8 +3,18 @@ import { signMessageHandler, walletMessageHandler } from './messageHandlers';
 
 global.browser = require('webextension-polyfill');
 
+const whitelisted = ['https://lunie.io', 'https://www.lunie.io'];
+if (process.env.NODE_ENV === 'development') {
+  whitelisted.push('https://localhost');
+}
+
 // main message handler
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (!senderAllowed(sender)) {
+    console.error('Sender is not whitelisted');
+    return;
+  }
+
   try {
     signMessageHandler(message, sender, sendResponse);
     walletMessageHandler(message, sender, sendResponse);
@@ -12,3 +22,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.error('Error with request', e);
   }
 });
+
+// only allow whitelisted websites to send us messages
+function senderAllowed(sender) {
+  if (sender.tab && !whitelisted.find(url => sender.tab.url.startsWith(url))) {
+    return false;
+  }
+  return true;
+}
