7fd6c43fd903653a7e087ac943864b3d88aa80bf
Jordan Bibla <jbibla@gmail.com>
2019-07-03 16:45:08 -0400

    moved styles into session.css

--- src/components/common/SessionFrame.vue
+++ src/components/common/SessionFrame.vue
@@ -1,8 +1,8 @@
 <template>
   <transition name="component-fade" mode="out-in">
-    <div class="session-modal">
+    <div class="session-frame">
       <router-link to="/">
-        <img class="logo" src="~assets/images/cosmos-wallet-logo.svg" />
+        <img class="session-logo" src="~assets/images/cosmos-wallet-logo.svg" />
       </router-link>
       <div class="session-outer-container">
         <slot></slot>
@@ -34,39 +34,4 @@ export default {
 
 <style>
 @import "../../styles/session.css";
-
-.session-modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: var(--z-modal);
-  width: 100vw;
-  height: 100vh;
-  background: var(--app-nav-light);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.session-outer-container {
-  position: relative;
-  padding: 3rem 0;
-}
-
-.logo {
-  height: 3rem;
-  position: absolute;
-  top: 1.25rem;
-  left: 1.25rem;
-}
-
-.component-fade-enter-active,
-.component-fade-leave-active {
-  transition: opacity 0.2s ease;
-}
-
-.component-fade-enter,
-.component-fade-leave-to .component-fade-leave-active {
-  opacity: 0;
-}
 </style>
