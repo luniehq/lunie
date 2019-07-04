8315e9d5fb32bb2e7f145d6446a24d936c2e81d3
Jordan Bibla <jbibla@gmail.com>
2019-07-03 20:28:33 -0400

    lint

--- test/unit/specs/components/common/SessionFrame.spec.js
+++ test/unit/specs/components/common/SessionFrame.spec.js
@@ -2,15 +2,15 @@ import { shallowMount } from "@vue/test-utils"
 import SessionFrame from "common/SessionFrame"
 
 describe(`SessionFrame`, () => {
-  let wrapper, $store
+	let wrapper
 
-  beforeEach(() => {
-    wrapper = shallowMount(SessionFrame, {
-      stubs: [`router-link`]
-    })
-  })
+	beforeEach(() => {
+		wrapper = shallowMount(SessionFrame, {
+			stubs: [`router-link`]
+		})
+	})
 
-  it(`shows an overview of all wallets to sign in with from the extension`, () => {
-    expect(wrapper.vm.$el).toMatchSnapshot()
-  })
+	it(`shows an overview of all wallets to sign in with from the extension`, () => {
+		expect(wrapper.vm.$el).toMatchSnapshot()
+	})
 })
