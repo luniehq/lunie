a715d43cc7f9a4e40162dbe5b19a8d56622443a5
Benji <thebkrcanbefound@gmail.com>
2019-07-04 14:13:38 -0400

    Adds a test for the conditional in parsers

--- test/unit/specs/scripts/parsers.spec.js
+++ test/unit/specs/scripts/parsers.spec.js
@@ -1,6 +1,6 @@
 import { parseTx, parseFee, parseValueObj } from "scripts/parsers"
 
-const signedMessaged = {
+const signedMessage = {
   type: "auth/StdTx",
   value: {
     msg: [
@@ -31,8 +31,39 @@ const signedMessaged = {
   }
 }
 
+const signedActionMessage = {
+  type: "auth/StdTx",
+  value: {
+    msg: [
+      {
+        type: "cosmos-sdk/MsgDelegate",
+        value: {
+          amount: [
+            {
+              amount: "10000000",
+              denom: "stake"
+            }
+          ],
+          from_address: "cosmos1ek9cd8ewgxg9w5xllq9um0uf4aaxaruvcw4v9e",
+          to_address: "cosmos1324vt5j3wzx0xsc32mjhkrvy5gn5ef2hrwcg29"
+        }
+      }
+    ],
+    fee: {
+      amount: [
+        {
+          amount: "40",
+          denom: "stake"
+        }
+      ],
+      gas: "39953"
+    },
+    memo: "(Sent via Lunie)"
+  }
+}
+
 describe(`parsers helper`, () => {
-  it(`should parse a signedmessage ParseTx`, () => {
+  it(`should parse a signedmessaged ParseTx`, () => {
     const shortMessage = {
       tx: {
         type: "auth/StdTx",
@@ -48,15 +79,25 @@ describe(`parsers helper`, () => {
     ).toMatchObject(shortMessage)
   })
 
-  it(`should parse a signedmessage parseFee`, () => {
-    expect(parseFee(signedMessaged)).toBe(40)
+  it(`should parse a signedmessaged parseFee`, () => {
+    expect(parseFee(signedMessage)).toBe(40)
   })
 
-  it(`should parse a signedmessage parseValueObj`, () => {
+  it(`should parse a signedmessaged parseValueObj`, () => {
     const parsedValueObj = {
       amount: "10000000",
       denom: "stake"
     }
-    expect(parseValueObj(signedMessaged)).toMatchObject(parsedValueObj)
+    expect(parseValueObj(signedMessage)).toMatchObject(parsedValueObj)
+  })
+
+  it(`should parse a signedActionMessage parseValueObj`, () => {
+    const parsedValueObj = [
+      {
+        amount: "10000000",
+        denom: "stake"
+      }
+    ]
+    expect(parseValueObj(signedActionMessage)).toMatchObject(parsedValueObj)
   })
 })
