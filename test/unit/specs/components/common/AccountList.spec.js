21e75479eb5f98a5eeb1925b35bf1c705929e1af
Jordan Bibla <jbibla@gmail.com>
2019-07-05 12:56:23 -0400

    lint

--- test/unit/specs/components/common/AccountList.spec.js
+++ test/unit/specs/components/common/AccountList.spec.js
@@ -2,31 +2,31 @@ import { shallowMount } from "@vue/test-utils"
 import AccountList from "common/AccountList"
 
 describe(`AccountList`, () => {
-	let wrapper
+  let wrapper
 
-	beforeEach(() => {
-		wrapper = shallowMount(AccountList, {
-			propsData: {
-				accounts: [
-					{
-						name: "Benjis account",
-						address: "cosmos1ek9cd8ewgxg9w5xllq9um0uf4aaxaruvcw4v9e"
-					},
-					{
-						name: "Colins account",
-						address: "cosmos1ek9cd8ewgxgyoyocolinyo4aaxaruvcw4v9e"
-					}
-				]
-			}
-		})
-	})
+  beforeEach(() => {
+    wrapper = shallowMount(AccountList, {
+      propsData: {
+        accounts: [
+          {
+            name: "Benjis account",
+            address: "cosmos1ek9cd8ewgxg9w5xllq9um0uf4aaxaruvcw4v9e"
+          },
+          {
+            name: "Colins account",
+            address: "cosmos1ek9cd8ewgxgyoyocolinyo4aaxaruvcw4v9e"
+          }
+        ]
+      }
+    })
+  })
 
-	it(`has the expected html structure`, () => {
-		expect(wrapper.vm.$el).toMatchSnapshot()
-	})
+  it(`has the expected html structure`, () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
 
-	it(`renders the correct accounts`, () => {
-		expect(wrapper.html()).toContain("Benjis account")
-		expect(wrapper.html()).toContain("Colins account")
-	})
+  it(`renders the correct accounts`, () => {
+    expect(wrapper.html()).toContain("Benjis account")
+    expect(wrapper.html()).toContain("Colins account")
+  })
 })
