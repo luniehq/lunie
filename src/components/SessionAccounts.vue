9fedc320050ae74c3829117c56aa4f1ab81c47d0
Jordan Bibla <jbibla@gmail.com>
2019-07-09 08:36:55 -0400

    added session accounts and session welcome as separate pages

---
+++ src/components/SessionAccounts.vue
@@ -0,0 +1,66 @@
+<template>
+  <SessionFrame>
+    <div class="session">
+      <div class="session-container">
+        <div class="session-header">
+          <h2 class="session-title">My accounts</h2>
+          <p>
+            You can use the accounts below to explore Lunie.io and to approve
+            transactions.
+          </p>
+        </div>
+        <div class="session-main">
+          <AccountList
+            :accounts="accounts"
+            :button-action="goToLunie"
+            :button-text="`Go to Lunie`"
+          />
+        </div>
+        <div class="session-footer">
+          <router-link to="/welcome">Want to add another account?</router-link>
+        </div>
+      </div>
+    </div>
+  </SessionFrame>
+</template>
+
+<script>
+import AccountList from 'common/AccountList'
+import SessionFrame from 'common/SessionFrame'
+import TmBtn from 'common/TmBtn'
+export default {
+  name: `session-accounts`,
+  components: {
+    AccountList,
+    SessionFrame,
+    TmBtn
+  },
+  computed: {
+    accounts() {
+      return this.$store.state.accounts
+    }
+  },
+  methods: {
+    goToWelcome() {
+      this.$router.push(`/welcome`)
+    },
+    goToLunie() {
+      window.open(
+        'https://lunie.io/#/extension',
+        '_blank',
+        'noreferrer noopener'
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button-add-account {
+  display: flex;
+  justify-content: flex-end;
+  padding: 1.5rem 0 1rem;
+  flex-grow: 1;
+  align-self: flex-end;
+}
+</style>
---
+++ src/components/SessionWelcome.vue
@@ -0,0 +1,81 @@
+<template>
+  <SessionFrame>
+    <div id="session-welcome" class="session">
+      <router-link to="/">
+        <img class="lunie-logo" src="../images/extension-logo.svg" />
+      </router-link>
+
+      <div class="session-list">
+        <LiSession
+          id="creat-new-address"
+          icon="person_add"
+          title="Create a new address"
+          route="create"
+        />
+        <LiSession
+          id="recover-with-backup"
+          icon="settings_backup_restore"
+          title="Recover with backup code"
+          route="recover"
+        />
+        <LiSession
+          v-if="accounts.length > 0"
+          id="see-accounts"
+          icon="person"
+          title="See existing accounts"
+          route="/"
+        />
+      </div>
+
+      <p class="footnote">
+        By using Lunie, you accept our
+        <a
+          href="https://lunie.io/#/terms"
+          target="_blank"
+          rel="nofollow noreferrer noopener"
+          >Terms of Service</a
+        >
+        and
+        <a
+          href="https://lunie.io/#/privacy"
+          target="_blank"
+          rel="nofollow noreferrer noopener"
+          >Privacy Policy</a
+        >.
+      </p>
+    </div>
+  </SessionFrame>
+</template>
+
+<script>
+import SessionFrame from 'common/SessionFrame'
+import LiSession from 'common/TmLiSession'
+export default {
+  name: `session-welcome`,
+  components: {
+    SessionFrame,
+    LiSession
+  },
+  computed: {
+    accounts() {
+      return this.$store.state.accounts
+    }
+  }
+}
+</script>
+<style scoped>
+.session-title {
+  padding-left: 1rem;
+  font-size: 28px;
+  line-height: normal;
+  padding-bottom: 0;
+}
+
+.lunie-logo {
+  margin: 1rem 0 1rem 1rem;
+}
+
+.session-paragraph {
+  padding: 0 1rem;
+}
+</style>
