3fe8f450ddd65c3ba19f4db7ed8826ce51712f94
Fabian Weber <frznhope@gmail.com>
2019-07-16 21:24:52 +0200

    fixed session approve import issues

--- test/unit/components/SessionApprove.spec.js
+++ test/unit/components/SessionApprove.spec.js
@@ -1,75 +1,75 @@
-// import { createLocalVue, shallowMount } from '@vue/test-utils'
-// import Vuelidate from 'vuelidate'
-// import SessionApprove from '../../../src/components/SessionApprove'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuelidate from 'vuelidate'
+import SessionApprove from '../../../src/components/SessionApprove'
 
-// xdescribe(`SessionApprove`, () => {
-//   const localVue = createLocalVue()
-//   localVue.use(Vuelidate)
-//   localVue.directive('focus', () => {})
+xdescribe(`SessionApprove`, () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuelidate)
+  localVue.directive('focus', () => {})
 
-//   let wrapper, $store
+  let wrapper, $store
 
-//   const signMessage = `{"account_number":"1","chain_id":"testnet","fee":{"amount":[{"amount":"40","denom":"stake"}],"gas":"39953"},"memo":"(Sent via Lunie)","msgs":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"12000000","denom":"stake"}],"from_address":"cosmos1ek9cd8ewgxg9w5xllq9um0uf4aaxaruvcw4v9e","to_address":"cosmos1324vt5j3wzx0xsc32mjhkrvy5gn5ef2hrwcg29"}}],"sequence":"0"}`
+  const signMessage = `{"account_number":"1","chain_id":"testnet","fee":{"amount":[{"amount":"40","denom":"stake"}],"gas":"39953"},"memo":"(Sent via Lunie)","msgs":[{"type":"cosmos-sdk/MsgSend","value":{"amount":[{"amount":"12000000","denom":"stake"}],"from_address":"cosmos1ek9cd8ewgxg9w5xllq9um0uf4aaxaruvcw4v9e","to_address":"cosmos1324vt5j3wzx0xsc32mjhkrvy5gn5ef2hrwcg29"}}],"sequence":"0"}`
 
-//   beforeEach(() => {
-//     const getters = {
-//       signRequest: {
-//         senderAddress: 'cosmos1234',
-//         signMessage
-//       }
-//     }
+  beforeEach(() => {
+    const getters = {
+      signRequest: {
+        senderAddress: 'cosmos1234',
+        signMessage
+      }
+    }
 
-//     $store = {
-//       commit: jest.fn(),
-//       dispatch: jest.fn(),
-//       getters
-//     }
-//     wrapper = shallowMount(SessionApprove, {
-//       localVue,
-//       mocks: {
-//         $store,
-//         $router: {
-//           push: jest.fn()
-//         }
-//       }
-//     })
-//   })
+    $store = {
+      commit: jest.fn(),
+      dispatch: jest.fn(),
+      getters
+    }
+    wrapper = shallowMount(SessionApprove, {
+      localVue,
+      mocks: {
+        $store,
+        $router: {
+          push: jest.fn()
+        }
+      }
+    })
+  })
 
-//   it(`shows the approval modal with the transaction and an invoice table`, () => {
-//     expect(wrapper.vm.$el).toMatchSnapshot()
-//   })
+  it(`shows the approval modal with the transaction and an invoice table`, () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
 
-//   describe('approve', () => {
-//     it('fails if no password', async () => {
-//       wrapper.vm.close = jest.fn()
-//       wrapper.vm.password = ''
-//       wrapper.find('#approve-btn').trigger('click')
-//       await wrapper.vm.$nextTick()
-//       expect(wrapper.vm.$store.dispatch).not.toHaveBeenCalled()
-//       expect(wrapper.vm.close).not.toHaveBeenCalled()
-//     })
+  describe('approve', () => {
+    it('fails if no password', async () => {
+      wrapper.vm.close = jest.fn()
+      wrapper.vm.password = ''
+      wrapper.find('#approve-btn').trigger('click')
+      await wrapper.vm.$nextTick()
+      expect(wrapper.vm.$store.dispatch).not.toHaveBeenCalled()
+      expect(wrapper.vm.close).not.toHaveBeenCalled()
+    })
 
-//     it('approves and closes', async () => {
-//       wrapper.vm.close = jest.fn()
-//       wrapper.vm.password = '1234'
-//       wrapper.find('#approve-btn').trigger('click')
-//       await wrapper.vm.$nextTick()
-//       expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith(
-//         'approveSignRequest',
-//         { password: '1234', senderAddress: 'cosmos1234', signMessage }
-//       )
-//       expect(wrapper.vm.$router.push).toHaveBeenCalledWith(`/approved`)
-//     })
-//   })
+    it('approves and closes', async () => {
+      wrapper.vm.close = jest.fn()
+      wrapper.vm.password = '1234'
+      wrapper.find('#approve-btn').trigger('click')
+      await wrapper.vm.$nextTick()
+      expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith(
+        'approveSignRequest',
+        { password: '1234', senderAddress: 'cosmos1234', signMessage }
+      )
+      expect(wrapper.vm.$router.push).toHaveBeenCalledWith(`/approved`)
+    })
+  })
 
-//   it('rejects', async () => {
-//     wrapper.vm.close = jest.fn()
-//     wrapper.find('#reject-btn').trigger('click')
-//     await wrapper.vm.$nextTick()
-//     expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith(
-//       'rejectSignRequest',
-//       { signMessage, senderAddress: 'cosmos1234' }
-//     )
-//     expect(wrapper.vm.$router.push).toHaveBeenCalledWith(`/`)
-//   })
-// })
+  it('rejects', async () => {
+    wrapper.vm.close = jest.fn()
+    wrapper.find('#reject-btn').trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith(
+      'rejectSignRequest',
+      { signMessage, senderAddress: 'cosmos1234' }
+    )
+    expect(wrapper.vm.$router.push).toHaveBeenCalledWith(`/`)
+  })
+})
