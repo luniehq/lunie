764106922672cbd1fb2e9ed92275d04d0622a25a
Benji <thebkrcanbefound@gmail.com>
2019-07-29 15:05:02 -0400

    Removes GA code from code

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const webpack = require('webpack');
-const ejs = require('ejs');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtensionReloader = require('webpack-extension-reloader');
-const { VueLoaderPlugin } = require('vue-loader');
-const { version } = require('./package.json');
-const path = require('path');
+const webpack = require('webpack')
+const ejs = require('ejs')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtensionReloader = require('webpack-extension-reloader')
+const { VueLoaderPlugin } = require('vue-loader')
+const { version } = require('./package.json')
+const path = require('path')
 
 function resolve(dir) {
-  return path.join(__dirname, dir);
+  return path.join(__dirname, dir)
 }
 
 const config = {
@@ -17,11 +17,11 @@ const config = {
   entry: {
     background: './background.js',
     contentScript: './contentScript.js',
-    'popup/popup': './popup/popup.js',
+    'popup/popup': './popup/popup.js'
   },
   output: {
     path: resolve('dist'),
-    filename: '[name].js',
+    filename: '[name].js'
   },
   resolve: {
     alias: {
@@ -31,40 +31,44 @@ const config = {
       modules: resolve('lunie/src/vuex/modules'),
       'src/config': resolve('config.js'),
       src: resolve('lunie/src'),
-      scripts: resolve('lunie/src/scripts'),
+      scripts: resolve('lunie/src/scripts')
     },
-    extensions: ['.js', '.vue', '.css'],
+    extensions: ['.js', '.vue', '.css']
   },
   devtool: 'cheap-module-source-map',
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loaders: 'vue-loader',
+        loaders: 'vue-loader'
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       },
       {
         test: /\.sass$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader?indentedSyntax'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ]
       },
       {
         test: /\.(png|jpg|gif|svg|ico)$/,
         loader: 'file-loader',
         query: {
-          name: `/images/[name].[ext]`,
-        },
+          name: `/images/[name].[ext]`
+        }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
@@ -72,68 +76,72 @@ const config = {
           {
             loader: `file-loader`,
             query: {
-              name: `/fonts/[name].[ext]`,
-            },
-          },
-        ],
-      },
-    ],
+              name: `/fonts/[name].[ext]`
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
       global: 'window',
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
     }),
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: '[name].css'
     }),
     new CopyWebpackPlugin([
       { from: 'icons', to: 'icons', ignore: ['icon.xcf'] },
-      { from: 'popup/popup.html', to: 'popup/popup.html', transform: transformHtml },
+      {
+        from: 'popup/popup.html',
+        to: 'popup/popup.html',
+        transform: transformHtml
+      },
       {
         from: 'manifest.json',
         to: 'manifest.json',
         transform: content => {
-          const jsonContent = JSON.parse(content);
-          jsonContent.version = version;
+          const jsonContent = JSON.parse(content)
+          jsonContent.version = version
 
           if (config.mode === 'development') {
             jsonContent['content_security_policy'] =
               "script-src 'self' 'unsafe-eval' https://www.google-analytics.com/analytics.js; object-src 'self'"
           }
 
-          return JSON.stringify(jsonContent, null, 2);
-        },
-      },
-    ]),
-  ],
-};
+          return JSON.stringify(jsonContent, null, 2)
+        }
+      }
+    ])
+  ]
+}
 
 if (config.mode === 'production') {
   config.plugins = (config.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"',
-      },
-    }),
-  ]);
+        NODE_ENV: '"production"'
+      }
+    })
+  ])
 }
 
 if (process.env.HMR === 'true') {
   config.plugins = (config.plugins || []).concat([
     new ExtensionReloader({
-      manifest: resolve('src/manifest.json'),
-    }),
-  ]);
+      manifest: resolve('src/manifest.json')
+    })
+  ])
 }
 
 function transformHtml(content) {
   return ejs.render(content.toString(), {
-    ...process.env,
-  });
+    ...process.env
+  })
 }
 
-module.exports = config;
+module.exports = config
