ddabba56a369e1ab3c8c886bd95b922962d379b2
Jordan Bibla <jbibla@gmail.com>
2019-07-30 18:02:04 -0400

    eliminate proposals table

--- src/components/governance/TableProposals.vue
+++ src/components/governance/TableProposals.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <table class="data-table">
-      <thead>
-        <PanelSort :sort="sort" :properties="properties" />
-      </thead>
       <tbody>
         <LiProposal
           v-for="(value, key) in filteredProposals"
@@ -33,14 +30,12 @@ export default {
     }
   },
   data: () => ({
-    query: ``,
     sort: {
       property: `id`,
       order: `desc`
     }
   }),
   computed: {
-    ...mapGetters([`session`]),
     enrichedProposals() {
       const copy = JSON.parse(JSON.stringify(this.proposals))
       Object.keys(copy).forEach(proposal_id => {
@@ -56,46 +51,6 @@ export default {
       )
 
       return proposals
-    },
-    properties() {
-      return [
-        {
-          title: `Title`,
-          value: `title`,
-          tooltip: `The title of the proposal`,
-          class: `proposal_title`
-        },
-        {
-          title: `Proposal Id`,
-          value: `id`,
-          tooltip: `Id of the proposal`,
-          class: `proposal_id`
-        },
-        {
-          title: `Yes`,
-          value: `tally_result.yes`,
-          tooltip: `Yes votes`,
-          class: `yes_votes`
-        },
-        {
-          title: `No`,
-          value: `tally_result.no`,
-          tooltip: `No votes`,
-          class: `no_votes`
-        },
-        {
-          title: `No with Veto`,
-          value: `tally_result.no_with_veto`,
-          tooltip: `No with veto votes`,
-          class: `no_veto_votes`
-        },
-        {
-          title: `Abstain`,
-          value: `tally_result.abstain`,
-          tooltip: `Abstain votes`,
-          class: `abstain_votes`
-        }
-      ]
     }
   },
   mounted() {
