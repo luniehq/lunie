8bceb1c4f215e63d07df3b77a709eefa9eea7c4d
colw <colw@outlook.com>
2019-08-06 10:09:27 +0200

    lint and snapshots

--- src/components/staking/TabMyDelegations.vue
+++ src/components/staking/TabMyDelegations.vue
@@ -2,11 +2,17 @@
   <div>
     <CardSignInRequired v-if="!session.signedIn" />
     <div v-else-if="delegation.loaded && yourValidators.length > 0">
-      <TableValidators :validators="yourValidators" show-on-mobile="my_delegations" />
+      <TableValidators
+        :validators="yourValidators"
+        show-on-mobile="my_delegations"
+      />
     </div>
     <TmDataConnecting v-else-if="!delegation.loaded && !connected" />
     <TmDataLoading v-else-if="!delegation.loaded && delegation.loading" />
-    <TmDataMsg v-else-if="yourValidators.length === 0" icon="sentiment_dissatisfied">
+    <TmDataMsg
+      v-else-if="yourValidators.length === 0"
+      icon="sentiment_dissatisfied"
+    >
       <div slot="title">No Active Delegations</div>
       <div slot="subtitle">
         Looks like you haven't delegated any {{ bondDenom | viewDenom }}s yet.
