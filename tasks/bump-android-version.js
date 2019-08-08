a6483294303b0e9f5f4d1da38cac78b42d12b8cc
Fabian Weber <frznhope@gmail.com>
2019-08-08 11:25:52 -0400

    update also version name

--- tasks/bump-android-version.js
+++ tasks/bump-android-version.js
@@ -3,16 +3,22 @@ const fs = require("fs")
 const gradlePath = "./android/app/build.gradle"
 
 function main() {
+  const packageJson = JSON.parse(fs.readFileSync("./package.json"))
   const gradle = fs.readFileSync(gradlePath, "utf8")
   const currentVersion = /versionCode (\d+)/.exec(gradle)[1]
   const newVersion = parseInt(currentVersion) + 1
-  const updatedGradle = gradle.replace(
-    /versionCode (\d+)/,
-    `versionCode ${newVersion}`
-  )
+  const updatedGradle = gradle
+    .replace(
+        /versionCode (\d+)/,
+        `versionCode ${newVersion}`
+    )
+    .replace(
+        /versionName "(.+)"/,
+        `versionName "${packageJson.version}"`
+    )
   fs.writeFileSync(gradlePath, updatedGradle)
 
-  console.log(`Updated version for Android build to ${newVersion}`)
+  console.log(`Updated version code for Android build to ${newVersion}`)
 }
 
 main()
