e0fbfb42940d407f2428557fdf4cb501c92b57ab
Fabian Weber <frznhope@gmail.com>
2019-08-13 23:31:28 +0200

    added generated avatars

---
+++ src/components/common/Avatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <div v-html="svg" />
+</template>
+
+<script>
+import Avatars from "@dicebear/avatars"
+import sprites from "@dicebear/avatars-jdenticon-sprites"
+
+export default {
+  name: `avatar`,
+  props: {
+    address: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    svg() {
+      const options = {}
+      const avatars = new Avatars(sprites(options))
+      return avatars.create(this.address)
+    }
+  }
+}
+</script>
+
+<style></style>
