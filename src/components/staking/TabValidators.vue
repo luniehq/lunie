db22e77a9108108acb64cfef8ee3dce0891375d9
colw <colw@outlook.com>
2019-08-20 18:44:16 +0200

    Remove unecessery code from TavbValidators

--- src/components/staking/TabValidators.vue
+++ src/components/staking/TabValidators.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <TmDataConnecting v-if="!connected && $apollo.queries.validators.loading" />
-    <TmDataLoading v-else-if="$apollo.queries.validators.loading" />
+    <TmDataLoading v-if="$apollo.queries.validators.loading" />
     <TmDataEmpty
       v-else-if="!$apollo.queries.validators.loading && validators.length === 0"
     />
@@ -14,28 +13,21 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex"
 import { AllValidators, AllValidatorsResult } from "src/gql"
 import TableValidators from "staking/TableValidators"
 import TmDataEmpty from "common/TmDataEmpty"
 import TmDataLoading from "common/TmDataLoading"
-import TmDataConnecting from "common/TmDataConnecting"
 
 export default {
   name: `tab-validators`,
   components: {
     TableValidators,
     TmDataEmpty,
-    TmDataLoading,
-    TmDataConnecting
+    TmDataLoading
   },
   data: () => ({
-    lastUpdate: 0,
     validators: []
   }),
-  computed: {
-    ...mapGetters([`connected`])
-  },
   apollo: {
     validators: {
       query: AllValidators,
--- tests/unit/specs/components/staking/TabValidators.spec.js
+++ tests/unit/specs/components/staking/TabValidators.spec.js
@@ -5,142 +5,53 @@ import validators from "../../store/json/validators.js"
 describe(`TabValidators`, () => {
   let wrapper, $store, $apollo
 
-  const state = {
-    session: {
-      signedIn: true
-    },
-    delegates: {
-      delegates: validators,
-      loading: false,
-      loaded: true
-    }
-  }
-
-  const getters = {
-    committedDelegations: {
-      [validators[0].operator_address]: 42
-    },
-
-    connected: true,
-    lastHeader: { height: 20 },
-    yourValidators: validators
+  const data = {
+    validators
   }
 
   beforeEach(async () => {
     $apollo = {
       queries: {
         validators: {
-          loading: true
+          loading: false
         }
       }
-    }
-    $store = {
-      dispatch: jest.fn(),
-      getters,
-      state
     }
 
     wrapper = shallowMount(TabValidators, {
       mocks: {
-        $store,
         $apollo
       }
     })
+    wrapper.setData(data)
   })
 
   it(`shows a list of validators`, async () => {
-    expect(wrapper.vm.$el).toMatchSnapshot()
-  })
-
-  it(`shows a message if still connecting`, async () => {
-    $store = {
-      dispatch: jest.fn(),
-      state: {
-        session: {
-          signedIn: true
-        },
-        delegates: {
-          delegates: validators,
-          loading: false,
-          loaded: false
-        }
-      },
-      getters: {
-        committedDelegations: {
-          [validators[0].operator_address]: 42
-        },
-        connected: false,
-        lastHeader: { height: 20 },
-        yourValidators: validators
-      }
-    }
-
-    wrapper = shallowMount(TabValidators, {
-      mocks: {
-        $store,
-        $apollo
-      }
-    })
-
-    expect(wrapper.vm.$el).toMatchSnapshot()
+    expect(wrapper.element).toMatchSnapshot()
   })
 
   it(`shows a message if still loading`, async () => {
-    $store = {
-      dispatch: jest.fn(),
-      state: {
-        session: {
-          signedIn: true
-        },
-        delegates: {
-          delegates: validators,
-          loading: true,
-          loaded: false
-        }
-      },
-      getters: {
-        committedDelegations: {
-          [validators[0].operator_address]: 42
-        },
-        connected: true,
-        lastHeader: { height: 20 },
-        yourValidators: validators
-      }
-    }
-
     wrapper = shallowMount(TabValidators, {
       mocks: {
-        $store,
-        $apollo
+        $apollo: {
+          queries: {
+            validators: {
+              loading: true
+            }
+          }
+        }
       }
     })
-
-    expect(wrapper.vm.$el).toMatchSnapshot()
+    wrapper.setData({ validators: [] })
+    expect(wrapper.element).toMatchSnapshot()
   })
 
   it(`shows a message if there is nothing to display`, async () => {
     $store = {
-      dispatch: jest.fn(),
       state: {
-        session: {
-          signedIn: true
-        },
-        delegates: {
-          delegates: [],
-          loading: false,
-          loaded: true
-        }
-      },
-      getters: {
-        committedDelegations: {
-          [validators[0].operator_address]: 42
-        },
-        connected: true,
-        lastHeader: { height: 20 },
-        yourValidators: validators
+        validators: []
       }
     }
-
     wrapper = shallowMount(TabValidators, {
       mocks: {
         $store,
@@ -148,6 +59,6 @@ describe(`TabValidators`, () => {
       }
     })
 
-    expect(wrapper.vm.$el).toMatchSnapshot()
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
