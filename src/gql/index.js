95265d32f409d8519baaab1641ce2bd0f2f820fd
colw <colw@outlook.com>
2019-08-20 12:52:07 +0200

    Use Fragment for Validator parts

--- src/gql/index.js
+++ src/gql/index.js
@@ -1,63 +1,49 @@
 import gql from "graphql-tag"
 
+const ValidatorFragment = gql`
+  fragment ValidatorParts on allValidators {
+    keybaseId
+    avatarUrl
+    userName
+    profileUrl
+    customized
+    operator_address
+    consensus_pubkey
+    jailed
+    status
+    tokens
+    delegator_shares
+    unbonding_height
+    unbonding_time
+    min_self_delegation
+    details
+    identity
+    moniker
+    website
+    rate
+    max_rate
+    max_change_rate
+    update_time
+    lastUpdated
+  }
+`
+
 export const AllValidators = gql`
   query AllValidators {
     allValidators {
-      keybaseId
-      avatarUrl
-      userName
-      profileUrl
-      customized
-      operator_address
-      consensus_pubkey
-      jailed
-      status
-      tokens
-      delegator_shares
-      unbonding_height
-      unbonding_time
-      min_self_delegation
-      details
-      identity
-      moniker
-      website
-      rate
-      max_rate
-      max_change_rate
-      update_time
-      lastUpdated
+      ...ValidatorParts
     }
   }
+  ${ValidatorFragment}
 `
 
 export const ValidatorProfile = gql`
   query ValidatorInfo($address: String) {
     allValidators(where: { operator_address: { _eq: $address } }) {
-      keybaseId
-      avatarUrl
-      userName
-      profileUrl
-      customized
-      operator_address
-      consensus_pubkey
-      jailed
-      status
-      tokens
-      delegator_shares
-      unbonding_height
-      unbonding_time
-      min_self_delegation
-      details
-      identity
-      moniker
-      website
-      rate
-      max_rate
-      max_change_rate
-      update_time
-      lastUpdated
+      ...ValidatorParts
     }
   }
+  ${ValidatorFragment}
 `
 
 export const AllValidatorsResult = data => data.allValidators
