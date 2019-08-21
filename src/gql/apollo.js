cf3a481438cc05576534f3a2868fded6129d07b6
colw <colw@outlook.com>
2019-08-21 16:21:33 +0200

    Include option to set graphql endpoint in URL

--- src/gql/apollo.js
+++ src/gql/apollo.js
@@ -3,15 +3,17 @@ import ApolloClient from "apollo-boost"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import VueApollo from "vue-apollo"
 
-const apolloClient = new ApolloClient({
-  uri: process.env.VUE_APP_GRAPHQL_URL
-})
-
 Vue.use(VueApollo)
 
-const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
-  cache: new InMemoryCache()
-})
+const createApolloClient = urlParams => {
+  return new ApolloClient({
+    uri: urlParams.graphql || process.env.VUE_APP_GRAPHQL_URL
+  })
+}
 
-export default apolloProvider
+export const createApolloProvider = urlParams => {
+  return new VueApollo({
+    defaultClient: createApolloClient(urlParams),
+    cache: new InMemoryCache()
+  })
+}
