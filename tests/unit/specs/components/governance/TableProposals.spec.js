4695ff8029cf3abad4629419f754aa0b05ed515f
colw <colw@outlook.com>
2019-08-23 19:17:02 +0200

    Update TableProposals tests and snapshot

--- tests/unit/specs/components/governance/TableProposals.spec.js
+++ tests/unit/specs/components/governance/TableProposals.spec.js
@@ -1,55 +1,22 @@
 import { mount, createLocalVue } from "@vue/test-utils"
 import TableProposals from "governance/TableProposals"
 
-import { proposals, tallies } from "../../store/json/proposals"
+import { proposals } from "../../store/json/proposals"
 
 describe(`TableProposals`, () => {
-  let wrapper, $store
+  let wrapper
   const localVue = createLocalVue()
   localVue.directive(`tooltip`, () => {})
 
   beforeEach(() => {
-    $store = {
-      commit: jest.fn(),
-      dispatch: jest.fn(),
-      getters: {
-        proposals: {
-          proposals,
-          tallies
-        }
-      }
-    }
-
     wrapper = mount(TableProposals, {
       localVue,
-      propsData: { proposals },
-      mocks: {
-        $store
-      },
+      propsData: { proposals: Object.values(proposals) },
       stubs: [`router-link`]
     })
   })
 
   it(`has the expected html structure`, async () => {
     expect(wrapper.element).toMatchSnapshot()
-  })
-
-  it(`should sort the proposals by selected property`, () => {
-    wrapper.vm.sort.property = `proposal_id`
-    wrapper.vm.sort.order = `asc`
-
-    expect(wrapper.vm.filteredProposals[0].title).toEqual(proposals[`1`].title)
-
-    wrapper.vm.sort.property = `proposal_id`
-    wrapper.vm.sort.order = `desc`
-
-    expect(wrapper.vm.filteredProposals[0].title).toEqual(proposals[`6`].title)
-  })
-
-  it(`should filter the proposals`, () => {
-    expect(wrapper.element).toMatchSnapshot()
-    expect(wrapper.vm.filteredProposals[0].description).toBe(
-      proposals[`6`].description
-    )
   })
 })
