ee91a5cb8ee9a8c9a187607d8caca8eb691184bf
Fabian Weber <frznhope@gmail.com>
2019-08-29 00:57:26 +0200

    fix crash when fees are 0

--- src/scripts/parsers.js
+++ src/scripts/parsers.js
@@ -19,7 +19,7 @@ export const parseFee = stdTx => {
   const {
     value: { fee }
   } = stdTx
-  return Number(fee.amount[0].amount)
+  return Number(fee.amount.length > 0 ? fee.amount[0].amount : 0)
 }
 
 export const parseValueObj = stdTx => {
