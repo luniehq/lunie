30a3084ee39a66f6ff057acccfa19790b8f3cec7
colw <colw@outlook.com>
2019-08-29 19:30:37 +0200

    Test LiValidator

--- tests/unit/specs/components/staking/LiValidator.spec.js
+++ tests/unit/specs/components/staking/LiValidator.spec.js
@@ -10,48 +10,45 @@ describe(`LiValidator`, () => {
   let wrapper, $store
 
   const validator = {
-    operator_address: `cosmosvaladdr15ky9du8a2wlstz6fpx3p4mqpjyrm5ctplpn3au`,
-    pub_key: `cosmosvalpub1234`,
+    //From GraphQL
+    avatarUrl: "https://s3.amazonaws.com/img/path.jpg",
+    consensus_pubkey: "cosmosvalpub1234",
+    customized: false,
+    delegator_shares: "697935712090.000000000000000000",
+    details: "Mr Mounty",
+    id: "9cfd0f08-c750-4cde-a267-e5a38feede20",
+    identity: "4BE49EABAA41B8BF",
     jailed: false,
-    tokens: `14`,
-    delegator_shares: `14`,
-    description: {
-      website: `www.monty.ca`,
-      details: `Mr Mounty`,
-      moniker: `mr_mounty`,
-      country: `Canada`
-    },
+    keybaseId: "4BE49EABAA41B8BF",
+    lastUpdated: "2019-08-15T16:03:34.988007+00:00",
+    max_change_rate: "0.010000000000000000",
+    max_rate: "0.500000000000000000",
+    min_self_delegation: "1",
+    moniker: "mr_mounty",
+    operator_address: "cosmosvaladdr15ky9du8a2wlstz6fpx3p4mqpjyrm5ctplpn3au",
+    profileUrl: "https://keybase.io/melea",
+    rate: "0.080000000000000000",
     status: 2,
-    bond_height: `0`,
-    bond_intra_tx_counter: 6,
-    proposer_reward_pool: null,
-    commission: `0.05`,
-    prev_bonded_shares: `0`,
-    signing_info: {
-      start_height: 0,
-      index_offset: 465400,
-      jailed_until: `1970-01-01T00:00:00Z`,
-      missed_blocks_counter: 122
-    },
-    uptime: 0.98778883,
+    tokens: 14,
+    unbonding_height: 556130,
+    unbonding_time: "2019-06-26T23:50:09.38840326Z",
+    update_time: "2019-04-01T17:07:19.892771401Z",
+    uptime_percentage: "0.9962",
+    userName: "mr_mounty",
+    website: "www.monty.ca",
+
+    // Enriched locally
     expectedReturns: 0.13
   }
 
   beforeEach(() => {
     $store = {
-      commit: jest.fn(),
-      dispatch: jest.fn(),
       state: {
         pool: {
           pool: {
             bonded_tokens: 1000
           }
         }
-      },
-      getters: {
-        delegates: { delegates: [] },
-        committedDelegations: {},
-        bondDenom: `stake`
       }
     }
 
@@ -59,12 +56,12 @@ describe(`LiValidator`, () => {
       localVue,
       propsData: {
         validator,
-        disabled: false
+        showOnMobile: "returns"
       },
       mocks: {
         $store
       },
-      stubs: [`router-link`, `apolloquery`]
+      stubs: [`router-link`]
     })
   })
 
