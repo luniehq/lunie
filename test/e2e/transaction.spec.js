8138dffa1e32377cea6ff41464f77f0b413a9121
Fabian Weber <frznhope@gmail.com>
2019-09-04 15:57:53 +0200

    switched to send for transaction test

--- test/e2e/transaction.spec.js
+++ test/e2e/transaction.spec.js
@@ -2,7 +2,7 @@ var { globals } = require('./nightwatch.conf.js')
 const formData = require('./formData.json')
 
 module.exports = {
-  'Send Transaction': function (browser) {
+  'Send Transaction': function(browser) {
     browser
       //Import funded account
       .url(`chrome-extension://${globals.EXTENSION_ID}/popup/popup.html`)
@@ -26,13 +26,13 @@ module.exports = {
       .assert.containsText('body', formData.name)
 
       //Send transaction on Lunie to extension
-      .execute(function () {
+      .execute(function() {
         window.open('https://localhost:9080/?experimental=true/#/extension')
       })
       .pause(500)
 
       //Switch to Localhost
-      .windowHandles(function (result) {
+      .windowHandles(function(result) {
         browser
           .switchWindow(result.value[1])
           .pause(300)
@@ -42,10 +42,14 @@ module.exports = {
           .waitForElementVisible('li.account button')
           .click('li.account button')
           .waitForElementNotPresent('.session')
-          .click('a[href="/validators"]')
-          .waitForElementVisible('.li-validator')
-          .click('.li-validator')
-          .click('#delegation-btn')
+
+          // Perform a token send
+          .click('a[href="#/portfolio"]')
+          .click('.send-button')
+          .setValue(
+            "input[placeholder='Address']",
+            'cosmos1akhswynqs07zj5p2k25r6s4ur45qyjnr30wt59'
+          )
           .setValue("input[placeholder='Amount']", '1')
           .click('div.action-modal-footer button')
           .click('div.action-modal-footer button')
@@ -60,7 +64,7 @@ module.exports = {
           .switchWindow(result.value[1])
         browser.expect
           .element('body')
-          .text.to.contain('Successful delegation!')
+          .text.to.contain('Successful Send')
           .before(10000)
       })
   }
