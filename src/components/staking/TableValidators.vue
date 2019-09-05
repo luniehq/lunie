1e3315399b2a65412cd90d986aa11c1e4669c4cf
colw <colw@outlook.com>
2019-09-05 18:31:17 +0200

    Remove old animation code

--- src/components/staking/TableValidators.vue
+++ src/components/staking/TableValidators.vue
@@ -148,22 +148,6 @@ export default {
   methods: {
     loadMore() {
       this.showing += 10
-    },
-    beforeEnter: function(el) {
-      el.style.opacity = 0
-      el.style.height = 0
-    },
-    enter: function(el, done) {
-      var delay = el.dataset.index * 150
-      setTimeout(function() {
-        Velocity(el, { opacity: 1, height: "1.6em" }, { complete: done })
-      }, delay)
-    },
-    leave: function(el, done) {
-      var delay = el.dataset.index * 150
-      setTimeout(function() {
-        Velocity(el, { opacity: 0, height: 0 }, { complete: done })
-      }, delay)
     }
   }
 }
