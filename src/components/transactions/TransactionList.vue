9edfebaf453ef14f9e6b70c2d4e64cdd9038a75d
Fabian Weber <frznhope@gmail.com>
2019-09-06 15:16:04 +0200

    working tx grouping

--- src/components/transactions/TransactionList.vue
+++ src/components/transactions/TransactionList.vue
@@ -1,22 +1,46 @@
 <template>
   <div>
-    <TransactionItem
-      v-for="msg in transactions"
-      :key="msg.key"
-      :transaction="msg"
-      :validators="validators"
-      :address="address"
-    />
+    <template v-for="dayTxs in days">
+      <div :key="getDate(dayTxs[0])">
+        <h1 v-if="getDate(dayTxs[0]) === getDate({ time: new Date() })">
+          Today
+        </h1>
+        <h1 v-else>{{ dayTxs[0] | date }}</h1>
+        <TransactionItem
+          v-for="msg in dayTxs"
+          :key="msg.key"
+          :transaction="msg"
+          :validators="validators"
+          :address="address"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import TransactionItem from "./TransactionItem"
+import groupBy from "lodash.groupby"
+import moment from "moment"
+
+function getDate(tx) {
+  var dateObj = tx.time
+  var month = dateObj.getUTCMonth() + 1 //months from 1-12
+  var day = dateObj.getUTCDate()
+  var year = dateObj.getUTCFullYear()
+
+  return year + "/" + month + "/" + day
+}
 
 export default {
   name: `transaction-list`,
   components: {
     TransactionItem
+  },
+  filters: {
+    date(tx) {
+      return moment(tx.time).format("MMM Do YYYY")
+    }
   },
   props: {
     transactions: {
@@ -31,6 +55,14 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    days() {
+      return groupBy(this.transactions, getDate)
+    }
+  },
+  methods: {
+    getDate
   }
 }
 </script>
