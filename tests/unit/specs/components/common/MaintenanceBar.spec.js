6c061e92090cc7c5e47bed164ad391daf5184ae8
mariopino <info@quequiereshacer.es>
2019-09-09 13:24:38 +0200

    Add MaintenanceBar.spec.js, write tests, upd. snap

---
+++ tests/unit/specs/components/common/MaintenanceBar.spec.js
@@ -0,0 +1,70 @@
+import { shallowMount } from "@vue/test-utils"
+import MaintenanceBar from "common/MaintenanceBar"
+
+describe(`MaintenanceBar`, () => {
+  let wrapper
+  beforeEach(async () => {
+    wrapper = shallowMount(MaintenanceBar)
+  })
+
+  it(`has the expected html structure`, () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it(`show success message`, () => {
+    wrapper.setData({
+      maintenance: [
+        {
+          message: 'success test message',
+          type: 'success',
+          show: true
+        }        
+      ],
+    });
+    expect(wrapper.find(".maintenance-bar.success p").text())
+      .toBe("success test message");
+  })
+
+  it(`show warning message`, () => {
+    wrapper.setData({
+      maintenance: [
+        {
+          message: 'warning test message',
+          type: 'warning',
+          show: true
+        }        
+      ],
+    });
+    expect(wrapper.find(".maintenance-bar.warning p").text())
+      .toBe("warning test message");
+  })
+
+  it(`show danger message`, () => {
+    wrapper.setData({
+      maintenance: [
+        {
+          message: 'danger test message',
+          type: 'danger',
+          show: true
+        }        
+      ],
+    });
+    expect(wrapper.find(".maintenance-bar.danger p").text())
+      .toBe("danger test message");
+  })
+
+  it(`close message button works`, () => {
+    wrapper.setData({
+      maintenance: [
+        {
+          message: 'success test message',
+          type: 'success',
+          show: true
+        }        
+      ],
+    });
+    wrapper.vm.maintenance[0].show = false
+    expect(wrapper.find(".maintenance-bar.success p").exists())
+      .toBe(false)
+  })
+})
