9544e544ecf83411101026c1a3e15e77e843113a
mariopino <info@quequiereshacer.es>
2019-09-12 17:40:06 +0200

    Debug

--- src/components/common/DisconnectedBar.vue
+++ src/components/common/DisconnectedBar.vue
@@ -2,7 +2,7 @@
   <!-- <div v-if="stopConnecting"> -->
   <div v-if="true">
     <Bar :show="show" :type="'info'">
-      You are currently not connected. <a class="link" @click="emitEvent('reconnect')">Try Reconnecting</a>?
+      You are currently not connected. <a class="link" @click="reconnect()">Try Reconnecting</a>?
     </Bar>
   </div>
 </template>
@@ -35,12 +35,7 @@ export default {
   methods: {
     reconnect() {
       console.log("reconnect")
-      //this.$store.dispatch("reconnect")
-    },
-    emitEvent(event) {
-      //console.log(event)
-      //this.$emit(event)
-      this.reconnect()
+      this.$store.dispatch("reconnect")
     }
   }
 }
