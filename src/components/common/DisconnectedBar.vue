988d4698b3078589a4501927e957dabe644b0eb8
mariopino <info@quequiereshacer.es>
2019-09-12 14:31:58 +0200

    Use Bar comp. in DisconnectedBar

--- src/components/common/DisconnectedBar.vue
+++ src/components/common/DisconnectedBar.vue
@@ -1,76 +1,34 @@
 <template>
-  <div v-if="stopConnecting" class="disconnected-bar">
-    <i></i>
-    <p>
-      You are currently not connected.
-      <a class="link" @click="reconnect">Try Reconnecting</a>?
-    </p>
-    <a class="close">
-      <i class="material-icons" @click="reconnect">refresh</i>
-    </a>
+  <div v-if="stopConnecting">
+    <Bar :show="show" :type="'primary'">
+      You are currently not connected. <a class="link" @click="this.$emit('reconnect', true)">Try Reconnecting</a>?
+    </Bar>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex"
+import Bar from "common/Bar"
 export default {
   name: `disconnected-bar`,
+  components: {
+    Bar
+  },
+  data: () => ({
+    show: true,
+    reconnect: false
+  }),
   computed: {
     ...mapState({
       stopConnecting: state => state.connection.stopConnecting
     })
   },
-  methods: {
-    reconnect() {
-      this.$store.dispatch("reconnect")
+  watch: {
+    reconnect: function(val) {
+      if (val === true) {
+        this.$store.dispatch("reconnect")
+      }
     }
   }
 }
 </script>
-
-<style scoped>
-.disconnected-bar {
-  left: 0;
-  right: 0;
-  top: 0;
-  width: 100%;
-  padding: 1rem;
-  font-family: var(--sans);
-  background-color: #551f38;
-  font-size: 14px;
-  line-height: 20px;
-  font-weight: 400;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  color: var(--bright);
-}
-
-.disconnected-bar .link {
-  text-decoration: underline;
-  color: var(--bright);
-  cursor: pointer;
-}
-
-.disconnected-bar .close {
-  cursor: pointer;
-  height: 1rem;
-  width: 1rem;
-  color: var(--bright);
-}
-
-@media (max-width: 1024px) {
-  .disconnected-bar {
-    position: fixed;
-    top: auto;
-    z-index: 99;
-    padding: 0.5rem;
-    justify-content: space-around;
-  }
-
-  .hide-on-mobile {
-    display: none;
-  }
-}
-</style>
