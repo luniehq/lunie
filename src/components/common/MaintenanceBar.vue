027d3c65ff7157094adfecb1fe340af514271fa6
mariopino <info@quequiereshacer.es>
2019-09-16 11:55:25 +0200

    Change v-for key to message.id

--- src/components/common/MaintenanceBar.vue
+++ src/components/common/MaintenanceBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="(message, index) in maintenance" :key="index">
+    <div v-for="message in maintenance" :key="message.id">
       <Bar :bar-type="message.type" :show="message.show">{{ message.message }}</Bar>
     </div>
   </div>
