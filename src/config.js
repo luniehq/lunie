eeb4ca0c63cfe9146c3dfa88f8b44b7a5c20a265
Jordan Bibla <jbibla@gmail.com>
2019-09-26 15:18:21 -0400

    cleaned up config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const dev = process.env.NODE_ENV === `development`
-const stargate = process.env.STARGATE || `http://localhost:9071`
-const rpc = process.env.RPC || `localhost:26657`
+const stargate = process.env.STARGATE || `https://lcd.nylira.net`
+const rpc = process.env.RPC || `https://rpc.nylira.net`
+const graphql = process.env.VUE_APP_GRAPHQL_URL || `https://backend.lunie.io/v1/graphql`
 
 export default {
   name: `Lunie`,
@@ -8,6 +9,7 @@ export default {
   network: dev ? `testnet` : `cosmoshub`,
   stargate,
   rpc,
+  graphql,
   google_analytics_uid: process.env.GOOGLE_ANALYTICS_UID || "",
   node_halted_timeout: 120000,
   block_timeout: 10000,
