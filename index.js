3d3ba1115f0d1b355de50e431439a5ac25cd0fc2
colw <colw@outlook.com>
2019-09-27 14:32:15 +0200

    Move Redis setup to own file

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ require("dotenv").config();
 var url = require("url");
 
 const { ApolloServer } = require("apollo-server");
-const { RedisCache } = require("apollo-server-cache-redis");
 const typeDefs = require("./lib/schema");
 const resolvers = require("./lib/resolvers");
 const CosmosAPI = require("./lib/cosmos-source");
 const networkData = require("./data/networks");
 
-const chainUrl = process.env.CHAIN_URL || "https://lcd.nylira.net";
-
 let options = {
   typeDefs,
   resolvers,
   dataSources: () => ({
-    cosmosAPI: new CosmosAPI(chainUrl),
+    cosmosAPI: new CosmosAPI(process.env.CHAIN_URL || "https://lcd.nylira.net"),
     networkData
   }),
   cacheControl: {
@@ -28,32 +25,14 @@ let options = {
   }
 };
 
-let redis_uri;
-if (process.env.NODE_ENV === "development") {
-  redis_uri = {
-    hostname: `localhost`,
-    port: 6379
-  };
-} else {
-  redis_uri = url.parse(process.env.REDIS_URL || "");
-}
-
 if (process.env.ENABLE_CACHE) {
-  options.cache = new RedisCache({
-    host: redis_uri.hostname,
-    port: redis_uri.port,
-    password: redis_uri.auth && redis_uri.auth.split(":")[1]
-  });
+  options.cache = require("./lib/redis").createCache();
 }
-
-console.log("SETTINGS");
-console.log("NODE_ENV", JSON.stringify(process.env));
-console.log("redis_uri", redis_uri);
 
 const server = new ApolloServer(options);
 
 server
   .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => `GraphQL Server listening on ${url}`)
-  // .then(console.log)
+  .then(console.log)
   .catch(console.error);
