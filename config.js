23b8ec0e4dd3287a78c13b0d47a63cfa2b50bef2
colw <colw@outlook.com>
2019-10-08 15:04:44 +0200

    Lint config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
-  env: process.env.NODE_ENV || "development",
-  port: process.env.PORT || "4000",
-  apollo_engine_api_key: process.env.ENGINE_API_KEY || "",
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || '4000',
+  apollo_engine_api_key: process.env.ENGINE_API_KEY || '',
   enable_cache: process.env.ENABLE_CACHE || false,
-  redis_url: process.env.REDIS_URL || ""
-};
+  redis_url: process.env.REDIS_URL || ''
+}
