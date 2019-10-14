dc6e2508705fed0200425ec63d7626df4295d0e5
aleksey rudometov <iambeone@gmail.com>
2019-10-14 11:32:41 +0500

    axios replaced with default RESTDataSource methods

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
   apollo_engine_api_key: process.env.ENGINE_API_KEY || '',
   enable_cache: process.env.ENABLE_CACHE || false,
   redis_url: process.env.REDIS_URL || '',
-  hasura_admin_key: process.env.HASURA_ADMIN_KEY || "",
+  hasura_admin_key: process.env.HASURA_ADMIN_KEY || '',
   hasura_url: env == 'development' ? "https://staging-db.lunie.io/v1/graphql" : 'https://production-db.lunie.io/v1/graphql',
 }
