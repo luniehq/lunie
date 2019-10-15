e1274df021b884f737dc774ae3abb6bff84d5578
mariopino <info@quequiereshacer.es>
2019-10-15 12:33:14 +0200

    Update insecure mode warning

--- src/components/common/InsecureModeWarning.vue
+++ src/components/common/InsecureModeWarning.vue
@@ -1,26 +1,16 @@
 <template>
   <div>
     <p>
-      Creating an address in the browser is unsafe. To offer you a secure
-      alternative we will be releasing a browser extension and a mobile app
-      soon.
-    </p>
-    <p>
-      In the meantime, you can create a new account outside of the browser by
-      using a
+      Did you know that storing your key in the browser is considered unsafe? We
+      offer a more secure way to storing your keys via our
       <a
-        href="https://shop.ledger.com/?r=3dd204ef7508"
+        href="http://bit.ly/lunie-ext"
         target="_blank"
         rel="noopener norefferer"
-        >Ledger Nano</a
       >
-      or the
-      <a
-        href="https://hub.cosmos.network/docs/delegator-guide-cli.html#creating-an-account"
-        target="_blank"
-        rel="noopener norefferer"
-        >command line</a
-      >.
+        browser extension</a
+      >. Check it out in a quiet minute. Soon you will also have out mobile app
+      available to store your keys. Stay tuned!
     </p>
     <router-link to="existing">Want to use an existing address?</router-link>
   </div>
