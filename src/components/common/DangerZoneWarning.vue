d2b712c68cea60f88b1a9f676c23eefb4651a1a8
mariopino <info@quequiereshacer.es>
2019-10-16 00:20:05 +0200

    Lint

--- src/components/common/DangerZoneWarning.vue
+++ src/components/common/DangerZoneWarning.vue
@@ -2,8 +2,8 @@
   <div class="danger-zone">
     <h2>DANGER ZONE</h2>
     <p>
-      Did you know that creating your address in the browser is considered unsafe? We
-      offer a more secure way to do this with our
+      Did you know that creating your address in the browser is considered
+      unsafe? We offer a more secure way to do this with our
       <a
         href="http://bit.ly/lunie-ext"
         target="_blank"
