a6313529e153193ed9b750295fcd28533cbe9c74
mariopino <info@quequiereshacer.es>
2019-10-16 22:17:21 +0200

    Improve test

--- test/e2e/createAccount.spec.js
+++ test/e2e/createAccount.spec.js
@@ -33,22 +33,30 @@ module.exports = {
       .click('a[href="#/create"]')
       .pause(500)
       // Name
+      .waitForElementVisible('h2.session-title')
       .setValue(
         "input[placeholder='Must be at least 5 characters']",
         formData.name
       )
       .click('div.session-footer button')
+      .pause(500)
       // Password
+      .waitForElementVisible('h2.session-title')
       .setValue(
         "input[placeholder='Must be at least 10 characters']",
         formData.password
       )
       .setValue("input[placeholder='Enter password again']", formData.password)
       .click('div.session-footer button')
+      .pause(500)
       // Seed
-      .click('input')
+      .waitForElementVisible('h2.session-title')
+      .click('div.field-checkbox-input label')
+      .pause(500)
       .click('div.session-footer button')
+      .pause(500)
       // Confirm
+      .waitForElementVisible('h2.session-title')
       .assert.containsText('body', 'Your account has been created')
   }
 }
