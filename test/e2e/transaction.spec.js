3a1ba7b39deee942eb0ceaea9f47bdcf33320d46
mariopino <info@quequiereshacer.es>
2019-10-16 22:30:35 +0200

    Update transaction test

--- test/e2e/transaction.spec.js
+++ test/e2e/transaction.spec.js
@@ -5,25 +5,37 @@ module.exports = {
   'Send Transaction': function(browser) {
     browser
       //Import funded account
-      .url(`chrome-extension://${globals.EXTENSION_ID}/popup/popup.html`)
-      .waitForElementVisible('.tm-li-session-title')
-      .click('a[href="#/recover"]')
       .pause(500)
+      .url(`chrome-extension://${globals.EXTENSION_ID}/popup/popup.html`)
+      .waitForElementVisible('a[href="#/create"]')
+      .click('a[href="#/create"]')
+      .pause(500)
+      // Name
+      .waitForElementVisible('h2.session-title')
       .setValue(
-        "input[placeholder='Must have at least 5 characters']",
+        "input[placeholder='Must be at least 5 characters']",
         formData.name
       )
+      .click('div.session-footer button')
+      .pause(500)
+      // Password
+      .waitForElementVisible('h2.session-title')
       .setValue(
         "input[placeholder='Must be at least 10 characters']",
         formData.password
       )
       .setValue("input[placeholder='Enter password again']", formData.password)
-      .setValue(
-        "textarea[placeholder='Must be exactly 24 words']",
-        formData.seedPhrase
-      )
       .click('div.session-footer button')
-      .assert.containsText('body', formData.name)
+      .pause(500)
+      // Seed
+      .waitForElementVisible('h2.session-title')
+      .click('div.field-checkbox-input label')
+      .pause(500)
+      .click('div.session-footer button')
+      .pause(500)
+      // Confirm
+      .waitForElementVisible('h2.session-title')
+      .assert.containsText('body', 'Your account has been created')
 
       //Send transaction on Lunie to extension
       .execute(function() {
