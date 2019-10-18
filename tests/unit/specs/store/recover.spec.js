cdc90a83200995aa9aa0bd4765daa8a5e81892b1
mariopino <info@quequiereshacer.es>
2019-10-18 15:31:16 +0200

    Improve testing of store modules, update snaps

---
+++ tests/unit/specs/store/recover.spec.js
@@ -0,0 +1,43 @@
+import recoverModule from "src/vuex/modules/recover.js"
+
+describe(`Module: Recover`, () => {
+  let module, state, actions, mutations, node
+
+  const emptyState = {
+    name: ``,
+    seed: ``,
+    password: ``,
+    passwordConfirm: ``,
+    warning: false
+  }
+
+  beforeEach(() => {
+    node = {}
+    module = recoverModule({ node })
+    state = module.state
+    actions = module.actions
+    mutations = module.mutations
+  })
+
+  describe(`mutations`, () => {
+    it(`should update field`, () => {
+      const name = `HappyLunieUser`
+      expect(state.name).toBe(``)
+      mutations.updateField(state, { field: `name`, value: name })
+      expect(state.name).toEqual(name)
+    })
+
+    it(`should reset recover data`, () => {
+      mutations.resetRecoverData(state)
+      expect(state).toEqual(emptyState)
+    })
+  })
+
+  describe(`actions`, () => {
+    it(`should reset recover data`, async () => {
+      const commit = jest.fn()
+      await actions.resetRecoverData({ commit })
+      expect(commit).toHaveBeenCalledWith(`resetRecoverData`)
+    })
+  })
+})
