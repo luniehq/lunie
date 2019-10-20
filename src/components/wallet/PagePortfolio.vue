371985c49c78cd9a0b4912cfc6d0cb6569914901
colw <colw@outlook.com>
2019-10-20 12:58:15 +0200

    PagePortfolio update, testing

--- src/components/wallet/PagePortfolio.vue
+++ src/components/wallet/PagePortfolio.vue
@@ -1,9 +1,9 @@
 <template>
   <TmPage
     :managed="true"
-    :loading="wallet.loading && delegation.loading"
-    :loaded="wallet.loaded && delegation.loaded"
-    :error="wallet.error || delegation.error"
+    :loading="false"
+    :loaded="true"
+    :error="false"
     :sign-in-required="true"
   >
     <template slot="managed-body">
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex"
 import TmPage from "common/TmPage"
 import DelegationsOverview from "staking/DelegationsOverview"
 import Undelegations from "staking/Undelegations"
@@ -25,35 +24,6 @@ export default {
     TmPage,
     Undelegations,
     DelegationsOverview
-  },
-  data: () => ({
-    lastUpdate: 0
-  }),
-  computed: {
-    ...mapState([`session`, `wallet`, `delegation`]),
-    ...mapGetters([`lastHeader`])
-  },
-  watch: {
-    lastHeader: {
-      immediate: true,
-      handler: function(newHeader) {
-        const height = Number(newHeader.height)
-        // run the update queries the first time and after every 10 blocks
-        const waitedTenBlocks = height - this.lastUpdate >= 10
-        if (
-          this.session.signedIn &&
-          (this.lastUpdate === 0 || waitedTenBlocks)
-        ) {
-          this.update(height)
-        }
-      }
-    }
-  },
-  methods: {
-    update(height) {
-      this.lastUpdate = height
-      this.$store.dispatch(`getRewardsFromMyValidators`)
-    }
   }
 }
 </script>
