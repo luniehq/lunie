c680d62384f5b65584fea1f835dac1bb5f106d02
Fabian Weber <frznhope@gmail.com>
2019-10-21 15:24:47 -0400

    skip more

--- src/components/wallet/PageTransactions.vue
+++ src/components/wallet/PageTransactions.vue
@@ -108,6 +108,9 @@ export default {
           }
         }
       `,
+      skip() {
+        return !this.address
+      },
       variables() {
         return {
           networkId: this.network
