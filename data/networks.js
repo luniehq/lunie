a86cd1dc8efdc3ea2fffa26958220e948ddb8980
Fabian Weber <frznhope@gmail.com>
2019-10-20 20:19:32 -0400

    removed hard coded network id

--- data/networks.js
+++ data/networks.js
@@ -2,10 +2,12 @@ const config = require('../config')
 
 let networks = {
   'gaia-testnet': {
+    id: 'gaia-testnet',
     api_url: 'https://gaia-13006.lunie.io',
     rpc_url: 'wss://gaia-13006.lunie.io:26657/websocket'
   },
   cosmoshub: {
+    id: 'cosmoshub',
     api_url: 'https://lcd.nylira.net',
     rpc_url: 'wss://rpc.nylira.net/websocket'
   }
@@ -13,6 +15,7 @@ let networks = {
 
 if (config.enableTestnet) {
   networks.testnet = {
+    id: 'testnet',
     api_url: 'http://localhost:9070',
     rpc_url: 'ws://localhost:26657/websocket'
   }
