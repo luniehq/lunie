bc540472fbfdbd07d0397933f187bee301218f6b
Fabian Weber <frznhope@gmail.com>
2019-10-20 21:34:26 -0400

    test cleanup

--- tests/unit/specs/store/session.spec.js
+++ tests/unit/specs/store/session.spec.js
@@ -119,9 +119,6 @@ describe(`Module: Session`, () => {
         `cosmos15ky9du8a2wlstz6fpx3p4mqpjyrm5ctpesxxn9`
       )
       expect(commit).toHaveBeenCalledWith(`setSessionType`, `local`)
-      expect(dispatch).toHaveBeenCalledWith(`initializeWallet`, {
-        address: `cosmos15ky9du8a2wlstz6fpx3p4mqpjyrm5ctpesxxn9`
-      })
       expect(state.externals.track).toHaveBeenCalledWith(
         `event`,
         `session`,
@@ -140,7 +137,6 @@ describe(`Module: Session`, () => {
       )
       expect(commit).toHaveBeenCalledWith(`setUserAddress`, address)
       expect(commit).toHaveBeenCalledWith(`setSessionType`, `ledger`)
-      expect(dispatch).toHaveBeenCalledWith(`initializeWallet`, { address })
       expect(state.externals.track).toHaveBeenCalledWith(
         `event`,
         `session`,
@@ -159,7 +155,6 @@ describe(`Module: Session`, () => {
       )
       expect(commit).toHaveBeenCalledWith(`setUserAddress`, address)
       expect(commit).toHaveBeenCalledWith(`setSessionType`, `explore`)
-      expect(dispatch).toHaveBeenCalledWith(`initializeWallet`, { address })
       expect(state.externals.track).toHaveBeenCalledWith(
         `event`,
         `session`,
@@ -193,7 +188,7 @@ describe(`Module: Session`, () => {
   })
 
   it(`should enable error collection`, async () => {
-    jest.spyOn(console, `log`).mockImplementationOnce(() => {})
+    jest.spyOn(console, `log`).mockImplementationOnce(() => { })
     const commit = jest.fn()
     const dispatch = jest.fn()
     await actions.setErrorCollection(
@@ -209,7 +204,7 @@ describe(`Module: Session`, () => {
   })
 
   it(`should disable error collection`, async () => {
-    jest.spyOn(console, `log`).mockImplementationOnce(() => {})
+    jest.spyOn(console, `log`).mockImplementationOnce(() => { })
     const commit = jest.fn()
     const dispatch = jest.fn()
     await actions.setErrorCollection(
@@ -225,7 +220,7 @@ describe(`Module: Session`, () => {
   })
 
   it(`should disable analytics collection`, async () => {
-    jest.spyOn(console, `log`).mockImplementationOnce(() => {})
+    jest.spyOn(console, `log`).mockImplementationOnce(() => { })
     const commit = jest.fn()
     const dispatch = jest.fn()
     await actions.setAnalyticsCollection(
