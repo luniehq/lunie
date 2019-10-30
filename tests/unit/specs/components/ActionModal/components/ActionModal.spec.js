91f1115c3140d99d2a3c946d17c8a854afe16484
colw <colw@outlook.com>
2019-10-30 14:34:57 +0100

    Lint

--- tests/unit/specs/components/ActionModal/components/ActionModal.spec.js
+++ tests/unit/specs/components/ActionModal/components/ActionModal.spec.js
@@ -6,7 +6,7 @@ import { focusParentLast } from "src/directives"
 const localVue = createLocalVue()
 localVue.use(Vuelidate)
 localVue.directive("focus-last", focusParentLast)
-localVue.directive("focus", () => { })
+localVue.directive("focus", () => {})
 
 let mockSimulate = jest.fn(() => 123456)
 let mockSend = jest.fn(() => ({
@@ -150,7 +150,7 @@ describe(`ActionModal`, () => {
     const self = {
       $store,
       actionManager: {
-        setContext: () => { },
+        setContext: () => {},
         simulate: () => 12345,
         send: ActionManagerSend
       },
@@ -164,7 +164,7 @@ describe(`ActionModal`, () => {
       },
       submissionErrorPrefix: `PREFIX`,
       trackEvent: jest.fn(),
-      connectLedger: () => { },
+      connectLedger: () => {},
       onSendingFailed: jest.fn()
     }
     await ActionModal.methods.submit.call(self)
@@ -607,7 +607,7 @@ describe(`ActionModal`, () => {
         isValidInput: jest.fn(() => true),
         selectedSignMethod: `local`,
         step: `details`,
-        validateChangeStep: jest.fn(() => { })
+        validateChangeStep: jest.fn(() => {})
       }
     })
 
