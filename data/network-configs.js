0a15615f8cfd2f813b745ffd331c75cbe4a23972
Fabian <frznhope@gmail.com>
2019-10-30 13:23:59 +0100

    Fabo/move testnet definition into network configs file (#56)
    * move testnet definition into network configs file
    
    * use configs for endpoints

--- data/network-configs.js
+++ data/network-configs.js
@@ -1,5 +1,31 @@
 const config = require('../config')
 
+const testnet = {
+  id: 'local-cosmos-hub-testnet',
+  title: 'Local Cosmos Testnet',
+  chain_id: 'testnet',
+  rpc_url: config.testnetRPC,
+  api_url: config.testnetAPI,
+  bech32_prefix: 'cosmos',
+  testnet: true,
+  feature_session: true,
+  feature_portfolio: true,
+  feature_validators: true,
+  feature_proposals: true,
+  feature_activity: true,
+  feature_explorer: true,
+  action_send: true,
+  action_claim_rewards: true,
+  action_delegate: true,
+  action_redelegate: true,
+  action_undelegate: true,
+  action_deposit: true,
+  action_vote: true,
+  action_proposal: true,
+  experimental: true,
+  stakingDenom: 'STAKE'
+}
+
 let networks = {
   'cosmos-hub-testnet': {
     id: 'cosmos-hub-testnet',
@@ -14,11 +40,7 @@ let networks = {
 }
 
 if (config.enableTestnet) {
-  networks['local-cosmos-hub-testnet'] = {
-    id: 'local-cosmos-hub-testnet',
-    api_url: config.testnetAPI,
-    rpc_url: config.testnetRPC
-  }
+  networks['local-cosmos-hub-testnet'] = testnet
 }
 
 module.exports = {
