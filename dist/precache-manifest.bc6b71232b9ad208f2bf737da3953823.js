5bb1f43284457f94f6a6be6c52c57d2f582b0492
Fabian Weber <frznhope@gmail.com>
2019-10-31 13:05:26 +0100

    temporary skip build step to debug ci faster

---
+++ dist/precache-manifest.bc6b71232b9ad208f2bf737da3953823.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b99e772117c42ea7aa01418e18f9f79b",
+    "url": "/img/lunie-logo-white.b99e7721.svg"
+  },
+  {
+    "revision": "07f102808481023d2c58292b28e0898e",
+    "url": "/img/networks/gaia-testnet.png"
+  },
+  {
+    "revision": "ef6ef962744654179f97",
+    "url": "/js/chunk-2d0b9633.3ba5c65c.js"
+  },
+  {
+    "revision": "f3e1dda5810bb06cac6f",
+    "url": "/js/chunk-2d0ba81d.eae89fb6.js"
+  },
+  {
+    "revision": "bdf7401c9415b3a3513d",
+    "url": "/js/chunk-vendors.bd79f741.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "14a83cc8dbe5a6c700049e0327eab3c8",
+    "url": "/img/readme-header.svg"
+  },
+  {
+    "revision": "6aaf238ea2cac8b913009760fcacc005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51da9c843660bdf999af",
+    "url": "/js/app.b4485e28.js"
+  },
+  {
+    "revision": "07f102808481023d2c58292b28e0898e",
+    "url": "/img/networks/cosmoshub.png"
+  },
+  {
+    "revision": "e6f21027ccb17e09e7c2bd7636de4608",
+    "url": "/img/loader.e6f21027.svg"
+  },
+  {
+    "revision": "e2dcdf112491564b63272bcfd93380ae",
+    "url": "/img/jordan.e2dcdf11.jpg"
+  },
+  {
+    "revision": "bd568e3f500c113a71082f13111061dc",
+    "url": "/img/fabo.bd568e3f.jpg"
+  },
+  {
+    "revision": "07f102808481023d2c58292b28e0898e",
+    "url": "/img/cosmos-logo.07f10280.png"
+  },
+  {
+    "revision": "51da9c843660bdf999af",
+    "url": "/css/app.3423f212.css"
+  },
+  {
+    "revision": "cf6956cf9a47b2fae113174b7a116f3e",
+    "url": "/_redirects"
+  }
+];
