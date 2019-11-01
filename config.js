ee7e5965cc350b1bf30f4b2ece12e5fff573f38a
Fabian Weber <frznhope@gmail.com>
2019-11-01 03:24:48 +0100

    use correct values for e2e

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const dev = process.env.NODE_ENV === `development`
 const stargate = process.env.STARGATE || `http://localhost:9071`
 const rpc = process.env.RPC || `localhost:26657`
-const graphql = process.env.VUE_APP_GRAPHQL_URL || `localhost:8080`
+const graphql = process.env.VUE_APP_GRAPHQL_URL || `http://localhost:8080/v1/graphql`
 
 export default {
   name: `Lunie`,
   development: dev,
-  network: dev ? `testnet` : `cosmoshub`,
+  network: process.env.VUE_APP_E2E ? `testnet` : `cosmoshub`,
   stargate,
   rpc,
   graphql,
