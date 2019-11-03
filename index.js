dee5640dada9a0d75ca4b87d7450de3e561ff467
Col <colw@users.noreply.github.com>
2019-11-03 22:34:42 +0100

    colw/subscription-refactor (#63)
    * Factor out subscription object
    
    * WIP
    
    * Reenable subs
    
    * Do not pre-populate cache (temp)
    
    This is run on every request.
    
    * Reenable static data
    
    * Fix
    
    * Remove comment
    
    * WIP
    
    * Use API class within subscription object
    
    * Update file and parameter naming
    
    * Load API classes once.
    
    * Revert "Load API classes once."
    
    ca567095360dbd7a8d81c7a53fe4fe885f3b63d2
    
    * Set headers in separate method
    
    * format comment
    
    * Rename class variable
    
    * Split static data up
    
    * Adjust cache timing, default 5
    
    * Update lib/schema.js
    
    Co-Authored-By: Fabian <frznhope@gmail.com>
    
    * Change client name to TendermintClient
    
    * Comment and rename for clarity
    
    * Cache specific blocks longer
    
    * Move Tendermint client to CosmosNodeSubscription
    
    * Comments for clarity

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 const { ApolloServer } = require('apollo-server')
+const responseCachePlugin = require('apollo-server-plugin-response-cache')
 const typeDefs = require('./lib/schema')
 const resolvers = require('./lib/resolvers')
+const CosmosNodeSubscription = require('./lib/cosmos-node-subscription')
 const CosmosV0API = require('./lib/cosmosV0-source')
 const CosmosV2API = require('./lib/cosmosV2-source')
 const LunieDBAPI = require('./lib/luniedb-source')
 const { networks } = require('./data/network-configs')
 const config = require('./config')
 
-const cosmosHubMainnetAPI = new CosmosV0API(networks['cosmos-hub-mainnet'])
-const cosmosHubTestnetAPI = new CosmosV2API(networks['cosmos-hub-testnet'])
-const lunieDBAPI = new LunieDBAPI()
+new CosmosNodeSubscription(networks['cosmos-hub-mainnet'], CosmosV0API)
+new CosmosNodeSubscription(networks['cosmos-hub-testnet'], CosmosV2API)
 
-const dataSources = {
-  CosmosHubMainnetAPI: cosmosHubMainnetAPI,
-  CosmosHubTestnetAPI: cosmosHubTestnetAPI,
-  LunieDBAPI: lunieDBAPI
-}
 if (config.enableTestnet) {
-  const localCosmosTestnetAPI = new CosmosV0API(
-    networks['local-cosmos-hub-testnet']
-  )
-  dataSources.TestnetAPI = localCosmosTestnetAPI
+  new CosmosNodeSubscription(networks['local-cosmos-hub-testnet'], CosmosV0API)
+}
+
+function createDataSources() {
+  const dataSources = {
+    CosmosHubMainnetAPI: new CosmosV0API(networks['cosmos-hub-mainnet']),
+    CosmosHubTestnetAPI: new CosmosV2API(networks['cosmos-hub-testnet']),
+    LunieDBAPI: new LunieDBAPI()
+  }
+  if (config.enableTestnet) {
+    dataSources.TestnetAPI = new CosmosV0API(
+      networks['local-cosmos-hub-testnet']
+    )
+  }
+  return dataSources
 }
 
 let options = {
   typeDefs,
   resolvers,
-  dataSources: () => dataSources,
+  dataSources: createDataSources,
   cacheControl: {
-    defaultMaxAge: 5000
+    defaultMaxAge: 10
   },
   introspection: true,
   playground: true,
   engine: {
     apiKey: config.apollo_engine_api_key,
     schemaTag: 'production'
-  }
+  },
+  plugins: [responseCachePlugin()]
 }
 
 if (config.enable_cache) {
