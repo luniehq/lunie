d69016629a42d922e164c00308c9340ba61b941c
Fabian Weber <frznhope@gmail.com>
2019-11-05 00:53:24 +0100

    remove network query parameter

--- tests/e2e/local-chrome.conf.js
+++ tests/e2e/local-chrome.conf.js
@@ -6,7 +6,7 @@ const nightwatch_config = {
   globals_path: "./globals.js",
   disable_colors: false,
   test_workers: false,
-  launch_url: "http://localhost:9080?network=local-cosmos-hub-testnet",
+  launch_url: "http://localhost:9080",
   webdriver: {
     start_process: true,
     port: 9515,
