8194065b05c1e7927585c497c5b60c8bf6a86e09
Fabian Weber <frznhope@gmail.com>
2019-11-06 17:36:30 +0100

    linted

--- tests/e2e/local-run.js
+++ tests/e2e/local-run.js
@@ -2,7 +2,7 @@ const util = require("util")
 const fs = require("fs")
 const exec = util.promisify(require("child_process").exec)
 const spawn = require("child_process").spawn
-const inquirer = require(`inquirer`);
+const inquirer = require(`inquirer`)
 
 const processes = {}
 
@@ -11,7 +11,9 @@ const processes = {}
 const main = async () => {
   if (!fs.existsSync("./lunie-backend")) {
     console.log("cloning lunie-backend repo")
-    await exec("git clone https://github.com/luniehq/lunie-backend.git lunie-backend")
+    await exec(
+      "git clone https://github.com/luniehq/lunie-backend.git lunie-backend"
+    )
   } else {
     console.log("updating lunie-backend repo")
     await exec("cd lunie-backend && git pull origin develop")
@@ -22,8 +24,14 @@ const main = async () => {
 
   console.log("starting website")
   const serve = spawn("yarn", ["test:e2e:serve"])
-  serve.stdout.pipe(process.stdout, { end: true })
-  serve.stderr.pipe(process.stderr, { end: true })
+  serve.stdout.pipe(
+    process.stdout,
+    { end: true }
+  )
+  serve.stderr.pipe(
+    process.stderr,
+    { end: true }
+  )
   // await until page is served
   await new Promise(resolve => {
     serve.stdout.on("data", async data => {
@@ -45,7 +53,7 @@ const terminateProcesses = async () => {
   processes.test.kill()
   console.log("Terminating Lunie website process")
   processes.serve.kill()
-  process.exit(exitCode)
+  process.exit(0)
 }
 
 const runTests = () => {
@@ -59,8 +67,14 @@ const runTests = () => {
   const filter = process.argv[2]
   if (filter) testArgs = testArgs.concat(`--filter`, `*${filter}*`)
   const test = spawn("yarn", testArgs)
-  test.stdout.pipe(process.stdout, { end: true })
-  test.stderr.pipe(process.stderr, { end: true })
+  test.stdout.pipe(
+    process.stdout,
+    { end: true }
+  )
+  test.stderr.pipe(
+    process.stderr,
+    { end: true }
+  )
 
   // cleanup on exit
   test.stderr.on("data", async data => {
@@ -78,14 +92,16 @@ const runTests = () => {
   processes.test = test
 }
 
-const onEnd = async (success) => {
+const onEnd = async () => {
   await new Promise(resolve => setTimeout(resolve), 500)
-  const answers = await inquirer.prompt([{
-    type: `confirm`,
-    name: `runAgain`,
-    message: `Want to run the tests again?`,
-    default: false
-  }])
+  const answers = await inquirer.prompt([
+    {
+      type: `confirm`,
+      name: `runAgain`,
+      message: `Want to run the tests again?`,
+      default: false
+    }
+  ])
 
   if (answers.runAgain) {
     runTests()
