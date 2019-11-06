aacc82c932db93065d3b963d4254edae75647e57
Fabian Weber <frznhope@gmail.com>
2019-11-06 12:27:36 +0100

    switch back to waitforelement

--- tests/e2e/signin.spec.js
+++ tests/e2e/signin.spec.js
@@ -1,27 +1,27 @@
 module.exports = {
-  "Sign in with local account": async function(browser) {
+  "Sign in with local account": async function (browser) {
     prepare(browser)
 
     browser.click("#use-an-existing-address")
-    browser.expect("#sign-in-with-account").to.be.visible.before(10000)
+    browser.waitForElementVisible("#sign-in-with-account", 10000, true)
     browser.pause(500)
     browser.click("#sign-in-with-account")
-    browser.expect("#sign-in-name").to.be.visible.before(10000)
+    browser.waitForElementVisible("#sign-in-name", 10000, true)
     browser.click(
       "#sign-in-name option[value=cosmos1ek9cd8ewgxg9w5xllq9um0uf4aaxaruvcw4v9e]"
     )
     browser.setValue("#sign-in-password", "1234567890")
     await next(browser)
     // check if signed in
-    browser.expect(".session").to.be.present.before(10000)
+    browser.waitForElementPresent(".session", 10000, true)
     openMenu(browser)
-    browser.expect("#sign-out").to.be.visible.before(10000)
+    browser.waitForElementVisible("#sign-out", 10000, true)
   },
-  "Create local account": async function(browser) {
+  "Create local account": async function (browser) {
     prepare(browser)
 
     browser.click("#creat-new-address")
-    browser.expect(".seed-table").to.be.visible.before(10000)
+    browser.waitForElementVisible(".seed-table", 10000, true)
     browser.expect
       .element(".seed-table")
       .value.to.match(/\w+( \w+){23}/)
@@ -48,18 +48,18 @@ module.exports = {
     browser.click("#sign-up-warning")
     await next(browser)
     // check if signed in
-    browser.expect(".session").to.be.present.before(10000)
+    browser.waitForElementPresent(".session", 10000, true)
     openMenu(browser)
-    browser.expect("#sign-out").to.be.visible.before(10000)
+    browser.waitForElementVisible("#sign-out", 10000, true)
   },
-  "Import local account": async function(browser) {
+  "Import local account": async function (browser) {
     prepare(browser)
 
     browser.click("#use-an-existing-address")
-    browser.expect("#recover-with-backup").to.be.visible.before(10000)
+    browser.waitForElementVisible("#recover-with-backup", 10000, true)
     browser.pause(500)
     browser.click("#recover-with-backup")
-    browser.expect("#import-seed").to.be.visible.before(10000)
+    browser.waitForElementVisible("#import-seed", 10000, true)
 
     await next(browser)
     browser.expect.elements(".tm-form-msg--error").count.to.equal(3)
@@ -85,15 +85,15 @@ module.exports = {
     )
     await next(browser)
     // check if signed in
-    browser.expect(".session").to.be.present.before(10000)
+    browser.waitForElementPresent(".session", 10000, true)
     openMenu(browser)
-    browser.expect("#sign-out").to.be.visible.before(10000)
+    browser.waitForElementVisible("#sign-out", 10000, true)
   }
 }
 
 async function next(browser) {
   browser.execute(
-    function(selector, scrollX, scrollY) {
+    function (selector, scrollX, scrollY) {
       var elem = document.querySelector(selector)
       elem.scrollLeft = scrollX
       elem.scrollTop = scrollY
@@ -105,30 +105,30 @@ async function next(browser) {
 }
 
 function openMenu(browser) {
-  browser.expect(".open-menu").to.be.visible.before(10000)
+  browser.waitForElementVisible(".open-menu", 10000, true)
   browser.pause(500)
   browser.click(".open-menu")
 }
 
 function signOut(browser) {
   openMenu(browser)
-  browser.expect("#sign-out").to.be.visible.before(10000)
+  browser.waitForElementVisible("#sign-out", 10000, true)
   browser.click("#sign-out")
 }
 
 function signIn(browser) {
   openMenu(browser)
-  browser.expect("#sign-in").to.be.visible.before(10000)
+  browser.waitForElementVisible("#sign-in", 10000, true)
   browser.click("#sign-in")
 }
 
 function prepare(browser) {
   browser.resizeWindow(400, 1024) // force mobile screen to be able to click some out of screen buttons
   browser.url(browser.launch_url)
-  browser.expect(`body`).to.be.visible.before(10000)
-  browser.expect(`#app-content`).to.be.visible.before(10000)
+  browser.waitForElementVisible(`body`, 10000, true)
+  browser.waitForElementVisible(`#app-content`, 10000, true)
   signOut(browser)
   signIn(browser)
 
-  browser.expect("#session-welcome").to.be.visible.before(10000)
+  browser.waitForElementVisible("#session-welcome", 10000, true)
 }
