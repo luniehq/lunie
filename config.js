aa108e6c95e8995f921b973937426b9756041441
Fabian Weber <frznhope@gmail.com>
2019-11-07 12:05:46 +0100

    bring back the network env variable

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const graphql = process.env.VUE_APP_GRAPHQL_URL || `http://localhost:4000`
 export default {
   name: `Lunie`,
   development: dev,
-  network: process.env.VUE_APP_E2E ? `local-cosmos-hub-testnet` : `cosmos-hub-mainnet`,
+  network: process.env.NETWORK || `cosmos-hub-mainnet`,
   stargate,
   google_analytics_uid: process.env.GOOGLE_ANALYTICS_UID || "",
   default_gas_price: dev ? 1e-9 : 2.5e-8, // Recommended from Cosmos Docs
