a7ea681aa0d5239ee62f99dc869291615867c831
Fabian Weber <frznhope@gmail.com>
2019-11-11 15:54:36 +0100

    remove log

--- tests/e2e/globals.js
+++ tests/e2e/globals.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   beforeEach(browser, done) {
-    browser.url(browser.launch_url).execute(function() {
+    browser.url(browser.launch_url).execute(function () {
       window.localStorage.setItem(
         `cosmos-wallets-cosmos1ek9cd8ewgxg9w5xllq9um0uf4aaxaruvcw4v9e`,
         JSON.stringify({
@@ -57,7 +57,7 @@ module.exports = {
    *
    * @param results
    */
-  reporter: function(results) {
+  reporter: function (results) {
     if (
       (typeof results.failed === `undefined` || results.failed === 0) &&
       (typeof results.error === `undefined` || results.error === 0)
@@ -85,7 +85,6 @@ async function apiUp() {
       if (response.data.errors) {
         throw new Error(JSON.stringify(response.data.errors))
       }
-      console.log("OVERVIEW", response.data.data.overview)
       if (response.data.data.overview.totalStake !== "1000") {
         continue
       }
