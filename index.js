2c1201d1b9f04eaed2826b6a84aaf64b4d7c47e0
Col <colw@users.noreply.github.com>
2019-11-20 18:04:35 +0100

    colw/Transactions allowed only from Lunie.io (#109)
    * Only allow requests from Lunie
    
    Plus localhost when in development mode
    
    * Remove console log
    
    Co-Authored-By: Fabian <frznhope@gmail.com>
    
    * Remove marketing site domain.
    
    Co-Authored-By: Fabian <frznhope@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require('http')
+const cors = require('cors')
 const { createApolloServer } = require('./lib/apollo')
 const transaction = require('./lib/routes/transaction')
 
@@ -14,7 +15,24 @@ const app = express()
 const httpServer = http.createServer(app)
 const apolloServer = new createApolloServer()
 
-app.use(config.transactionPath, transaction)
+var allowed = ['https://app.lunie.io']
+
+if (config.env === 'development') {
+  allowed.push('http://localhost:9080')
+}
+
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (allowed.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(express.json())
+app.use(config.transactionPath, cors(corsOptions), transaction)
 app.use(apolloServer.getMiddleware({ app, path: config.queryPath }))
 
 apolloServer.installSubscriptionHandlers(httpServer)
