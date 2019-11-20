5abc3ac7ff9ec710b12e40242b0771a1a2b8ac30
Col <colw@users.noreply.github.com>
2019-11-20 13:17:23 +0100

    colw/Separate endpoints (#103)
    * Convert to express server with 3 endpoints
    
    - /transaction (transaction API)
    - /query (graphql API)
    - /graphql (graphql subscription API)
    
    * Factor functions to separate modules
    
    - Apollo
    - Transaction
    
    * Lint
    
    * Log transaction endpoint message
    
    Co-Authored-By: Fabian <frznhope@gmail.com>
    
    * Configure endpoints in config file
    
    * Refactor transaction route to use Router object
    
    * Lint
    
    * Backward compatible paths

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const { ApolloServer } = require('apollo-server')
-const responseCachePlugin = require('apollo-server-plugin-response-cache')
-const { mapValues } = require('lodash')
-const typeDefs = require('./lib/schema')
-const resolvers = require('./lib/resolvers')
-const CosmosNodeSubscription = require('./lib/cosmos-node-subscription')
-const CosmosV0API = require('./lib/cosmosV0-source')
-const CosmosV2API = require('./lib/cosmosV2-source')
-const LunieDBAPI = require('./lib/luniedb-source')
-const BlockStore = require('./lib/block-store')
+const express = require('express')
+const http = require('http')
+const { createApolloServer } = require('./lib/apollo')
+const transaction = require('./lib/routes/transaction')
 
-const { networks } = require('./data/network-configs')
 const config = require('./config')
 
 if (config.SENTRY_DSN) {
@@ -17,68 +10,19 @@ if (config.SENTRY_DSN) {
   Sentry.init({ dsn: config.SENTRY_DSN })
 }
 
-const store = mapValues(networks, network => new BlockStore(network.id))
+const app = express()
+const httpServer = http.createServer(app)
+const apolloServer = new createApolloServer()
 
-new CosmosNodeSubscription(
-  networks['cosmos-hub-mainnet'],
-  CosmosV0API,
-  store['cosmos-hub-mainnet']
-)
-new CosmosNodeSubscription(
-  networks['cosmos-hub-testnet'],
-  CosmosV2API,
-  store['cosmos-hub-testnet']
-)
+app.use(config.transactionPath, transaction)
+app.use(apolloServer.getMiddleware({ app, path: config.queryPath }))
 
-if (config.enableTestnet) {
-  new CosmosNodeSubscription(
-    networks['local-cosmos-hub-testnet'],
-    CosmosV0API,
-    store['local-cosmos-hub-testnet']
+apolloServer.installSubscriptionHandlers(httpServer)
+
+httpServer.listen({ port: config.port }, () => {
+  console.log(`GraphQL Queries ready at ${apolloServer.graphqlPath}`)
+  console.log(
+    `GraphQL Subscriptions ready at ${apolloServer.subscriptionsPath}`
   )
-}
-
-function createDataSources() {
-  const dataSources = {
-    CosmosHubMainnetAPI: new CosmosV0API(networks['cosmos-hub-mainnet']),
-    CosmosHubTestnetAPI: new CosmosV2API(networks['cosmos-hub-testnet']),
-    LunieDBAPI: new LunieDBAPI(),
-    store: store
-  }
-  if (config.enableTestnet) {
-    dataSources.TestnetAPI = new CosmosV0API(
-      networks['local-cosmos-hub-testnet']
-    )
-  }
-  return dataSources
-}
-
-let options = {
-  typeDefs,
-  resolvers,
-  dataSources: createDataSources,
-  cacheControl: {
-    defaultMaxAge: 10
-  },
-  introspection: true,
-  playground: true,
-  engine: {
-    apiKey: config.apollo_engine_api_key,
-    schemaTag: 'production'
-  },
-  plugins: [responseCachePlugin()]
-}
-
-if (config.enable_cache) {
-  options.cache = require('./lib/redis').createCache()
-}
-
-const server = new ApolloServer(options)
-
-server
-  .listen({ port: config.port })
-  .then(({ url, subscriptionsUrl }) => {
-    console.log(`Server ready at ${url}`)
-    console.log(`Subscriptions ready at ${subscriptionsUrl}`)
-  })
-  .catch(console.error)
+  console.log(`Transaction service ready at ${config.transactionPath}`)
+})
