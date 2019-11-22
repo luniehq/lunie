45b23bf413f56885e4f70b3b30ad280ded498635
Col <colw@users.noreply.github.com>
2019-11-22 17:04:19 +0100

    Allow all for development/e2e environments (#119)

--- cors-rules.js
+++ cors-rules.js
@@ -2,13 +2,14 @@ const cors = require('cors')
 const config = require('./config')
 
 var allowed = ['https://app.lunie.io']
-if (config.env === 'development') {
-  allowed.push('http://localhost:9080')
-}
 
 var corsOptions = {
   origin: function(origin, callback) {
-    if (allowed.indexOf(origin) !== -1 || !origin) {
+    if (
+      config.env === 'development' ||
+      allowed.indexOf(origin) !== -1 ||
+      !origin
+    ) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
@@ -16,6 +17,6 @@ var corsOptions = {
   }
 }
 
-module.exports = function () {
+module.exports = function() {
   return cors(corsOptions)
 }
