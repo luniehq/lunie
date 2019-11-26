15a4264058a509fa21057a87f017b2fecfbf7359
mariopino <info@quequiereshacer.es>
2019-11-26 12:30:54 +0100

    Add transaction metadata icons

--- src/components/transactions/TransactionMetadata.vue
+++ src/components/transactions/TransactionMetadata.vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
-    <div class="tx__metadata">
-      Block
-      <router-link :to="{ name: `block`, params: { height } }"
-        >#{{ height | prettyInt }}&nbsp;</router-link
-      >@&nbsp;{{ date }}
+  <div class="tx__metadata">
+    <div>
+      <i class="material-icons">widgets</i> Block
+      <router-link :to="{ name: `block`, params: { height } }">
+        #{{ height | prettyInt }}
+      </router-link>
+      <i class="material-icons">access_time</i> {{ date }}
     </div>
     <p v-if="memo">Memo: {{ memo }}</p>
     <div>
-      Network Fee:&nbsp;
+      <i class="material-icons">call_split</i>
+      Fee:
       <b>{{ fee.amount }}</b>
       <span> {{ fee.denom }}</span>
     </div>
-    <p class="hash">Hash: {{ hash }}</p>
+    <p class="hash">
+      <i class="material-icons">fingerprint</i>
+      Hash: {{ hash }}
+    </p>
   </div>
 </template>
 
@@ -62,6 +67,12 @@ export default {
 <style scoped>
 .tx__metadata {
   margin-top: 0.5rem;
+}
+
+.material-icons {
+  font-size: 1rem;
+  padding-bottom: 1px;
+  vertical-align: text-bottom;
 }
 
 .hash {
