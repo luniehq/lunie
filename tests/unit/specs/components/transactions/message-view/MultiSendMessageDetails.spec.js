83154ad4581367055684c79f23a942665da0ee95
mariopino <info@quequiereshacer.es>
2019-11-28 19:53:07 +0100

    Remove unused tx in test

--- tests/unit/specs/components/transactions/message-view/MultiSendMessageDetails.spec.js
+++ tests/unit/specs/components/transactions/message-view/MultiSendMessageDetails.spec.js
@@ -52,54 +52,6 @@ describe(`MultiSendMessageDetails`, () => {
     type: `cosmos-sdk/MsgMultiSend`
   }
 
-  const tx2 = {
-    height: 2000000,
-    value: {
-      inputs: [
-        {
-          address: `cosmos1ahtlr29s38w23xxq7slcwmmz4c8x9efmr8qmee`,
-          coins: [
-            {
-              amount: "20000000000",
-              denom: "uatom"
-            }
-          ]
-        }
-      ],
-      outputs: [
-        {
-          address: `cosmos14kn0kk33szpwus9nh8n87fjel8djx0y0mmswhp`,
-          coins: [
-            {
-              amount: "10000000000",
-              denom: "uatom"
-            }
-          ]
-        },
-        {
-          address: `cosmos1clpqr4nrk4khgkxj78fcwwh6dl3uw4ep4tgu9q`,
-          coins: [
-            {
-              amount: "10000000000",
-              denom: "uatom"
-            }
-          ]
-        }
-      ]
-    },
-    fee: {
-      amount: "37",
-      denom: "uatom"
-    },
-    hash: `ABCD1234`,
-    group: `banking`,
-    key: "keyhash",
-    memo: "(Sent via Lunie)",
-    timestamp: 123456789,
-    liquidDate: null,
-    type: `cosmos-sdk/MsgMultiSend`
-  }
-
   it(`renders a sent transaction message`, () => {
     wrapper = shallowMount(MultiSendMessageDetails, {
       propsData: {
