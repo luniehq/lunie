be298654c9b0ec06c6d939f3cbce9cfe652341f7
Ana G <40721795+Bitcoinera@users.noreply.github.com>
2019-11-28 12:39:39 +0100

    Ana/add no proposal history (#3198)
    * fix pageproposals
    
    * changelog
    
    * update snapshot
    
    * add tmpage
    
    * last fixes
    
    * never say last fix
    
    * update pageproposals snapshot
    
    * change else-if to else

--- src/components/governance/PageProposals.vue
+++ src/components/governance/PageProposals.vue
@@ -1,56 +1,45 @@
 <template>
-  <PageContainer
+  <TmPage
     data-title="Proposals"
-    :managed="true"
-    :loading="
-      $apollo.queries.proposals.loading && $apollo.queries.parameters.loading
-    "
-    :error="$apollo.queries.proposals.error || $apollo.queries.parameters.error"
-    hide-header
-  >
-    <template slot="no-data">
-      <!-- duplicated, I have no proper way of refactoring this -->
-      <div class="button-container">
-        <TmBtn
-          id="propose-btn"
-          value="Create Proposal"
-          type="secondary"
-          @click.native="onPropose"
-        />
-      </div>
-      <TmDataMsg
-        title="No Governance Proposals"
-        subtitle="There are currently no governance proposals to display.
-    Click the 'Create Proposal' button to submit a proposal."
-        icon="gavel"
+    :managed="false">
+    <div class="button-container">
+      <TmBtn
+        id="propose-btn"
+        value="Create Proposal"
+        type="secondary"
+        @click.native="onPropose"
       />
-    </template>
-    <template slot="managed-body">
-      <!-- duplicated, I have no proper way of refactoring this -->
-      <div class="button-container">
-        <TmBtn
-          id="propose-btn"
-          value="Create Proposal"
-          type="secondary"
-          @click.native="onPropose"
-        />
+    </div>
+    <ModalPropose
+      ref="modalPropose"
+      :denom="parameters.depositDenom"
+      @success="() => afterPropose()"
+    />
+    <div v-if="!$apollo.loading && proposals.length === 0">     
+      <div>
+        <TmDataMsg icon="gavel">
+          <div slot="title">
+            No Governance Proposals
+          </div>
+          <div slot="subtitle">
+            There are currently no governance proposals to display.
+            Click the 'Create Proposal' button to submit the first network proposal!
+          </div>
+        </TmDataMsg>
       </div>
+    </div>
+    <div v-else>
       <TmDataLoading v-if="$apollo.loading" />
       <TableProposals v-else :proposals="proposals" />
-      <ModalPropose
-        ref="modalPropose"
-        :denom="parameters.depositDenom"
-        @success="() => afterPropose()"
-      />
-    </template>
-  </PageContainer>
+    </div>
+  </TmPage>
 </template>
 
 <script>
 import ModalPropose from "src/ActionModal/components/ModalPropose"
 import TableProposals from "governance/TableProposals"
+import TmPage from "common/TmPage"
 import TmBtn from "common/TmBtn"
-import PageContainer from "common/PageContainer"
 import TmDataMsg from "common/TmDataMsg"
 import TmDataLoading from "common/TmDataLoading"
 import { mapGetters } from "vuex"
@@ -65,7 +54,7 @@ export default {
     TmDataMsg,
     TmDataLoading,
     TmBtn,
-    PageContainer
+    TmPage
   },
   data: () => ({
     proposals: [],
