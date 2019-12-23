2f6e6cab61518a78da10dfaebe3becee6f8fe26b
Bitcoinera <anagjordano@gmail.com>
2019-12-23 16:17:36 +0100

    lint

--- src/components/staking/Undelegations.vue
+++ src/components/staking/Undelegations.vue
@@ -1,6 +1,10 @@
 <template>
   <div
-    v-if="!$apollo.queries.undelegations.loading && undelegations && undelegations.length > 0"
+    v-if="
+      !$apollo.queries.undelegations.loading &&
+        undelegations &&
+        undelegations.length > 0
+    "
   >
     <h3 class="tab-header">
       Pending Undelegations
