ddd5cbb97f20c399c45b0109216f2d2c6d54194d
Jordan Bibla <jbibla@gmail.com>
2020-01-10 14:19:28 -0500

    only show the button when signed in with a ledger and not on mobile

--- src/components/common/AppMenu.vue
+++ src/components/common/AppMenu.vue
@@ -11,7 +11,7 @@
         </a>
       </div>
       <a
-        v-if="session.signedIn"
+        v-if="!session.isMobile && session.sessionType === 'ledger'"
         class="show-on-ledger"
         @click="showAddressOnLedger()"
       >
