525d391f31cdceb418c551598bcf22fe5651de4e
FaboWeb <frznhope@gmail.com>
2020-01-25 17:12:24 -0400

    comments

--- src/components/common/TmSelectNetwork.vue
+++ src/components/common/TmSelectNetwork.vue
@@ -47,12 +47,16 @@ export default {
       // sorts networks setting showing the current network first, mainnets at the top and the default one the first
       if (this.networks.length > 0) {
         return [
+          // current network first
           this.networks.find(({ id }) => id === this.networkId),
           ...this.networks
+            // ignore the current network in the rest of the list as already showing on the top
             .filter(({ id }) => id !== this.networkId)
+            // show all mainnets next
             .sort((a, b) => {
               return a.testnet - b.testnet
             })
+            // show the default network on the top
             .sort((a, b) => {
               return b.default - a.default
             })
