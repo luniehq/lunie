06894700c6d06823e6a8f81e70fc56807e40114e
iambeone <iambeone@gmail.com>
2020-01-29 10:29:41 +0500

    network selection fixes

--- tests/e2e/globals.js
+++ tests/e2e/globals.js
@@ -80,9 +80,18 @@ async function createNewAccount(browser, networkData) {
       await browser.waitForElementVisible(
         `.select-network-item[data-network=${networkData.network}]`,
         10000,
-        true
+        true,
+        () => {
+          browser.execute(
+            function(network) {
+              document
+                .querySelector(`.select-network-item[data-network=${network}]`)
+                .click()
+            },
+            [networkData.network]
+          )
+        }
       )
-      browser.click(`.select-network-item[data-network=${networkData.network}]`)
       await browser.waitForElementVisible("#sign-up-name", 10000, true)
       browser.setValue("#sign-up-name", "demo-account")
       await next(browser)
