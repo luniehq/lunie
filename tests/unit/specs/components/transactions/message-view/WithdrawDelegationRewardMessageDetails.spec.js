2492b54c7251407dd2974a79f9d48adcb2401458
Bitcoinera <anagjordano@gmail.com>
2020-01-30 10:29:54 +0100

    add tests

--- tests/unit/specs/components/transactions/message-view/WithdrawDelegationRewardMessageDetails.spec.js
+++ tests/unit/specs/components/transactions/message-view/WithdrawDelegationRewardMessageDetails.spec.js
@@ -35,6 +35,12 @@ describe(`WithdrawalDelegationRewardMessageDetails`, () => {
       operatorAddress: `"cosmos1"`,
       picture: "",
       __typename: `Validator`
+    },
+    cosmosvaloper12w6tynmjzq4l8zdla3v4x0jt8lt4rcz5gk7zg2: {
+      name: `GiveMeYourMoney`,
+      operatorAddress: `cosmosvaloper12w6tynmjzq4l8zdla3v4x0jt8lt4rcz5gk7zg2`,
+      picture: "",
+      __typename: `Validator`
     }
   }
 
@@ -58,5 +64,68 @@ describe(`WithdrawalDelegationRewardMessageDetails`, () => {
       stubs: [`router-link`]
     })
     expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it(`if the transaction value is an array with more than one element, it returns an array of validators
+  of more than one element too`, () => {
+    const multiTx = {
+      ...tx,
+      value: [
+        {
+          value: {
+            delegator_address: "cosmos15r4tc0m6hc7z8drq3dzlrtcs6rq2q9l2kc6z4s",
+            validator_address:
+              "cosmosvaloper15r4tc0m6hc7z8drq3dzlrtcs6rq2q9l2nvwher"
+          }
+        },
+        {
+          value: {
+            delegator_address: "cosmos15r4tc0m6hc7z8drq3dzlrtcs6rq2q9l2kc6z4s",
+            validator_address:
+              "cosmosvaloper12w6tynmjzq4l8zdla3v4x0jt8lt4rcz5gk7zg2"
+          }
+        }
+      ]
+    }
+    wrapper = shallowMount(WithdrawDelegationRewardMessageDetails, {
+      propsData: {
+        transaction: multiTx,
+        validators: validators
+      },
+      stubs: [`router-link`]
+    })
+    expect(wrapper.vm.getValidators).toEqual([
+      {
+        name: "SuperValidator",
+        operatorAddress: "cosmosvaloper15r4tc0m6hc7z8drq3dzlrtcs6rq2q9l2nvwher",
+        picture: "",
+        __typename: "Validator"
+      },
+      {
+        name: "GiveMeYourMoney",
+        operatorAddress: "cosmosvaloper12w6tynmjzq4l8zdla3v4x0jt8lt4rcz5gk7zg2",
+        picture: "",
+        __typename: "Validator"
+      }
+    ])
+  })
+
+  it(`if the transaction value is an array with just one element, it returns an array with just
+  one validator`, () => {
+    wrapper = shallowMount(WithdrawDelegationRewardMessageDetails, {
+      propsData: {
+        transaction: tx,
+        validators: validators
+      },
+      stubs: [`router-link`]
+    })
+    expect(wrapper.vm.getValidators).toEqual([
+      {
+        __typename: "Validator",
+        name: "SuperValidator",
+        operatorAddress: "cosmosvaloper15r4tc0m6hc7z8drq3dzlrtcs6rq2q9l2nvwher",
+        picture: ""
+      }
+    ])
   })
 })
