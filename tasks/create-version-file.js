0072ff8ae4a400f19f3fa46e82ae86022794e354
Fabian <frznhope@gmail.com>
2020-02-03 11:54:31 -0500

    Fabo/force reload (#3518)
    * force a refresh on a new version
    
    * cleanup
    
    * comments
    
    * changelog
    
    * fix wrong file name
    
    * fix missings tags for process.env
    
    Co-authored-by: Ana G. <40721795+Bitcoinera@users.noreply.github.com>

--- tasks/create-version-file.js
+++ tasks/create-version-file.js
@@ -1,8 +1,12 @@
 const fs = require("fs")
 
+/*
+ * Creates a file that holds the current version on the server so the app can check at start if it is outdated.
+ */
+
 function main() {
   const { version } = JSON.parse(fs.readFileSync("./package.json"))
-  fs.writeFileSync("./dist/version.json", `{"version": "${version}"}`, "utf8")
+  fs.writeFileSync("./public/version.json", `{"version": "${version}"}`, "utf8")
 }
 
 main()
