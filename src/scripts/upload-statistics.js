f99b15bb1c07ff7f9160a7640b4c181143f6d14e
Aleksei Rudometov <iambeone@gmail.com>
2020-02-25 16:43:05 +0500

    browser-id replaced to fingerprintjs2

--- src/scripts/upload-statistics.js
+++ src/scripts/upload-statistics.js
@@ -1,8 +1,18 @@
-import browserID from "browser-id"
+import * as Fingerprint2 from "fingerprintjs2"
 import { getGraphqlHost } from "scripts/url"
 
-export const uploadStatistics = (customObject, ...args) => {
+export const uploadStatistics = async (customObject, ...args) => {
+  customObject = Object.assign({}, customObject) // needed to prevent further modifications
   const graphqlHost = getGraphqlHost()
+  var fp_options = {
+    excludes: {
+      touchSupport: true
+    }
+  }
+  let components = await Fingerprint2.getPromise(fp_options)
+  var values = components.map(component => component.value)
+  var murmur = Fingerprint2.x64hash128(values.join(``), 31)
+
   const options = {
     method: "POST",
     headers: {
@@ -16,7 +26,7 @@ export const uploadStatistics = (customObject, ...args) => {
           action: args[1],
           label: args[2],
           value: args[3],
-          browserid: browserID()
+          browserid: murmur
         }
       }
     })
