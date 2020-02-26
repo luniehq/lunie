224d0bdbfed18dde46799e96ebe0c4dab0d9bb80
Алексей Рудометов <iambeone@gmail.com>
2020-02-26 20:13:13 +0500

    add math.trunc

--- src/scripts/google-analytics.js
+++ src/scripts/google-analytics.js
@@ -61,7 +61,7 @@ module.exports.sendEvent = function event(customObject, ...args) {
       delete customObject[key]
     })
     if (args[3]) {
-      args[3] *= 1000
+      args[3] = Math.trunc(args[3] * 1000)
     }
     window.ga(`send`, `event`, ...args, customObject)
   }
