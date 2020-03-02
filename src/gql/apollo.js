df8e049518a7506d8b96f2670b85faec4fd9e213
Fabian <frznhope@gmail.com>
2020-03-01 19:51:55 -0500

    Revert "Fabo/revert analystics header (#3623)"
    This reverts commit f79e4ec857ff2885626f83e9686fc2cd93db9fd1.

--- src/gql/apollo.js
+++ src/gql/apollo.js
@@ -15,8 +15,8 @@ import VueApollo from "vue-apollo"
 import { getGraphqlHost } from "scripts/url"
 import * as Sentry from "@sentry/browser"
 import config from "src/../config"
-import * as Fingerprint2 from "fingerprintjs2"
 import introspectionQueryResultData from "src/../fragmentTypes.json"
+import { getFingerprint } from "scripts/fingerprint"
 
 Vue.use(VueApollo)
 
@@ -42,18 +42,8 @@ const makeWebSocketLink = () => {
   return new WebSocketLink({ uri })
 }
 
-const createApolloClient = () => {
-  var fp_options = {
-    excludes: {
-      touchSupport: true
-    }
-  }
-  const fingerprint = (async () => {
-    const components = await Fingerprint2.getPromise(fp_options)
-    const values = components.map(component => component.value)
-    const fingerprint = Fingerprint2.x64hash128(values.join(``), 31)
-    return fingerprint
-  })()
+const createApolloClient = async () => {
+  const fingerprint = await getFingerprint()
   const middleware = new ApolloLink((operation, forward) => {
     // add the authorization to the headers
     operation.setContext({
@@ -126,9 +116,9 @@ const createApolloClient = () => {
   })
 }
 
-export const createApolloProvider = () => {
+export const createApolloProvider = async () => {
   return new VueApollo({
-    defaultClient: createApolloClient(),
+    defaultClient: await createApolloClient(),
     defaultOptions: {
       // apollo options applied to all queries in components
       $query: {
