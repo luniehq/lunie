653e8b9f9595508a8f28ba48a6e3b961e089ed06
Fabian <frznhope@gmail.com>
2020-03-01 23:12:13 -0500

    Bring back the fingerprint header (#3630)
    * Revert "Fabo/revert analystics header (#3623)"
    
    This reverts commit f79e4ec857ff2885626f83e9686fc2cd93db9fd1.
    
    * fix time test
    
    * changelog

--- src/ActionModal/utils/ActionManager.js
+++ src/ActionModal/utils/ActionManager.js
@@ -4,6 +4,7 @@ import transaction from "./transactionTypes"
 import { uatoms } from "scripts/num"
 import { toMicroDenom } from "src/scripts/common"
 import { getGraphqlHost } from "scripts/url"
+import { getFingerprint } from "scripts/fingerprint"
 import {
   getMessage,
   getMultiMessage,
@@ -11,12 +12,13 @@ import {
   transformMessage
 } from "./MessageConstructor.js"
 
-const txFetchOptions = {
+const txFetchOptions = fingerprint => ({
   method: "POST",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    fingerprint
   }
-}
+})
 
 export default class ActionManager {
   constructor() {
@@ -70,11 +72,11 @@ export default class ActionManager {
   }
 
   async transactionAPIRequest(payload) {
+    const fingerprint = await getFingerprint()
     const options = {
-      ...txFetchOptions,
+      ...txFetchOptions(fingerprint),
       body: JSON.stringify({ payload })
     }
-
     const command = payload.simulate ? "estimate" : "broadcast"
 
     const graphqlHost = getGraphqlHost()
