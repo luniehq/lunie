65316e39e93b481c5e402746168ced118372516b
show <anagjordano@gmail.com>
2020-03-02 15:42:11 +0100

    improve powered by styles in connected network

--- src/components/common/TmConnectedNetwork.vue
+++ src/components/common/TmConnectedNetwork.vue
@@ -271,8 +271,8 @@ export default {
   display: block;
   height: 1px;
   border: 0;
-  border-top: 1px solid var(--dark-grey-blue);
-  width: 90%;
+  border-top: 1px solid #252d4a;
+  width: 100%;
   margin-top: 1.25rem;
   margin-bottom: 0.75rem;
 }
@@ -280,7 +280,7 @@ export default {
 .powered-div {
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   font-size: 12px;
 }
 
