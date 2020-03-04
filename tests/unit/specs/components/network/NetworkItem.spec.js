b76fe480def22133e190641cd35705c33aea7e1b
show <anagjordano@gmail.com>
2020-03-04 12:11:18 +0100

    clean test

--- tests/unit/specs/components/network/NetworkItem.spec.js
+++ tests/unit/specs/components/network/NetworkItem.spec.js
@@ -1,11 +1,6 @@
-// import { shallowMount, createLocalVue } from "@vue/test-utils"
 import NetworkItem from "network/NetworkItem"
 
-// const localVue = createLocalVue()
-
 describe(`NetworkItem`, () => {
-  //   let wrapper, $store
-
   const networkitem = {
     id: "cosmoshub",
     powered: {
@@ -14,28 +9,6 @@ describe(`NetworkItem`, () => {
       picture: ""
     }
   }
-
-  beforeEach(() => {
-    // $store = {
-    //   getters: {
-    //     network: `cosmoshub`,
-    //     connected: ""
-    //   }
-    // }
-    // wrapper = shallowMount(NetworkItem, {
-    //   localVue,
-    //   propsData: {
-    //     networkitem,
-    //     enabled: true
-    //   },
-    //   mocks: {
-    //     $store,
-    //     $router: {
-    //       push: jest.fn()
-    //     }
-    //   }
-    // })
-  })
 
   it(`returns true if the network in NetworkItem is the same than the network we are connected to`, () => {
     const self = {
