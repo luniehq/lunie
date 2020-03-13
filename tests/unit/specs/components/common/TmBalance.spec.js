3390bd76c3670e02111ac2463b8256c8d25c0019
Jordan Bibla <jbibla@gmail.com>
2020-03-13 12:38:35 -0400

    fixed tests

--- tests/unit/specs/components/common/TmBalance.spec.js
+++ tests/unit/specs/components/common/TmBalance.spec.js
@@ -1,4 +1,6 @@
-import { shallowMount } from "@vue/test-utils"
+import {
+  shallowMount
+} from "@vue/test-utils"
 import TmBalance from "common/TmBalance"
 
 describe(`TmBalance`, () => {
@@ -41,8 +43,7 @@ describe(`TmBalance`, () => {
         totalStake: 3210,
         liquidStake: 1230,
         totalRewards: 1000.45,
-        rewards: [
-          {
+        rewards: [{
             amount: 1,
             denom: `TOKEN1`
           },
@@ -61,12 +62,8 @@ describe(`TmBalance`, () => {
 
   it(`show the balance header when signed in`, () => {
     expect(wrapper.element).toMatchSnapshot()
-    expect(wrapper.text()).toMatch(/Total ATOM.*3,210/s)
-    expect(wrapper.text()).toMatch(/Available ATOM.*1,230/s)
-    expect(wrapper.text()).toMatch(/Total Rewards.*1,000.45/s)
-    expect(wrapper.text()).toContain("Total ATOM")
-    expect(wrapper.text()).toContain("Total ATOM")
-    expect(wrapper.text()).toContain("Total ATOM")
+    expect(wrapper.text()).toMatch(/ATOM.*3,210/s)
+    expect(wrapper.text()).toMatch(/Available.*1,230/s)
   })
 
   it(`do not show available atoms when the user has none in the first place`, () => {
@@ -80,20 +77,28 @@ describe(`TmBalance`, () => {
       }
     })
     expect(wrapper.element).toMatchSnapshot()
-    expect(wrapper.text()).toContain("Total ATOM")
+    expect(wrapper.text()).toContain("Token")
     expect(wrapper.text()).not.toContain("Available ATOM")
     expect(wrapper.text()).not.toContain("Total Rewards")
   })
 
   it(`opens send modal`, () => {
-    const $refs = { SendModal: { open: jest.fn() } }
+    const $refs = {
+      SendModal: {
+        open: jest.fn()
+      }
+    }
     wrapper.vm.$refs = $refs
     wrapper.find(".send-button").trigger("click")
     expect($refs.SendModal.open).toHaveBeenCalled()
   })
 
   it(`opens claim rewards modal`, () => {
-    const $refs = { ModalWithdrawRewards: { open: jest.fn() } }
+    const $refs = {
+      ModalWithdrawRewards: {
+        open: jest.fn()
+      }
+    }
     wrapper.vm.$refs = $refs
     wrapper.find("#withdraw-btn").trigger("click")
     expect($refs.ModalWithdrawRewards.open).toHaveBeenCalled()
@@ -106,7 +111,11 @@ describe(`TmBalance`, () => {
         rewards: []
       }
     })
-    const $refs = { ModalWithdrawRewards: { open: jest.fn() } }
+    const $refs = {
+      ModalWithdrawRewards: {
+        open: jest.fn()
+      }
+    }
     wrapper.vm.$refs = $refs
     wrapper.find("#withdraw-btn").trigger("click")
     expect($refs.ModalWithdrawRewards.open).not.toHaveBeenCalled()
@@ -121,8 +130,7 @@ describe(`TmBalance`, () => {
 
   it(`should return true if rewards contain multiple denoms`, () => {
     wrapper.setData({
-      balances: [
-        {
+      balances: [{
           amount: 1,
           denom: "ATOM",
           fiatValue: `33€`
