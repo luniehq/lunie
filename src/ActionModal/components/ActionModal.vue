79e465ef2a820c143d93ba6f4b1351bddb8c2659
Aleksey Rudometov <iambeone@gmail.com>
2020-03-25 16:37:06 +0500

    Aleksei/action modal block number fixed (#3757)
    * changelog
    
    * fixing blockHeight update after several staking
    
    * includedHeight added to data
    
    * null to undefined changed
    
    * filter applied

--- src/ActionModal/components/ActionModal.vue
+++ src/ActionModal/components/ActionModal.vue
@@ -206,7 +206,7 @@
                 :to="
                   `/${$router.history.current.params.networkId}/blocks/${includedHeight}`
                 "
-                >#{{ prettyIncludedHeight }}</router-link
+                >#{{ includedHeight | prettyInt }}</router-link
               >
             </div>
           </TmDataMsg>
@@ -355,7 +355,8 @@ export default {
     FeatureNotAvailable
   },
   filters: {
-    viewDenom
+    viewDenom,
+    prettyInt
   },
   props: {
     title: {
@@ -425,7 +426,8 @@ export default {
     overview: {},
     isMobileApp: config.mobileApp,
     balances: [],
-    queueEmpty: true
+    queueEmpty: true,
+    includedHeight: undefined
   }),
   computed: {
     ...mapState([`extension`, `session`]),
@@ -497,9 +499,6 @@ export default {
         default:
           return "Sending..."
       }
-    },
-    prettyIncludedHeight() {
-      return prettyInt(this.includedHeight)
     },
     getDenom() {
       return this.selectedDenom || this.network.stakingDenom
