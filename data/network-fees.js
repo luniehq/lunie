ab3ead1b4e9c2544219506a2873d7332f3f128a0
Ana G <40721795+Bitcoinera@users.noreply.github.com>
2020-03-27 15:57:31 +0100

    Ana/add gas estimates query (#503)
    * add network fees query
    
    * fix schema
    
    * refactor using networkid with hyphens
    
    * return only estimate for a transaction type
    
    * add gas estimates to network object
    
    * remove network fees from queries
    
    * revert last two commits manually
    
    * add default to network gas estimates
    
    * apply suggestion

---
+++ data/network-fees.js
@@ -0,0 +1,39 @@
+const { UserInputError } = require('apollo-server')
+
+const transactionTypesSet = new Set(['SendTx', 'StakeTx', 'UnstakeTx', 'RestakeTx', 'ClaimRewardsTx', 'SubmitProposalTx', 'VoteTx', 'DepositTx', 'UnknownTx'])
+const transactionTypes = [...transactionTypesSet]
+
+const getNetworkTransactionGasEstimates = (networkId, transactionType) => {
+    if (transactionType && !transactionTypesSet.has(transactionType)) {
+      throw new UserInputError(`Unrecognized transaction type. Valid transaction types are 'SendTx', 'StakeTx', 'UnstakeTx', 'RestakeTx', 'ClaimRewardsTx', 'SubmitProposalTx', 'VoteTx', 'DepositTx' and 'UnknownTx'`)
+    }
+    const networkGasEstimates = networkGasEstimatesDictionary[networkId]
+    if (!networkGasEstimates) {
+        throw new UserInputError(`Unrecognized network. Currently only Cosmos, Terra and e-Money do have a network fees set`)
+    }
+    return transactionType ? networkGasEstimates[`${transactionType}`] : networkGasEstimates.default
+}
+
+const terraGasEstimates = {
+    default: 200000
+}
+
+const cosmosGasEstimates = {
+    default: 550000
+}
+
+const emoneyGasEstimates = {
+    default: 200000,
+    ClaimRewardsTx: 550000
+}
+
+const networkGasEstimatesDictionary = {
+    "cosmos-hub-mainnet": cosmosGasEstimates,
+    "cosmos-hub-testnet": cosmosGasEstimates,
+    "terra-mainnet": terraGasEstimates,
+    "terra-testnet": terraGasEstimates,
+    "emoney-mainnet": emoneyGasEstimates,
+    "emoney-testnet": emoneyGasEstimates
+}
+
+module.exports = {getNetworkTransactionGasEstimates}
