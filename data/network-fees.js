e389106fee8286eb17c81c4af5fa96fb1252e0e0
Fabian <frznhope@gmail.com>
2020-04-02 11:27:10 -0500

    Fabo/add polkadot fees (#557)
    * add polkadot fees
    
    * refactored

--- data/network-fees.js
+++ data/network-fees.js
@@ -1,16 +1,36 @@
 const { UserInputError } = require('apollo-server')
 
-const transactionTypesSet = new Set(['SendTx', 'StakeTx', 'UnstakeTx', 'RestakeTx', 'ClaimRewardsTx', 'SubmitProposalTx', 'VoteTx', 'DepositTx', 'UnknownTx'])
+const transactionTypesSet = new Set([
+  'SendTx',
+  'StakeTx',
+  'UnstakeTx',
+  'RestakeTx',
+  'ClaimRewardsTx',
+  'SubmitProposalTx',
+  'VoteTx',
+  'DepositTx',
+  'UnknownTx'
+])
 
 const getNetworkTransactionGasEstimates = (networkId, transactionType) => {
-    if (transactionType && !transactionTypesSet.has(transactionType)) {
-      throw new UserInputError(`Unrecognized transaction type. Valid transaction types are 'SendTx', 'StakeTx', 'UnstakeTx', 'RestakeTx', 'ClaimRewardsTx', 'SubmitProposalTx', 'VoteTx', 'DepositTx' and 'UnknownTx'`)
-    }
-    const networkGasEstimates = networkGasEstimatesDictionary[networkId]
-    if (!networkGasEstimates) {
-        throw new UserInputError(`Unrecognized network. Currently only Cosmos, Terra and e-Money do have a network fees set`)
-    }
-    return transactionType && networkGasEstimates[`${transactionType}`] ? networkGasEstimates[`${transactionType}`] : networkGasEstimates.default
+  if (transactionType && !transactionTypesSet.has(transactionType)) {
+    throw new UserInputError(
+      `Unrecognized transaction type. Valid transaction types are ${Array.from(
+        transactionTypesSet.keys()
+      ).join(', ')}.`
+    )
+  }
+  const networkGasEstimates = networkGasEstimatesDictionary[networkId]
+  if (!networkGasEstimates) {
+    throw new UserInputError(
+      `Unrecognized network. Currently only ${Object.keys(
+        networkGasEstimatesDictionary
+      ).join(', ')} are supported`
+    )
+  }
+  return transactionType && networkGasEstimates[`${transactionType}`]
+    ? networkGasEstimates[`${transactionType}`]
+    : networkGasEstimates.default
 }
 
 const terraGasEstimates = {
@@ -19,26 +39,31 @@ const terraGasEstimates = {
 }
 
 const cosmosGasEstimates = {
-    default: 550000
+  default: 550000
 }
 
 const emoneyGasEstimates = {
-    default: 200000,
-    ClaimRewardsTx: 550000
+  default: 200000,
+  ClaimRewardsTx: 550000
 }
 
 const akashGasEstimates = {
-    default: 200000
+  default: 200000
+}
+
+const polkadotGasEstimates = {
+  default: 0
 }
 
 const networkGasEstimatesDictionary = {
-    "cosmos-hub-mainnet": cosmosGasEstimates,
-    "cosmos-hub-testnet": cosmosGasEstimates,
-    "terra-mainnet": terraGasEstimates,
-    "terra-testnet": terraGasEstimates,
-    "emoney-mainnet": emoneyGasEstimates,
-    "emoney-testnet": emoneyGasEstimates,
-    "akash-testnet": akashGasEstimates
+  'cosmos-hub-mainnet': cosmosGasEstimates,
+  'cosmos-hub-testnet': cosmosGasEstimates,
+  'terra-mainnet': terraGasEstimates,
+  'terra-testnet': terraGasEstimates,
+  'emoney-mainnet': emoneyGasEstimates,
+  'emoney-testnet': emoneyGasEstimates,
+  'akash-testnet': akashGasEstimates,
+  'polkadot-testnet': polkadotGasEstimates
 }
 
-module.exports = {getNetworkTransactionGasEstimates}
+module.exports = { getNetworkTransactionGasEstimates }
