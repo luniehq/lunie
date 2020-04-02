e6fec85dcf69f0fc2a8bf1e11e3938aee7cbeb96
Jordan Bibla <jbibla@gmail.com>
2020-04-02 08:49:05 -0400

    card sign in tests

--- tests/unit/specs/components/common/CardSignInRequired.spec.js
+++ tests/unit/specs/components/common/CardSignInRequired.spec.js
@@ -2,10 +2,32 @@ import { shallowMount } from "@vue/test-utils"
 import CardSignInRequired from "common/CardSignInRequired"
 
 describe(`CardSignInRequired`, () => {
-  let wrapper
+  let wrapper, $store
 
   beforeEach(() => {
-    wrapper = shallowMount(CardSignInRequired)
+    const state = {
+      session: {
+        insecureMode: false,
+        browserWithLedgerSupport: null
+      },
+      extension: {
+        enabled: true
+      },
+      keystore: {
+        accounts: []
+      }
+    }
+    $store = {
+      state,
+      commit: jest.fn(),
+      dispatch: jest.fn()
+    }
+    wrapper = shallowMount(CardSignInRequired, {
+      mocks: {
+        $store
+      },
+      stubs: [`router-link`]
+    })
   })
 
   it(`shows a sign in required card`, () => {
