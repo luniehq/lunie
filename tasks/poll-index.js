45d6c32cea64a610ee5b0a0641fa600f3bfeedb9
show <anagjordano@gmail.com>
2020-04-03 23:57:40 +0200

    very important fix

--- tasks/poll-index.js
+++ tasks/poll-index.js
@@ -27,6 +27,7 @@ setInterval(() => {
             // the client has an outdated version. We need to refresh the browser to get the latest changes (and chunks)
             // when the reload method receives true as an argument, it will always reload the page from the server
             window.location.reload(true)
+            loaded = false
         }
     })
     // The check for new versions is repeated every minute
