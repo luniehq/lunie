e281bc4b5c063dbc8b913badd2f7d9f1a08a6f41
show <anagjordano@gmail.com>
2020-04-04 20:22:43 +0200

    set mode to no-cors

--- src/scripts/check-for-new-lunie-versions.js
+++ src/scripts/check-for-new-lunie-versions.js
@@ -14,7 +14,7 @@ const checkForNewLunieVersions = () => {
   let loaded = false
 
   setInterval(() => {
-    fetch("https://app.lunie.io").then(result => {
+    fetch("https://app.lunie.io", { mode: "no-cors" }).then(result => {
       console.log("Current Etag", result.headers._headers.etag[0])
       if (!loaded) {
         currentEtag = result.headers._headers.etag[0]
