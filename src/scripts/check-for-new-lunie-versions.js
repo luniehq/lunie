5f9623013cf107b463aed5f8def4a7f1fe77c26a
show <anagjordano@gmail.com>
2020-04-05 23:15:04 +0200

    why not console logging. make faster for test

--- src/scripts/check-for-new-lunie-versions.js
+++ src/scripts/check-for-new-lunie-versions.js
@@ -10,19 +10,19 @@ const checkForNewLunieVersions = () => {
   setInterval(() => {
     fetch(window.location.host).then(result => {
       console.log('Result', result)
-      console.log("Current Etag", result.headers._headers.etag[0])
-      if (!loaded) {
-        currentEtag = result.headers._headers.etag[0]
-        loaded = true
-      }
-      console.log("Client Etag", currentEtag)
-      if (currentEtag !== result.headers._headers.etag[0]) {
-        console.log("Outdated Lunie version detected! Refreshing...")
-        // the client has an outdated version. We need to refresh the browser to get the latest changes (and chunks)
-        // when the reload method receives true as an argument, it will always reload the page from the server
-        window.location.reload(true)
-        loaded = false
-      }
+      // console.log("Current Etag", result.headers._headers.etag[0])
+      // if (!loaded) {
+      //   currentEtag = result.headers._headers.etag[0]
+      //   loaded = true
+      // }
+      // console.log("Client Etag", currentEtag)
+      // if (currentEtag !== result.headers._headers.etag[0]) {
+      //   console.log("Outdated Lunie version detected! Refreshing...")
+      //   // the client has an outdated version. We need to refresh the browser to get the latest changes (and chunks)
+      //   // when the reload method receives true as an argument, it will always reload the page from the server
+      //   window.location.reload(true)
+      //   loaded = false
+      // }
     })
     // The check for new versions is repeated every minute
   }, 60000)
