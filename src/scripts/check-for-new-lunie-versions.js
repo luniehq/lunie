e5ae5d0be5e1aea0f0ce2622364a21fe7a2f5039
show <anagjordano@gmail.com>
2020-04-06 14:35:35 +0200

    improve console logs

--- src/scripts/check-for-new-lunie-versions.js
+++ src/scripts/check-for-new-lunie-versions.js
@@ -4,19 +4,22 @@
  */
 
 const checkForNewLunieVersions = () => {
-  let currentEtag = ""
+  let clientEtag = ""
   let loaded = false
 
   setInterval(() => {
     fetch(window.location.origin).then(result => {
-      console.log("Current Etag", result.headers.get("etag"))
       if (!loaded) {
-        currentEtag = result.headers.get("etag")
+        clientEtag = result.headers.get("etag")
         loaded = true
       }
-      console.log("Client Etag", currentEtag)
-      if (currentEtag !== result.headers.get("etag")) {
+      if (clientEtag !== result.headers.get("etag")) {
         console.log("Outdated Lunie version detected! Refreshing...")
+        console.log(
+          `Client Etag was ${clientEtag} and current etag is ${result.headers.get(
+            "etag"
+          )}`
+        )
         // the client has an outdated version. We need to refresh the browser to get the latest changes (and chunks)
         // when the reload method receives true as an argument, it will always reload the page from the server
         window.location.reload(true)
