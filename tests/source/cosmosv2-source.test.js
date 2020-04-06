45512bfd73cafd3027b35be90a1e12a793b982a2
Fabian <frznhope@gmail.com>
2020-04-06 13:30:04 -0500

    Fabo/rewards per era (#563)
    * initial working state
    
    * working, this time is true ;-)
    
    * fix validator, comments
    
    * fix, cleanup
    
    * fix validator
    
    * update comment
    
    * Use coinReducer
    
    * cleanup
    
    * cleanup unused vars
    
    * cleanup
    
    * Flatten era rewards and then aggregate per valid
    
    * refactored
    
    * typo
    
    * uüdated polkadot
    
    * load rewards
    
    * steps towards storing the rewards
    
    * fix era format
    
    * fix validator in row needs to be address
    
    * fix gql query
    
    * format bug
    
    * some refactoring
    
    * fix missing await
    
    * correctly get api
    
    * push validator getter inside sources
    
    * added address to lunie reward internaly
    
    * add address correctly
    
    * fix variable naming
    
    * filter empty rewards
    
    * add height from rewards
    
    * fixed tests
    
    * also fixed terra getRewards here
    
    * Update lib/block-listeners/polkadot-node-subscription.js
    
    Co-Authored-By: Fabian <frznhope@gmail.com>
    
    * remove dead code
    
    * fix stuff
    
    * fix operatorAddress of undefined
    
    * readded vanished code
    
    * don't store validatorAddress
    
    Co-authored-by: mariopino <info@quequiereshacer.es>
    Co-authored-by: Bitcoinera <anagjordano@gmail.com>
    Co-authored-by: Ana G <40721795+Bitcoinera@users.noreply.github.com>

--- tests/source/cosmosv2-source.test.js
+++ tests/source/cosmosv2-source.test.js
@@ -28,22 +28,22 @@ jest.mock('apollo-datasource-rest', () => {
 
 describe('Cosmos V2 API', function() {
   describe('getRewards()', function() {
-    let api, cosmosNetworkConfig
+    let api, cosmosNetworkConfig, store
 
     beforeEach(() => {
       cosmosNetworkConfig = networks.find(
         network => network.id === 'cosmos-hub-testnet'
       )
-      api = new CosmosV2API(cosmosNetworkConfig, {})
+      store = {
+        validators: mockValidatorsDictionary
+      }
+      api = new CosmosV2API(cosmosNetworkConfig, store)
       mockDelegatorRewards = JSON.parse(JSON.stringify(delegatorRewards))
     })
 
     it('When an existing delegator address is passed, it should return the calculated rewards', async () => {
       //Act
-      const result = await api.getRewards(
-        delegatorAddress,
-        mockValidatorsDictionary
-      )
+      const result = await api.getRewards(delegatorAddress)
 
       //Assert
       expect(result[0]).toHaveProperty('amount')
@@ -58,9 +58,7 @@ describe('Cosmos V2 API', function() {
       mockDelegatorRewards.result.rewards = []
 
       //Act & Assert
-      await expect(
-        api.getRewards(delegatorAddress, mockValidatorsDictionary)
-      ).resolves.toEqual([])
+      await expect(api.getRewards(delegatorAddress)).resolves.toEqual([])
     })
 
     it('When an existing delegator address is passed with a reward 49000000 (umuon), it should return amount 49 (muon)', async () => {
@@ -68,9 +66,7 @@ describe('Cosmos V2 API', function() {
       mockDelegatorRewards.result.rewards[0].reward[0].amount = 49000000
 
       //Act & Assert
-      await expect(
-        api.getRewards(delegatorAddress, mockValidatorsDictionary)
-      ).resolves.toEqual([
+      await expect(api.getRewards(delegatorAddress)).resolves.toEqual([
         {
           amount: '49.000000',
           denom: 'MUON',
@@ -84,9 +80,7 @@ describe('Cosmos V2 API', function() {
       mockDelegatorRewards.result.rewards[0].reward[0].amount = 0.05
 
       //Act & Assert
-      await expect(
-        api.getRewards(delegatorAddress, mockValidatorsDictionary)
-      ).resolves.toEqual([
+      await expect(api.getRewards(delegatorAddress)).resolves.toEqual([
         {
           amount: '0.000000',
           denom: 'MUON',
