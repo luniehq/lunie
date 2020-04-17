8c7bf2f6a484897f043d7ebde7a1426daf370392
michielmulders <michielmulders1@gmail.com>
2020-04-17 23:32:35 +0200

    Fix e2e tests

--- config.js
+++ config.js
@@ -21,5 +21,5 @@ export default {
   graphqlHost: graphql,
 
   e2e: process.env.VUE_APP_E2E || false,
-  firebasePublicVapidKey: process.env.VUE_APP_FIREBASE_PUBLIC_KEY || ""
+  firebasePublicVapidKey: process.env.VUE_APP_FIREBASE_PUBLIC_KEY || "BC_2HRHQW9erg_lOd-dFe_R2ISeiXi0qPNqNcL-jBDnsmMXkqnFcBpXqIklsJtkDPiBmSoOlAMDMOyZMt_Njugo"
 }
