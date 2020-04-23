5d44ac8b8905c1a1f9817acbf93ca5ad3303b769
mariopino <info@quequiereshacer.es>
2020-04-23 19:24:51 +0200

    dont set type in keyring creation

--- src/ActionModal/utils/polkadot-signing.js
+++ src/ActionModal/utils/polkadot-signing.js
@@ -14,7 +14,7 @@ function getSignature(rawSignature) {
 }
 
 export function getSignedMessage({ payload, transaction }, seed) {
-  const keyring = new Keyring({ type: "sr25519" })
+  const keyring = new Keyring()
   const keypair = keyring.createFromUri(seed, {}, "sr25519")
 
   const rawSignature = keypair.sign(hexToU8a(payload.toRaw().data))
