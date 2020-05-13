190fd7e01b51a98427dcce92f6670e5934ff7967
Michiel Mulders <michielmulders1@gmail.com>
2020-05-13 16:28:39 +0200

    Michiel/docker secret for hasura (#737)
    * changelog
    
    * Add docker secret for hasura
    
    * Add linting fixes

--- config.js
+++ config.js
@@ -2,11 +2,10 @@ require('dotenv').config()
 const secrets = require('./secrets')
 
 const env = process.env.NODE_ENV || 'development'
-console.info('\nPort read from Docker secrets', secrets.read('PORT'))
 
 module.exports = {
   env: env,
-  port: secrets.read('PORT') || process.env.PORT || '4000',
+  port: process.env.PORT || '4000',
   queryPath: '/',
   transactionPath: '/transaction',
   pushRegistrationPath: '/push',
@@ -14,7 +13,8 @@ module.exports = {
   apollo_engine_api_key: process.env.ENGINE_API_KEY || '',
   enable_cache: process.env.ENABLE_CACHE || false,
   redis_url: process.env.REDIS_URL || '',
-  hasura_admin_key: process.env.HASURA_ADMIN_KEY || '',
+  hasura_admin_key:
+    secrets.read('HASURA_ADMIN_KEY') || process.env.HASURA_ADMIN_KEY || '',
   hasura_url: process.env.HASURA_URL || 'http://localhost:8080/v1/graphql',
   enableTestnet: process.env.TESTNET === 'true',
   testnetRPC: process.env.TESTNET_RPC_URL || 'ws://localhost:26657/websocket',
