0505183a83a8f8397961dea31063156a80945067
show <anagjordano@gmail.com>
2020-05-25 20:35:12 +0200

    fix actionmodal between networks

--- src/ActionModal/components/SendModal.vue
+++ src/ActionModal/components/SendModal.vue
@@ -286,7 +286,8 @@ export default {
   },
   methods: {
     open(denom = undefined) {
-      this.selectedToken = denom || this.selectedToken
+      this.selectedToken =
+        denom || this.balances.find(({ denom }) => denom).denom
       this.$refs.actionModal.open()
     },
     onSuccess(event) {
