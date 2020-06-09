641c54d055163d6d23dacbe6404741f6fa08ccaa
show <anagjordano@gmail.com>
2020-06-09 19:48:37 +0200

    add commented initialize FirebaseApp

---
+++ app/src/firebase/app.js
@@ -0,0 +1,6 @@
+// import Firebase from "firebase/app"
+// import credentials from "./credentials"
+
+// const FirebaseApp = Firebase.initializeApp(credentials.config)
+
+// export const App = FirebaseApp
