66e5d9675317c1bbea735d7bb38bdb1716955c24
Bitcoinera <anagjordano@gmail.com>
2020-06-11 13:54:13 +0200

    remove evil magic-link-message

--- app/src/components/common/CardSignInRequired.vue
+++ app/src/components/common/CardSignInRequired.vue
@@ -14,9 +14,6 @@
         </TmFormGroup>
       </div>
       <TmBtn value="Sign In" type="secondary" @click.native="sendMagicLink()" />
-      <span id="magic-link-message"
-        >Magic link to sign in sent to your e-mail</span
-      >
     </div>
     <h2>Welcome to Lunie 👋</h2>
     <h3>How would you like to get started?</h3>
@@ -120,9 +117,6 @@ export default {
       await this.$store.dispatch(`sendUserMagicLink`, {
         user: { email: this.email },
       })
-      if (!this.user.signInError) {
-        this.$el.querySelector(`#magic-link-message`).style.display = `inline`
-      }
     },
   },
 }
@@ -155,10 +149,6 @@ h3 {
 .session-list {
   width: 100%;
   padding: 2rem 0;
-}
-
-#magic-link-message {
-  display: none;
 }
 
 @media screen and (max-width: 667px) {
