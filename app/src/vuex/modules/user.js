ce5d626324b3d904e9703bc78a5a216ebad4641c
show <anagjordano@gmail.com>
2020-06-12 20:02:40 +0200

    fix the first error with file extension

--- app/src/vuex/modules/user.js
+++ app/src/vuex/modules/user.js
@@ -1,5 +1,5 @@
 import config from "src/../config"
-import firebase from "../../firebase"
+import firebase from "../../firebase.js"
 import * as Sentry from "@sentry/browser"
 
 const Auth = firebase.auth()
