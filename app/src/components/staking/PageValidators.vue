4e24562848544844c8511155f7f9f719c5876b10
Bitcoinera <anagjordano@gmail.com>
2020-06-22 19:29:47 +0200

    linting

--- app/src/components/staking/PageValidators.vue
+++ app/src/components/staking/PageValidators.vue
@@ -225,11 +225,8 @@ export default {
 
   .btn-radio {
     min-width: 100px;
+    border-radius: 0;
   }
-}
-
-.filterContainer .btn-radio {
-  border-radius: 0;
 }
 
 .filterContainer .btn-radio:last-child {
@@ -244,6 +241,7 @@ export default {
 
 .show-mobile-sorting {
   cursor: pointer;
+  display: none;
 }
 
 .show-mobile-sorting.active {
@@ -268,10 +266,6 @@ export default {
 
 .filter-toggle {
   margin-left: 1em;
-}
-
-.show-mobile-sorting {
-  display: none;
 }
 
 .no-results {
