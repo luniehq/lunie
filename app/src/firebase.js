36e07e604acba3d5498be89bf54f599f9f433ca5
show <anagjordano@gmail.com>
2020-06-26 13:09:34 +0200

    import sentry

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,24 +1,24 @@
 import config from "../config"
+import * as Sentry from "@sentry/browser"
 
 // load firebase async
 let firebaseInstance
 const getFirebase = async () => {
-    if (firebaseInstance) {
-        return firebaseInstance
-    }
-try {
+  if (firebaseInstance) {
+    return firebaseInstance
+  }
+  try {
     const [firebase] = await Promise.all([
-        import("firebase/app"),
-        import("firebase/auth")
+      import("firebase/app"),
+      import("firebase/auth"),
     ])
     firebase.initializeApp(config.firebaseConfig)
     firebaseInstance = firebase
     return firebase
- } catch (error) {
-   console.error(`Firebase could not be initialized`)
-   Sentry.captureException(error) // needs to be imported in an async way ✌️ 
- }
+  } catch (error) {
+    console.error(`Firebase could not be initialized`)
+    Sentry.captureException(error)
+  }
 }
-
 
 export default getFirebase
