5d11d6dbadaeb817a77e434e0b65a6472a914dcf
Bitcoinera <anagjordano@gmail.com>
2020-07-14 12:56:43 +0200

    delete dictionaries file

--- common/dictionaries.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export const accountTypesDictionary = {
-    cosmosStandard: `m/44'/118'/0'/0/0`,
-    terraStation: `m/44'/330'/0'/0/0`,
-    Schnorrkel: `sr25519`,
-    Edwards: `ed25519`,
-    ECDSA: `ecdsa`,
-}
