6eec3d95da8e0c62de8743290011c4006ef2030f
Ana G <40721795+Bitcoinera@users.noreply.github.com>
2020-07-15 17:13:54 +0200

    Update app/src/ActionModal/components/SendModal.vue
    Co-authored-by: Jordan Bibla <jbibla@gmail.com>

--- app/src/ActionModal/components/SendModal.vue
+++ app/src/ActionModal/components/SendModal.vue
@@ -129,7 +129,7 @@
         @keyup.enter.native="enterPressed"
       />
       <span class="memo-span"
-        >To know more on memos read
+        >To learn more about how to use the memo field, read
         <a
           href=" https://intercom.help/lunie/en/articles/3776563-using-the-memo-option-when-sending-tokens-to-and-from-exchanges"
           target="_blank"
