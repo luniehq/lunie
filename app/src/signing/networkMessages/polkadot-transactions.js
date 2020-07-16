1cb39bcc13e9c0d3495d6e3e2b91b95f2c2e510b
Bitcoinera <anagjordano@gmail.com>
2020-07-16 19:13:47 +0200

    revert unnecessary lint change

--- app/src/signing/networkMessages/polkadot-transactions.js
+++ app/src/signing/networkMessages/polkadot-transactions.js
@@ -83,6 +83,7 @@ export function getBroadcastableObject(
   { transaction, payload, rawSignature }
 ) {
   const signature = formatSignature(rawSignature)
+
   transaction.addSignature(
     payload.address.toJSON(),
     signature,
