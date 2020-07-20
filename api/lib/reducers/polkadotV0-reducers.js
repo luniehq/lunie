bac1bb7e611fb0a3751d235129978702530c3d2b
mariopino <info@quequiereshacer.es>
2020-07-20 13:30:56 +0200

    fix

--- api/lib/reducers/polkadotV0-reducers.js
+++ api/lib/reducers/polkadotV0-reducers.js
@@ -243,7 +243,7 @@ function parsePolkadotTransaction(
 
 function getExtrinsicSuccess(extrinsicIndex, blockEvents) {
   let extrinsicSuccess = false
-  for (let index = 0, length = blockEvents.length; index < length; i++) {
+  for (let index = 0, length = blockEvents.length; index < length; index++) {
     const { event, phase } = blockEvents[index]
     if (
       parseInt(phase.toHuman().ApplyExtrinsic) === extrinsicIndex &&
