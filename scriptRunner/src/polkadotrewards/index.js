27942e360441a836dbcf27a2f22715df3a568d22
mariopino <info@quequiereshacer.es>
2020-07-22 17:32:03 +0200

    lint

--- scriptRunner/src/polkadotrewards/index.js
+++ scriptRunner/src/polkadotrewards/index.js
@@ -3,11 +3,11 @@ const {scheduleJob} = require('../executor')
 module.exports = async function (req, res) {
   const {era, networkId} = req.body;
   try {
-      scheduleJob("polkadotRewards", {era, networkId})
-      // we don't wait for the job to end because of timeouts
-      res.status(200)
-      res.send()
-      return
+    scheduleJob("polkadotRewards", {era, networkId})
+    // we don't wait for the job to end because of timeouts
+    res.status(200)
+    res.send()
+    return
   } catch (error) {
     res.status(500)
     res.send(error.message)
