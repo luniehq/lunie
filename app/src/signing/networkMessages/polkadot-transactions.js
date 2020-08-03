c38f9d3f5c9ce33c9fa99ad3174381dbd8e56636
Fabian <frznhope@gmail.com>
2020-08-03 14:01:23 -0500

    Fabo/use polkadot api signing (#4650)
    * use polkadot api for signing offline
    
    * changelog
    
    * changelog
    
    Co-authored-by: MergeBack Lunie Bot <bot@lunie.io>

--- app/src/signing/networkMessages/polkadot-transactions.js
+++ app/src/signing/networkMessages/polkadot-transactions.js
@@ -1,8 +1,3 @@
-/*
- * Source: https://github.com/polkadot-js/tools/blob/master/packages/signer-cli/src/cmdSendOffline.ts
- */
-import { u8aToHex, u8aConcat } from "@polkadot/util"
-
 /**
  * Send a JSONRPC request to the node at substrateNodeEndpoint.
  *
@@ -32,46 +27,9 @@ export async function getSignMessage(senderAddress, transaction, api) {
     }
   }
 
-  const nonce = (await api.derive.balances.account(senderAddress)).accountNonce
-  let options
-  let blockNumber
-
-  // Get current block if we want to modify the number of blocks we have to sign
-  const signedBlock = await api.rpc.chain.getBlock()
-  options = {
-    blockHash: signedBlock.block.header.hash,
-    era: api.createType("ExtrinsicEra", {
-      current: signedBlock.block.header.number,
-      period: 50,
-    }),
-    nonce,
+  return {
+    transaction
   }
-  blockNumber = signedBlock.block.header.number
-
-  const payload = api.createType("SignerPayload", {
-    version: api.extrinsicVersion,
-    runtimeVersion: api.runtimeVersion,
-    genesisHash: api.genesisHash,
-    ...options,
-    address: senderAddress,
-    method: transaction.method,
-    blockNumber,
-  })
-
-  return { payload, transaction }
-}
-
-// just for completeness in here
-const curvePrefixes = {
-  ed25519: [0],
-  sr25519: [1],
-  ecdsa: [2],
-}
-
-function formatSignature(rawSignature, curve) {
-  const prefix = new Uint8Array(curvePrefixes[curve])
-  const signature = u8aToHex(u8aConcat(prefix, rawSignature))
-  return signature
 }
 
 export function getSignableObject(chainMessages) {
@@ -81,18 +39,7 @@ export function getSignableObject(chainMessages) {
 export function getBroadcastableObject(
   chainMessages,
   transactionData,
-  { transaction, payload, rawSignature },
-  HDPath,
-  curve
+  { signedMessage }
 ) {
-  const signature = formatSignature(rawSignature, curve)
-
-  transaction.addSignature(
-    payload.address.toJSON(),
-    signature,
-    payload.toPayload()
-  )
-
-  const signedMessage = transaction.toJSON()
   return signedMessage
 }
