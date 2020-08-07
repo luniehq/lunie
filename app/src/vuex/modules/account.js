2b0af5bfc3f9986e29433a0fe9324c13c560560a
Bitcoinera <anagjordano@gmail.com>
2020-08-08 00:11:46 +0200

    comment out evil code chunk of disgrace

--- app/src/vuex/modules/account.js
+++ app/src/vuex/modules/account.js
@@ -109,14 +109,14 @@ export default ({ apollo }) => {
           ? `https://app.lunie.io/email-authentication`
           : `${window.location.protocol}//${window.location.host}/email-authentication`,
         handleCodeInApp: true,
-        android: {
-          packageName: `org.lunie.lunie`,
-          installApp: true,
-          minimumVersion: `1.0.221`, // the first version with deep linking enabled
-        },
-        iOS: {
-          bundleId: `1475911030.org.lunie.lunie`,
-        },
+        // android: {
+        //   packageName: `org.lunie.lunie`,
+        //   installApp: true,
+        //   minimumVersion: `1.0.221`, // the first version with deep linking enabled
+        // },
+        // iOS: {
+        //   bundleId: `1475911030.org.lunie.lunie`,
+        // },
       }
       try {
         await Auth.sendSignInLinkToEmail(user.email, actionCodeSettings)
