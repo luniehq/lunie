2f4cdc1bfd0eb2ed3f0090e0639602454339b111
Jordan Bibla <jbibla@gmail.com>
2020-08-07 15:12:07 -0400

    fix for iOS sign in

--- app/src/vuex/modules/account.js
+++ app/src/vuex/modules/account.js
@@ -111,9 +111,9 @@ export default ({ apollo }) => {
         //   installApp: true,
         //   minimumVersion: `1.0.221`, // the first version with deep linking enabled
         // },
-        // iOS: {
-        //   bundleId: `1475911030.org.lunie.lunie`,
-        // },
+        iOS: {
+          bundleId: `org.lunie.lunie`,
+        },
       }
       try {
         await Auth.sendSignInLinkToEmail(user.email, actionCodeSettings)
@@ -171,8 +171,8 @@ export function handleDeeplink(url, router) {
 
   // if we receive a deeplink for firebase authentication we follow that link
   // the target will perform the authentication and then redirect back to lunie
-  if (queryObject.link) {
-    window.open(unescape(queryObject.link), "_blank")
+  if (queryObject.ifl) {
+    window.open(unescape(queryObject.ifl), "_blank")
   }
 
   try {
