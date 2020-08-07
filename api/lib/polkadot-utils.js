77d74758372f1172e540a34f9c609f547761eb63
Bitcoinera <anagjordano@gmail.com>
2020-08-07 13:07:56 +0200

    fixing blockToDate

--- api/lib/polkadot-utils.js
+++ api/lib/polkadot-utils.js
@@ -16,23 +16,33 @@ function secondsToDhm(seconds) {
   const h = Math.floor((seconds % (3600 * 24)) / 3600)
   const m = seconds < 60 ? 1 : Math.floor((seconds % 3600) / 60)
 
-  const dDisplay = d + 'd '
-  const hDisplay = h + 'h '
-  const mDisplay = m + 'm'
+  const dDisplay = d
+  // const hDisplay = h + 'h '
+  // const mDisplay = m + 'm'
 
-  return dDisplay + hDisplay + mDisplay
+  // return dDisplay + hDisplay + mDisplay
+  return dDisplay
+}
+
+const getDateFromDayNum = function (dayNum) {
+  const date = new Date()
+  date.setMonth(0)
+  date.setDate(0)
+  const timeOfFirst = date.getTime() // this is the time in milliseconds of 1/1/YYYY
+  const dayMilli = 1000 * 60 * 60 * 24
+  const dayNumMilli = dayNum * dayMilli
+  date.setTime(timeOfFirst + dayNumMilli)
+  return date
 }
 
 const blockToDate = (block, network) => {
   const blocktime = (network.blockTime || 6000) / 1000 // 6000ms is the block time for both Polkadot and Kusama
-
   if (typeof block !== 'number') {
     block = block.toNumber()
   }
-
-  const time = secondsToDhm(block * blocktime)
-
-  return time
+  const dayNumberInYear = secondsToDhm(block * blocktime)
+  const date = getDateFromDayNum(dayNumberInYear - 23) // there is a 23 days offset, no idea why
+  return date.toUTCString()
 }
 
 module.exports = {
