e23b6cf8770fb2386c490803db3a62e6d60888f6
Bitcoinera <anagjordano@gmail.com>
2020-08-11 19:26:11 +0200

    fix delegation amount

--- api/lib/reducers/akashV0-reducers.js
+++ api/lib/reducers/akashV0-reducers.js
@@ -19,15 +19,14 @@ function setTransactionSuccess(transaction) {
 }
 
 function delegationReducer(delegation, validator) {
-  const delegationCoin = delegation.balance
-    ? terraV3Reducers.atoms(delegation.balance.amount)
-    : 0
   return {
     id: delegation.validator_address,
     validatorAddress: delegation.validator_address,
     delegatorAddress: delegation.delegator_address,
     validator,
-    amount: delegationCoin.amount
+    amount: delegation.balance
+      ? terraV3Reducers.atoms(delegation.balance.amount)
+      : 0
   }
 }
 
