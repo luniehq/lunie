b86412a91c9adbfa8dc206e9b53e01c556981e28
Bitcoinera <anagjordano@gmail.com>
2020-09-01 19:09:24 +0200

    add comment on resolveValidatorName length check

--- app/src/filters/index.js
+++ app/src/filters/index.js
@@ -45,6 +45,8 @@ export const formatAddress = (address, length = 4) => {
 }
 
 export const resolveValidatorName = (address, validators) => {
+  // Substrate validators can have their operatorAddress as their name, which is too long and thus breaks the
+  // extension size. If name is too long, then we just display the formatted address
   if (validators[address] && validators[address].name.length < 20) {
     return validators[address].name
   }
