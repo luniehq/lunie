ab1a35e4f24f008dd0c638a404f0c2d27c2f1299
Bitcoinera <anagjordano@gmail.com>
2020-09-15 12:27:27 +0200

    remove getProposalSummary duplicates

--- api/lib/reducers/polkadotV0-reducers.js
+++ api/lib/reducers/polkadotV0-reducers.js
@@ -527,19 +527,6 @@ function networkAccountReducer(account) {
   }
 }
 
-function getProposalSummary(type) {
-  switch (type) {
-    case `TEXT`:
-      return `This is a text proposal. Text proposals can be proposed by anyone and are used as a signalling mechanism for this community. If this proposal is accepted, nothing will change without community coordination.`
-    case `PARAMETER_CHANGE`:
-      return `This is a parameter change proposal. Parameter change proposals can be proposed by anyone and include changes to the code of this network. If this proposal is approved the underlying code will change.`
-    case `TREASURY`:
-      return `This is a treasury proposal. Treasury proposals can be proposed by anyone and are a request for funds from the treasury / community pool.`
-    default:
-      return `Unknown proposal type`
-  }
-}
-
 function democracyProposalReducer(
   network,
   proposal,
@@ -562,7 +549,6 @@ function democracyProposalReducer(
     deposit: toViewDenom(network, proposal.balance),
     summary: getProposalSummary(proposalTypeEnum.PARAMETER_CHANGE),
     proposer,
-    summary: getProposalSummary(proposalTypeEnum.PARAMETER_CHANGE),
     detailedVotes
   }
 }
