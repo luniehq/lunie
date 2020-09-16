6f0527cd7c2c0ab023cc07a439f2cfcb72fc13d4
Bitcoinera <anagjordano@gmail.com>
2020-09-16 13:18:06 +0200

    avoid Invalid Dates in Timeline

--- app/src/components/governance/PageProposal.vue
+++ app/src/components/governance/PageProposal.vue
@@ -19,7 +19,14 @@
 
       <ParticipantList v-if="participants" :participants="participants" />
 
-      <template v-if="proposal.detailedVotes.timeline">
+      <template
+        v-if="
+          proposal.detailedVotes.timeline &&
+          Object.values(proposal.detailedVotes.timeline).find(
+            ({ time }) => time
+          )
+        "
+      >
         <Timeline :timeline="proposal.detailedVotes.timeline" />
       </template>
 
