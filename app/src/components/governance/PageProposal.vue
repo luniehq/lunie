74160b7036f74d2a4ea16f093ecad34ed007b69c
Bitcoinera <anagjordano@gmail.com>
2020-09-18 18:12:57 +0200

    don't hide democracy deposit bar for polkadot

--- app/src/components/governance/PageProposal.vue
+++ app/src/components/governance/PageProposal.vue
@@ -10,7 +10,7 @@
       />
 
       <ProposalStatusBar
-        v-if="tallyHasValues && !isPolkadotDemocracy"
+        v-if="tallyHasValues"
         :status="status"
         :status-begin-time="proposal.statusBeginTime"
         :total-votes="proposal.tally.total"
