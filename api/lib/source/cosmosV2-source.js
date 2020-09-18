0cb1021c8aa1e8e79cc99a550fb17260834b46c3
Ana G <40721795+Bitcoinera@users.noreply.github.com>
2020-09-18 17:23:25 +0200

    Ana/fix proposer in cosmos (#4937)
    * fix proposer in cosmos
    
    * fix this.query
    
    * changelog
    
    * add noRetry logic to cosmos-V2 query

--- api/lib/source/cosmosV2-source.js
+++ api/lib/source/cosmosV2-source.js
@@ -6,8 +6,8 @@ class CosmosV2API extends CosmosV0API {
     this.reducers = require('../reducers/cosmosV2-reducers')
   }
 
-  async query(url, resultSelector = 'result') {
-    const response = await this.getRetry(url)
+  async query(url, noRetry, resultSelector = 'result') {
+    const response = await this.getRetry(url, noRetry ? 3 : 0)
     return response[resultSelector]
   }
 
