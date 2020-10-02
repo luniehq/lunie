418667e7fb30d42c7f88d88382fea9f07d9de934
Bitcoinera <anagjordano@gmail.com>
2020-10-02 20:29:50 +0200

    fix terra delegations

--- api/lib/reducers/terraV3-reducers.js
+++ api/lib/reducers/terraV3-reducers.js
@@ -1,5 +1,6 @@
 const cosmosV3Reducers = require('./cosmosV3-reducers')
 const cosmosV2Reducers = require('./cosmosV2-reducers')
+const { coinReducer } = require('./cosmosV0-reducers')
 
 // Terra has a slightly different structure and needs its own undelegationEndTimeReducer
 function undelegationEndTimeReducer(transaction) {
@@ -24,8 +25,23 @@ function undelegationEndTimeReducer(transaction) {
   }
 }
 
+function delegationReducer(delegation, validator, active, network) {
+  const coinLookup = network.getCoinLookup(network, delegation.balance.denom)
+  const { amount, denom } = coinReducer(delegation.balance, coinLookup)
+
+  return {
+    id: delegation.validator_address.concat(`-${denom}`),
+    validatorAddress: delegation.validator_address,
+    delegatorAddress: delegation.delegator_address,
+    validator,
+    amount,
+    active
+  }
+}
+
 module.exports = {
   ...cosmosV2Reducers,
   blockReducer: cosmosV3Reducers.blockReducer,
-  undelegationEndTimeReducer
+  undelegationEndTimeReducer,
+  delegationReducer
 }
