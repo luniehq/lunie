ef8a746b6d54b9a3c8b962f600088de93fabeb28
Bitcoinera <anagjordano@gmail.com>
2020-10-14 18:41:14 +0200

    revert first fixing changes

--- api/lib/stores/block-store.js
+++ api/lib/stores/block-store.js
@@ -102,11 +102,10 @@ class BlockStore {
   // this adds all the validator addresses to the database so we can easily check in the database which ones have an image and which ones don't
   async updateDBValidatorProfiles(validators) {
     const validatorRows = Object.values(validators).map(
-      ({ operatorAddress, name, identity, picture }) => ({
+      ({ operatorAddress, name, identity }) => ({
         operator_address: operatorAddress,
         name,
-        profile_identifier: identity,
-        picture
+        profile_identifier: identity
       })
     )
     return this.db.upsert('validatorprofiles', validatorRows)
