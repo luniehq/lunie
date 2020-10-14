ecdab1ceb20d493a3ed1e3c7d6f5a063eeaddfd4
Mario Pino <info@quequiereshacer.es>
2020-10-14 21:43:48 +0200

    Reenable substrate api sidecar (#5093)
    * fix handling of getting block without height
    
    * changelog
    
    * Revert "Ana/return to old polkadot node subscription (#4953)"
    
    This reverts commit 776e960a976565a3cf4ce95afc3d117d138e48f2.
    
    * get block height correctly
    
    * get correct block hash
    
    * work in progress
    
    * work in progress
    
    * reducers
    
    * fix tx
    
    * cache +  retry
    
    * fix sessionIndex
    
    * replacing  queries
    
    * lint
    
    * cleanup
    
    * cleanup
    
    * balances
    
    * address role queries
    
    * getUndelegationsForDelegatorAddress
    
    * getDelegationForValidator
    
    * fix
    
    * fix stakedBalance for controller addresses
    
    * getActiveEra getter
    
    * getTreasurySize
    
    * loadClaimedRewardsForValidators
    
    * getNetworkAccountInfo
    
    * cleanup, work in progress
    
    * getAllValidatorsExpectedReturns
    
    * fix
    
    * fix
    
    * fixes
    
    * fix again
    
    * fix
    
    * fix
    
    * typo
    
    * fix
    
    * getTopVoters
    
    * getGovernanceOverview
    
    * wip
    
    * replace query, parallelize
    
    * migrate sidecar endpoints to v1
    
    * fix, lint
    
    * fix
    
    * changelog
    
    * lint
    
    * we want latest block, not finalized
    
    * fix, add balances.transferKeepAlive
    
    * modify extrinsic arg parsing
    
    * fix
    
    * fix tx, cleanup
    
    * lint
    
    * fix
    
    * cleanup
    
    * fixes
    
    * changelog
    
    * cleanup
    
    * cleanup
    
    * fix conflict error
    
    * lint
    
    * Update api/lib/source/polkadotV0-source.js
    
    Co-authored-by: Fabian <frznhope@gmail.com>
    
    * remove not needed this.baseURL
    
    * lint
    
    * refactor
    
    Co-authored-by: MergeBack Lunie Bot <bot@lunie.io>
    Co-authored-by: Fabian <frznhope@gmail.com>

--- api/lib/block-listeners/polkadot-node-subscription.js
+++ api/lib/block-listeners/polkadot-node-subscription.js
@@ -40,9 +40,7 @@ class PolkadotNodeSubscription extends BaseNodeSubscription {
       )
       const [validators, era] = await Promise.all([
         dataSource.getAllValidators(),
-        this.store.polkadotRPC.query.staking.activeEra().then(async (era) => {
-          return era.toJSON().index
-        })
+        dataSource.getActiveEra()
       ])
       this.store.update({
         data: {
