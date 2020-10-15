27e31e70387e1bb59de422035fa804bd2be976f8
Bitcoinera <anagjordano@gmail.com>
2020-10-15 13:44:17 +0200

    lint

--- api/lib/source/polkadotV0-source.js
+++ api/lib/source/polkadotV0-source.js
@@ -8,7 +8,10 @@ const {
   getPassingThreshold,
   getFailingThreshold
 } = require('@polkassembly/util')
-const { fixDecimalsAndRoundUpBigNumbers, toViewDenom } = require('../../common/numbers.js')
+const {
+  fixDecimalsAndRoundUpBigNumbers,
+  toViewDenom
+} = require('../../common/numbers.js')
 const { BaseRESTDataSource } = require('./BaseRESTDataSource.js')
 const delegationEnum = { ACTIVE: 'ACTIVE', INACTIVE: 'INACTIVE' }
 
