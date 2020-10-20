d98c9b49390ac62e6c4b031f44305dde06ad3761
Fabian <frznhope@gmail.com>
2020-10-20 16:20:39 -0500

    Fabo/fix twitter (#5145)
    * emoney fixes (#5069) (#5070)
    
    * looks like some emoney fixes got reverted
    
    * cha cha changelog
    
    Co-authored-by: Jordan Bibla <jbibla@gmail.com>
    
    * deploy staging api with more memory
    
    * deploy api
    
    * deploy prodcution api
    
    * disable get all delegators query
    
    * handle disabled datasource in notifications
    
    * deploy script runner production
    
    * switched to twitter api 2
    
    * changelog
    
    Co-authored-by: Ana G <40721795+Bitcoinera@users.noreply.github.com>
    Co-authored-by: Jordan Bibla <jbibla@gmail.com>
    Co-authored-by: MergeBack Lunie Bot <bot@lunie.io>

--- scriptRunner/src/twitterImages.js
+++ scriptRunner/src/twitterImages.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch")
+const _ = require("lodash")
 const Sentry = require("@sentry/node")
 const database = require("../../api/lib/database")
 const config = require("../config")
@@ -17,35 +18,40 @@ module.exports.getTwitterImages = async () => {
     const updateableValidators = validators
       // in the DB we have sometimes "undefined" for the identifier
       .filter(({profile_identifier}) => profile_identifier && profile_identifier !== 'undefined')
-      .filter(({last_updated}) => {
-        // only update validators every 24h
-        return Date.now() - new Date(last_updated).getTime() > 1000 * 60 * 60 * 24
-      })
-    const updatedValidators = await Promise.all(
-      updateableValidators
-      .map(async ({ operator_address, profile_identifier, name }) => {
-        const query = `https://api.twitter.com/1.1/users/show.json?screen_name=${profile_identifier}`
-        const result = await fetch(query, {
-          headers: {
-            authorization: `Bearer ${config.twitterAuthToken}`
-          }
-        }).then(res => res.json())
-        if (!result.errors) {
-          return {
-            name,
-            operator_address,
-            picture: result.profile_image_url.replace('http://', 'https://')
-          }
-        } else {
-          console.log(JSON.stringify(result.errors, null, 2))
-          Sentry.captureException(new Error(JSON.stringify(result.errors)))
+      // .filter(({last_updated}) => {
+      //   // only update validators every 24h
+      //   return Date.now() - new Date(last_updated).getTime() > 1000 * 60 * 60 * 24
+      // })
+    const chunksOfHundred = _.chunk(updateableValidators, 100) // twitter accepts max 100 usernames in parallel
+    let updatedValidators = []
+    await Promise.all(chunksOfHundred.map(async chunk => {
+      const allTwitterNames = chunk.map(({ profile_identifier }) => profile_identifier.replace("@", ""))
+      const query = `https://api.twitter.com/2/users/by?usernames=${allTwitterNames.join(',')}&user.fields=profile_image_url`
+      const twitterResult = await fetch(query, {
+        headers: {
+          authorization: `Bearer ${config.twitterAuthToken}`
         }
-        return {
-          name,
-          operator_address
-        }
-      })
-    )
+      }).then(res => res.json())
+      if (!twitterResult.errors) {
+        updatedValidators = updatedValidators.concat(
+          chunk.map((({ profile_identifier, name, operator_address }) => {
+            const userName = profile_identifier.replace("@", "")
+            const user = twitterResult.data.find(({username}) => username === userName)
+            if (!user) {
+              return {
+                name,
+                operator_address,
+              }
+            }
+            return {
+              name,
+              operator_address,
+              picture: user.profile_image_url.replace('http://', 'https://')
+            }
+          }))
+        )
+      }
+    }))
     const rows = updatedValidators
       .map(({name, operator_address, picture}) => {
         let row = {
