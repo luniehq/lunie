8ebcefc4bd8aebf143f30cf79b78f56dfd2246b2
Ana G <40721795+Bitcoinera@users.noreply.github.com>
2020-11-10 17:14:44 +0100

    Ana/5190 extension banner about sunsetting (#5192)
    * add sunsetting warning bar
    
    * some small style fixes
    
    * some more style fixes
    
    * changelog
    
    * fix a bit styles for accounts view
    
    * Apply suggestions from code review
    
    Co-authored-by: Jordan Bibla <jbibla@gmail.com>
    
    * hide x from sunsetting warning bar
    
    * update snaps
    
    * changelog
    
    Co-authored-by: Fabian <frznhope@gmail.com>
    Co-authored-by: Jordan Bibla <jbibla@gmail.com>

---
+++ extension/src/components/SunsettingWarningBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <Bar
+    class="sunsetting-warning-bar"
+    :show="true"
+    :hide-close="true"
+    bar-type="info"
+  >
+    We are sunsetting the Lunie Browser Extension. Please read our announcement
+    for more information.
+    <a
+      href="https://medium.com/luniehq/sunsetting-lunie-io-66d566c14ba1"
+      target="_blank"
+      >here.</a
+    >
+  </Bar>
+</template>
+
+<script>
+import Bar from 'common/Bar'
+
+export default {
+  name: `sunsetting-warning-bar`,
+  components: {
+    Bar
+  }
+}
+</script>
+
+<style>
+.sunsetting-warning-bar {
+  margin: 0;
+}
+
+.bar-container {
+  margin: 0 0 1rem !important;
+}
+
+.bar {
+  padding: 0.75rem 0.75rem 0 !important;
+}
+
+.left {
+  padding: 0;
+  font-size: 12px;
+}
+</style>
